8d076feb34021d4b3f5fbc2deb3fe596083ba88d
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-03-10 23:11:47 +0000

    Adding a decoder vanilla block, which exposes issuse in the MHA interface

--- tests/test_block_factory.py
+++ tests/test_block_factory.py
@@ -6,8 +6,10 @@ from xformers.block_factory import (
     FeedforwardConfig,
     MultiHeadDispatchConfig,
     PositionEncodingConfig,
-    xFormerBlock,
-    xFormerConfig,
+    xFormerDecoderBlock,
+    xFormerDecoderConfig,
+    xFormerEncoderBlock,
+    xFormerEncoderConfig,
 )
 
 # Automatically fetch all registered attentions and Feedforwards
@@ -15,7 +17,7 @@ from xformers.components.attention import ATTENTION_REGISTRY
 from xformers.components.feedforward import FEEDFORWARD_REGISTRY, Activations
 
 BATCH = 20
-SEQ = 1920
+SEQ = 512
 MODEL = 384
 DROPOUT = 0.5
 
@@ -27,7 +29,7 @@ DROPOUT = 0.5
 @pytest.mark.parametrize("activation", [a.value for a in Activations])
 @pytest.mark.parametrize("attention_name", ATTENTION_REGISTRY.keys())
 @pytest.mark.parametrize("feedforward_name", FEEDFORWARD_REGISTRY.keys())
-def test_xformer_block(
+def test_xformer_encoder_block(
     attention_name: str,
     feedforward_name: str,
     heads: int,
@@ -41,7 +43,7 @@ def test_xformer_block(
         "name": attention_name,
         "dropout": attn_dropout,
         "causal": causal,
-        "window_size": SEQ // 10,
+        "window_size": SEQ // 8,
     }
 
     multi_head_config = {
@@ -61,7 +63,7 @@ def test_xformer_block(
 
     position_encoding_config = {"name": "sine", "dim_model": MODEL, "seq_len": SEQ}
 
-    block_config = xFormerConfig(
+    block_config = xFormerEncoderConfig(
         MODEL,
         AttentionConfig(**attention_config),
         MultiHeadDispatchConfig(**multi_head_config),
@@ -70,8 +72,77 @@ def test_xformer_block(
     )
 
     # Test that the whole block can be instantiated
-    block = xFormerBlock.from_config(block_config)
+    block = xFormerEncoderBlock.from_config(block_config)
 
     # Check that the dimensions make sense, to a FW pass
     inputs = torch.rand(BATCH, SEQ, MODEL)
     _ = block(inputs)
+
+
+@pytest.mark.parametrize("attn_dropout", [0.0, 0.1])
+@pytest.mark.parametrize("residual_dropout", [0.0, 0.1])
+@pytest.mark.parametrize("causal", [True, False])
+@pytest.mark.parametrize("heads", [1, 3])
+@pytest.mark.parametrize("activation", [a.value for a in Activations])
+@pytest.mark.parametrize("attention_name", ATTENTION_REGISTRY.keys())
+@pytest.mark.parametrize("feedforward_name", FEEDFORWARD_REGISTRY.keys())
+def _test_xformer_decoder_block(
+    attention_name: str,
+    feedforward_name: str,
+    heads: int,
+    attn_dropout: float,
+    residual_dropout: float,
+    causal: bool,
+    activation: Activations,
+):
+
+    attention_config = {
+        "name": attention_name,
+        "dropout": attn_dropout,
+        "causal": causal,
+        "window_size": SEQ // 8,
+    }
+
+    multi_head_config = {
+        "n_heads": heads,
+        "dim_seq": SEQ,
+        "dim_model": MODEL,
+        "residual_dropout": residual_dropout,
+    }
+
+    feedforward_config = {
+        "name": feedforward_name,
+        "dim_latent": MODEL,
+        "dropout": DROPOUT,
+        "activation": activation,
+        "hidden_layer_multiplier": 4,
+    }
+
+    position_encoding_config = {"name": "sine", "dim_model": MODEL, "seq_len": SEQ}
+
+    encoder_block_config = xFormerEncoderConfig(
+        MODEL,
+        AttentionConfig(**attention_config),
+        MultiHeadDispatchConfig(**multi_head_config),
+        FeedforwardConfig(**feedforward_config),
+        PositionEncodingConfig(**position_encoding_config),
+    )
+
+    decoder_block_config = xFormerDecoderConfig(
+        MODEL,
+        (AttentionConfig(**attention_config),) * 2,
+        (MultiHeadDispatchConfig(**multi_head_config),) * 2,
+        FeedforwardConfig(**feedforward_config),
+        PositionEncodingConfig(**position_encoding_config),
+    )
+
+    # Test that the whole block can be instantiated
+    encoder_block = xFormerEncoderBlock.from_config(encoder_block_config)
+    decoder_block = xFormerDecoderBlock.from_config(decoder_block_config)
+
+    # Check that the dimensions make sense, to a FW pass
+    inputs = torch.rand(BATCH, SEQ, MODEL)
+    encoded = encoder_block(inputs)
+    _ = decoder_block(
+        inputs, encoded
+    )  # FIXME: does not make a lot of sense, just checking dimensions
--- xformers/block_factory.py
+++ xformers/block_factory.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from typing import Optional, Tuple
 
+import torch
 import torch.nn as nn
 
 from xformers.components.attention import (  # noqa
@@ -8,25 +10,43 @@ from xformers.components.attention import (  # noqa
     build_multi_head_attention,
 )
 from xformers.components.feedforward import FeedforwardConfig, build_feedforward
-from xformers.components.positional_encoding import PositionEncodingConfig
+from xformers.components.positional_encoding import (
+    PositionEncodingConfig,
+    build_positional_encoding,
+)
 
 
 @dataclass
-class xFormerConfig:
+class xFormerEncoderConfig:
     dim_model: int
     attention_config: AttentionConfig
     multi_head_config: MultiHeadDispatchConfig
     feedforward_config: FeedforwardConfig
-    position_encoding_config: PositionEncodingConfig
+    position_encoding_config: Optional[PositionEncodingConfig]
 
 
-class xFormerBlock(nn.Module):
-    """ a vanilla Transformer block """
+@dataclass
+class xFormerDecoderConfig:
+    dim_model: int
+    attention_configs: Tuple[AttentionConfig, ...]
+    multi_head_configs: Tuple[MultiHeadDispatchConfig, ...]
+    feedforward_config: FeedforwardConfig
+    position_encoding_config: Optional[PositionEncodingConfig]
 
-    def __init__(self, config: xFormerConfig):
+
+class xFormerEncoderBlock(nn.Module):
+    r""" A vanilla Transformer Encoder block """
+
+    def __init__(self, config: xFormerEncoderConfig):
         super().__init__()
         self.ln1 = nn.LayerNorm(config.dim_model)
         self.ln2 = nn.LayerNorm(config.dim_model)
+
+        self.pose_encoding = (
+            build_positional_encoding(config.position_encoding_config)
+            if config.position_encoding_config
+            else None
+        )
 
         self.attn = build_multi_head_attention(
             config.attention_config, config.multi_head_config
@@ -34,10 +54,60 @@ class xFormerBlock(nn.Module):
         self.ff = build_feedforward(config.feedforward_config)
 
     @classmethod
-    def from_config(cls, config: xFormerConfig):
+    def from_config(cls, config: xFormerEncoderConfig):
         return cls(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln1(x))
-        x = x + self.ff(self.ln2(x))
+        if self.pose_encoding:
+            x = self.pose_encoding(x)
+
+        x = self.ln1(x + self.attn(x))
+        x = self.ln2(x + self.ff(x))
+        return x
+
+
+class xFormerDecoderBlock(nn.Module):
+    r""" A vanilla Transformer Decoder block """
+
+    def __init__(self, config: xFormerDecoderConfig):
+        super().__init__()
+        self.linear1 = nn.Linear(config.dim_model, config.feedforward_config.dim_latent)
+        self.linear2 = nn.Linear(config.dim_model, config.feedforward_config.dim_latent)
+
+        self.ln1 = nn.LayerNorm(config.dim_model)
+        self.ln2 = nn.LayerNorm(config.dim_model)
+        self.ln3 = nn.LayerNorm(config.dim_model)
+
+        self.pose_encoding = (
+            build_positional_encoding(config.position_encoding_config)
+            if config.position_encoding_config
+            else None
+        )
+
+        self.attn1 = build_multi_head_attention(
+            config.attention_configs[0], config.multi_head_configs[0]
+        )
+        self.attn2 = build_multi_head_attention(
+            config.attention_configs[1], config.multi_head_configs[1]
+        )
+
+        self.ff = build_feedforward(config.feedforward_config)
+
+    @classmethod
+    def from_config(cls, config: xFormerDecoderConfig):
+        return cls(config)
+
+    def forward(self, target: torch.Tensor, memory: torch.Tensor):
+        if self.pose_encoding:
+            target = self.pose_encoding(target)
+
+        # Masked multi head attention
+        x = self.ln1(target + self.attn1(target))
+
+        # Include the memory/Encoder results
+        # FIXME: Expose direct K,V,Q in the MHA
+        x = self.ln2(x + self.attn2(torch.cat([memory, memory, target])))
+
+        # FF
+        x = self.ln3(x + self.ff(x))
         return x
