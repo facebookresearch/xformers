c8571e02aa6a64e3a6ec2f24ded2360c673aac7b
Francisco Massa <fvsmassa@gmail.com>
2021-06-03 17:39:14 +0200

    Add improved heuristics for using COO kernels (#145)
    * Add improved heuristics for using COO kernels
    
    * Update vision_transformer notebook with new numbers

--- xformers/components/attention/_sputnik_sparse.py
+++ xformers/components/attention/_sputnik_sparse.py
@@ -9,9 +9,39 @@ def _csr_to_coo(m, n, row_offsets, column_indices):
     return row_coo, column_indices
 
 
+def _should_use_coo(a, sparsity):
+    if not a.is_cuda:
+        return False
+    B, M, K = a.shape
+    # amortize overhead of converting from csr to coo
+    if B < 32 and M < 4096:
+        return False
+    if sparsity > 0.995:
+        return False
+    if sparsity < 0.9:
+        return False
+    if K > 64:
+        return False
+    # let's be overly cautious here for now
+    return sparsity > 0.97
+
+
+def _should_use_csr_ge(a, sparsity):
+    if not a.is_cuda:
+        return False
+    return sparsity > 0.99
+
+
 def _sddmm_func(a, b, row_indices, row_offsets, column_indices):
     sparsity = 1 - column_indices.shape[0] / (a.shape[1] * b.shape[1])
-    if sparsity > 0.99 and a.is_cuda:
+    if _should_use_coo(a, sparsity):
+        m = a.shape[-2]
+        n = b.shape[-2]
+        # converting from csr to coo has a constant overhead of ~150us
+        # so only dispatch to it for reasonably large problem sizes
+        ro, ci = _csr_to_coo(m, n, row_offsets, column_indices)
+        return torch.ops.xformers.coo_sddmm(a, b, row_indices, ro, ci)
+    elif _should_use_csr_ge(a, sparsity):
         return torch.ops.xformers.csr_sddmm(
             a, b, row_indices, row_offsets, column_indices
         )
