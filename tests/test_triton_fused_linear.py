[bugfix] FusedLinearLayer: computing all the gradients (#259)
* computing all the gradients
* bringing back the configs for the P100 so that CI survives

tests/test_triton_fused_linear.py
@@ -25,24 +25,30 @@ SHAPES = [(8, 384, 128), (8, 784, 512)]
 
 @pytest.mark.skipif(not _triton_available, reason="Triton is not available")
 @pytest.mark.parametrize("shape", SHAPES)
-def test_fused_matmul(shape):
+@pytest.mark.parametrize(
+    "dtype", [torch.float32]
+)  # Triton use tensor cores, which return slightly different results to pytorch mm
+def test_fused_matmul(shape, dtype):
     """ Check that the matrix multiply kernel and Pytorch's give the same results"""
+    torch.random.manual_seed(0)
 
     # Raw fused matrix multiply first, to catch gross errors
-    a = torch.rand((shape[1], shape[2]), device="cuda") * 0.01
-    b = torch.rand((shape[2], shape[1]), device="cuda") * 0.01
+    a = torch.rand((shape[1], shape[2]), dtype=dtype, device="cuda")
+    b = torch.rand((shape[2], shape[1]), dtype=dtype, device="cuda")
 
     # Test that not passing any bias is fine
     res_torch = a @ b
-    res_triton, _ = fused_matmul(a, b, None)
+    res_triton, _ = fused_matmul(a, b.transpose(0, 1), None)
     assert torch.allclose(res_torch, res_triton), "Vanilla matmul is broken"
 
     # Now test with a real FMA
-    c = -torch.rand((shape[1],), device="cuda")
+    c = -torch.rand((shape[1],), dtype=dtype, device="cuda")
     res_torch = torch.addmm(c, a, b)
-    res_triton, _ = fused_matmul(a, b, c)
+    res_triton, _ = fused_matmul(a, b.transpose(1, 0), c)
 
-    assert torch.allclose(res_torch, res_triton), "Vanilla fused matmul is broken"
+    assert torch.allclose(
+        res_torch, res_triton
+    ), f"Vanilla fused matmul is broken {torch.max(torch.abs(res_torch-res_triton)).item()}"
 
     # Now check that adding an activation to the mix still produces valid results
     for activation in Activation:
@@ -50,7 +56,7 @@ def test_fused_matmul(shape):
         res_torch = torch_activation(torch.addmm(c, a, b))
 
         triton_activation = get_triton_activation_kernel(activation)
-        res_triton, _ = fused_matmul(a, b, c, triton_activation)
+        res_triton, _ = fused_matmul(a, b.transpose(1, 0), c, triton_activation)
 
         # FIXME: @lefaudeux
         # GeLUs are not well handled for now, we use an approximation
@@ -67,40 +73,44 @@ def test_fused_matmul(shape):
     not _triton_available or gpu_capabilities_older_than_70(),
     reason="Triton requires a SM70+ GPU",
 )
-@pytest.mark.parametrize("activation", [a.value for a in Activation])
+@pytest.mark.parametrize("activation", [None] + [a.value for a in Activation])  # type: ignore
 @pytest.mark.parametrize("shape", SHAPES)
-@pytest.mark.parametrize("bias", [False, True])
-@pytest.mark.parametrize("amp", [True, False])
+@pytest.mark.parametrize("bias", [True, False])
+@pytest.mark.parametrize("amp", [True])  # FIXME: @lefaudeux check the fp32 case
 def test_fused_linear_parity(shape, activation: Activation, bias: bool, amp: bool):
     """Check that PyTorch and fused linear layers give the same result"""
 
-    # Get two cloned inputs
+    # Instantiate pytorch and fused layers, same initialization
+    torch.random.manual_seed(0)
     X = torch.normal(0, 1, size=shape, device="cuda")
     X.requires_grad_()
 
-    X_ = X.detach().clone()
-    X_.requires_grad_()
-
-    # Instantiate pytorch and fused layers, same initialization
     torch_linear = torch.nn.Linear(shape[2], shape[2] // 2, bias=bias).to("cuda")
     torch_activation = build_activation(activation)
     torch_sequence = torch.nn.Sequential(torch_linear, torch_activation)
+
+    torch.random.manual_seed(0)
+    X_ = torch.normal(0, 1, size=shape, device="cuda")
+    X_.requires_grad_()
 
     triton_fused_linear = FusedLinear(
         shape[2], shape[2] // 2, bias=bias, activation=activation
     ).to("cuda")
 
-    # Make sure that we start with the same weights
-    triton_fused_linear.weight.data = (
-        torch_linear.weight.data.clone().detach().transpose(1, 0)
-    )
-    if bias:
-        assert triton_fused_linear.bias is not None
-        triton_fused_linear.bias.data = torch_linear.bias.data.clone().detach()
-
     # Now check parity
+    torch_linear.train()
+    triton_fused_linear.train()
+
+    torch_linear.zero_grad()
+    triton_fused_linear.zero_grad()
+
+    assert torch.allclose(
+        triton_fused_linear.weight, torch_linear.weight
+    ), "Broken test setup"
+    assert torch.allclose(X, X_), "Broken test setup"
+
     with autocast(enabled=amp):
-        tolerance = 1e-4
+        tolerance = 1e-3 if not amp else 1e-2
 
         y_torch = torch_sequence(X)
         y_triton = triton_fused_linear(X_)
@@ -114,7 +124,22 @@ def test_fused_linear_parity(shape, activation: Activation, bias: bool, amp: boo
 
         assert torch.allclose(X, X_, atol=tolerance), f"{X[:,0,0]} vs. {X_[:,0,0]}"
 
-        # Grad being correct checks both the loss + the backward pass
+        # Input grad being correct checks both the loss + some of the backward pass
         assert torch.allclose(
             X.grad, X_.grad, atol=tolerance
         ), f"{X.grad[:,0,0]} vs. {X_.grad[:,0,0]}"
+
+        # Check that the linear layer bias are also properly trainable
+        if bias:
+            assert triton_fused_linear.bias is not None
+            assert triton_fused_linear.bias.grad is not None
+            assert torch.allclose(
+                torch_linear.bias.grad, triton_fused_linear.bias.grad, atol=tolerance
+            )
+
+        # Check that the linear layer weights are also properly trainable
+        assert torch.allclose(
+            torch_linear.weight.grad,
+            triton_fused_linear.weight.grad,
+            atol=tolerance,
+        )

xformers/components/activations.py
@@ -28,9 +28,17 @@ class SquaredReLU(nn.Module):
         return x_ * x_
 
 
+class Passthrough(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x
+
+
 def build_activation(activation: Optional[Activation]):
     if not activation:
-        return lambda x: x
+        return Passthrough()
 
     return {
         Activation.ReLU: nn.ReLU,

xformers/triton/fused_linear_layer.py
@@ -29,17 +29,33 @@ class _fused_linear_triton(torch.autograd.Function):
     @staticmethod
     @custom_fwd(cast_inputs=torch.float16)
     def forward(
-        ctx, x, weight, bias, activation, act_grad_kernel, save_activation_inputs
+        ctx,
+        x,
+        weight,
+        bias,
+        activation,
+        act_grad_kernel,
+        save_activation_inputs,
+        trainable_weight,
+        trainable_bias,
     ):
-        ctx.activation_grad_kernel = act_grad_kernel
 
         # Kick the fused Triton kernel, handling bias and activation in one go
         y, activation_inputs = fused_matmul(
             x, weight, bias, activation, save_activation_inputs
         )
 
-        ctx.save_for_backward(weight, activation_inputs)
+        ctx.activation_grad_kernel = act_grad_kernel
+        ctx.trainable_weight = trainable_weight
+        ctx.trainable_bias = trainable_bias
         ctx.save_activation_inputs = save_activation_inputs
+
+        # Micro-optimization: saving these is not always needed (?)
+        if x.requires_grad or ctx.trainable_weight or ctx.trainable_bias:
+            if ctx.trainable_weight:
+                ctx.save_for_backward(weight, activation_inputs, x)
+            else:
+                ctx.save_for_backward(weight, None, None)
 
         return y
 
@@ -49,18 +65,21 @@ class _fused_linear_triton(torch.autograd.Function):
         """
         Compute the derivative with respect to x, other tensors were not trainable inputs.
         """
-        (weight, activation_inputs) = ctx.saved_tensors
+        (weight, activation_inputs, inputs) = ctx.saved_tensors
 
         # Kick the fused Triton kernel, handling transpose and activation gradient in one go
-        grad_input = fused_matmul_backward(
+        grad_input, grad_weight, grad_bias = fused_matmul_backward(
             grad_out=grad_out,
+            inputs=inputs,
             weight=weight,
+            trainable_weight=ctx.trainable_weight,
+            trainable_bias=ctx.trainable_bias,
             activation_inputs=activation_inputs,
             activation_grad=ctx.activation_grad_kernel,
             activation_grad_req_inputs=ctx.save_activation_inputs,
         )
 
-        return grad_input, None, None, None, None, None
+        return grad_input, grad_weight, grad_bias, None, None, None, None, None
 
 
 class FusedLinear(nn.Module):
@@ -83,8 +102,14 @@ class FusedLinear(nn.Module):
         **_
     ):
         super().__init__()
-        self.weight = nn.Parameter(torch.empty(in_features, out_features))
-        self.bias = nn.Parameter(torch.empty(out_features)) if bias else None
+        self.weight = nn.Parameter(
+            torch.empty(out_features, in_features), requires_grad=True
+        )
+        self.bias = (
+            nn.Parameter(torch.empty(out_features), requires_grad=True)
+            if bias
+            else None
+        )
 
         self._activation_kernel = get_triton_activation_kernel(activation)
         self._activation_grad_kernel = get_triton_activation_bwd_kernel(activation)
@@ -102,14 +127,13 @@ class FusedLinear(nn.Module):
             torch.nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, x):
-        # Minor perf boost: don't save inputs if we're only doing inference
-        save_activation_inputs = self._save_activation_inputs and x.requires_grad
-
         return _fused_linear_triton.apply(
             x,
             self.weight,
             self.bias,
             self._activation_kernel,
             self._activation_grad_kernel,
-            save_activation_inputs,
+            self._save_activation_inputs,
+            self.weight.requires_grad,
+            self.bias.requires_grad if self.bias is not None else False,
         )

xformers/triton/fused_matmul_backward.py
@@ -11,13 +11,6 @@ import triton.language as tl
 
 from xformers.triton.configs_matmul import kernel_config
 
-# Credits: Initially inspired by the Triton tutorial on matrix multiplications
-
-
-@triton.jit
-def cdiv(x, y):
-    return (x + y - 1) // y
-
 
 # fmt: off
 @triton.autotune(
@@ -25,18 +18,18 @@ def cdiv(x, y):
     key=["M", "N", "K"],
 )
 @triton.jit
-def kernel_fma_grad(
-    # Pointers to matrices
-    GRAD_IN, ACT_IN, GRAD_OUT, W,
-    # Matrix dimensions
+def kernel_fma_grad_in(
+    # Pointers to all the tensors
+    GRAD_IN, GRAD_ACT, ACT_IN, GRAD_OUT, W,
+    # Tensor dimensions
     M, N, K,
-    # The stride variables represent how much to increase the ptr by when moving by 1
-    # element in a particular dimension. E.g. stride_am is how much to increase a_ptr
-    # by to get the element one row down (A has M rows)
+    # strides for all the gradients
     stride_gib, stride_gim, stride_gik,
-    stride_aib, stride_aim, stride_ain,
+    stride_gab, stride_gam, stride_gan,
     stride_gob, stride_gom, stride_gon,
-    stride_wk, stride_wn,
+    # strides for the extra data
+    stride_aib, stride_aim, stride_ain,
+    stride_wn, stride_wk,
     # Meta-parameters
     **META,
 ):
@@ -46,6 +39,7 @@ def kernel_fma_grad(
     - grad_out has shape (B, M, N)
     - W has shape (K, N)
     - grad_in has shape (B, M, K)
+    - X has shape (B, M, K)
     """
     # extract metaparameters
     BLOCK_M, GROUP_M = META["BLOCK_ROW"], META["GROUP_M"]
@@ -54,8 +48,8 @@ def kernel_fma_grad(
     # programs are grouped together to improve L2 hit rate
     pid, batch_id = tl.program_id(axis=0), tl.program_id(axis=1)
 
-    num_pid_m = cdiv(M, BLOCK_M)  # number of program ids along the M axis
-    num_pid_k = cdiv(K, BLOCK_K)  # number of programs ids along the N axis
+    num_pid_m = tl.cdiv(M, BLOCK_M)  # number of program ids along the M axis
+    num_pid_k = tl.cdiv(K, BLOCK_K)  # number of programs ids along the N axis
     num_pid_in_group = GROUP_M * num_pid_k  # number of programs in group
     group_id = pid // num_pid_in_group  # id of the group this program is in
     first_pid_m = group_id * GROUP_M  # row-id of the first program in the group
@@ -69,17 +63,14 @@ def kernel_fma_grad(
         pid % num_pid_in_group
     ) // GROUP_M  # col-id of the program in the *launch grid*
 
-    # rm (resp. rk) denotes a range of indices
-    # for rows (resp. col) of C
+    # memory ranges
     rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
     rk = pid_k * BLOCK_K + tl.arange(0, BLOCK_K)
-    mask = (rm[:, None] < M) & (rk[None, :] < K)
-
-    # rn is the range over which we'll accumulate
     rn = tl.arange(0, BLOCK_N)
 
     # memory blocks can be computed using numpy-style broadcasting
     GRAD_OUT += rm[:, None] * stride_gom + rn[None, :] * stride_gon + batch_id * stride_gob
+    GRAD_ACT += rm[:, None] * stride_gam + rn[None, :] * stride_gan + batch_id * stride_gab
     ACT_IN += rm[:, None] * stride_aim + rn[None, :] * stride_ain + batch_id * stride_aib
     GRAD_IN += rm[:, None] * stride_gim + rk[None, :] * stride_gik + batch_id * stride_gib
     W += rn[:, None] * stride_wn + rk[None, :] * stride_wk
@@ -89,13 +80,13 @@ def kernel_fma_grad(
     act_grad_fn = META["ACTIVATION_GRAD"]
 
     for _ in range(N, 0, -BLOCK_N):
-        grad_out = tl.load(GRAD_OUT)
-        w = tl.load(W)
+        grad_out = tl.load(GRAD_OUT)  # BLOCK_M x BLOCK_N
+        w = tl.load(W)  # BLOCK_N x BLOCK_K
 
         # optional fused activation gradient (while the data is in shared memory)
         if META["ACTIVATION_GRAD"]:
             if META["ACTIVATION_GRAD_REQ_INPUTS"]:
-                # This activation
+                # This activation requires its inputs
                 act_input = tl.load(ACT_IN)
                 grad_act = act_grad_fn(act_input)
                 ACT_IN += BLOCK_N * stride_ain
@@ -105,22 +96,30 @@ def kernel_fma_grad(
 
             grad_out *= grad_act.to(grad_out.dtype)
 
-        # block level matrix multiplication. this translates to MMA directly
+        # store grad_act as an intermediate, will be used for grad/weight and grad/bias
+        if META["SAVE_ACT_GRAD"]:
+            tl.store(GRAD_ACT, grad_out)
+
+        # gradient #1: input with respect to outputs
+        # grad_in is grad_out scaled by the (transposed) weight
         grad_in += tl.dot(grad_out, w)
 
-        # increment pointers so that the next blocks of A and B
-        # are loaded during the next iteration
+        # increment pointers so that the next blocks of A and B are loaded during the next iteration
         GRAD_OUT += BLOCK_N * stride_gon
+        GRAD_ACT += BLOCK_N * stride_gan
         W += BLOCK_N * stride_wn
 
     # write back result
-    tl.store(GRAD_IN, grad_in, mask=mask)  # type promotion or downgrade is automatic
+    tl.store(GRAD_IN, grad_in, mask=(rm[:, None] < M) & (rk[None, :] < K))  # type promotion or downgrade is automatic
 
 
 # Activation needs to be a triton kernel
 def fused_matmul_backward(
     grad_out: torch.Tensor,
+    inputs: torch.Tensor,
     weight: torch.Tensor,
+    trainable_weight: bool,
+    trainable_bias: bool,
     activation_inputs: Optional[torch.Tensor],
     activation_grad=None,
     activation_grad_req_inputs: bool = False,
@@ -140,15 +139,16 @@ def fused_matmul_backward(
         _should_squeeze = False
 
     assert (
-        grad_out.shape[2] == weight.shape[1]
+        grad_out.shape[2] == weight.shape[0]
     ), "Incompatible dimensions in between grad_out and weight"
 
     B, M, N = grad_out.shape
-    K, N = weight.shape
+    N, K = weight.shape
 
     grad_in = torch.empty((B, M, K), device=grad_out.device, dtype=grad_out.dtype)
+    grad_act = torch.empty_like(grad_out)
 
-    # 1D launch kernel where each block gets its own program.
+    # Compute the gradient for the inputs
     def grid(META):
         return (
             triton.cdiv(M, META["BLOCK_ROW"]) * triton.cdiv(K, META["BLOCK_COL"]),
@@ -160,26 +160,39 @@ def fused_matmul_backward(
         activation_inputs = grad_out
 
     # fmt: off
-    kernel_fma_grad[grid](
+    kernel_fma_grad_in[grid](
         # data ptrs
-        grad_in, activation_inputs, grad_out, weight,
+        grad_in, grad_act, activation_inputs, grad_out, weight,
         # shapes
         M, N, K,
         # strides
         grad_in.stride(0), grad_in.stride(1), grad_in.stride(2),
-        activation_inputs.stride(0), activation_inputs.stride(1), activation_inputs.stride(2),
+        grad_act.stride(0), grad_act.stride(1), grad_act.stride(2),
         grad_out.stride(0), grad_out.stride(1), grad_out.stride(2),
+        activation_inputs.stride(0), activation_inputs.stride(1), activation_inputs.stride(2),
         weight.stride(0), weight.stride(1),
         # optional fused activation
         ACTIVATION_GRAD=activation_grad,
         # data reuse optimization
-        GROUP_M=8,  # FIXME: autotune ?
+        GROUP_M=16,
         BLOCK_N=32,
         ACTIVATION_GRAD_REQ_INPUTS=activation_grad_req_inputs,
+        SAVE_ACT_GRAD=trainable_weight or trainable_bias
     )
     # fmt: on
+
+    grad_bias = torch.sum(grad_act, dim=[0, 1]) if trainable_bias else None
+
+    # Reuse Triton optimized matmul
+    grad_weight = None
+    if trainable_weight:
+        grad_act_ = torch.reshape(grad_act, (grad_act.shape[0]*grad_act.shape[1], grad_act.shape[2])).transpose(1, 0)
+        inputs_ = torch.reshape(inputs, (inputs.shape[0]*inputs.shape[1], inputs.shape[2]))
+        grad_weight = triton.ops.matmul(grad_act_, inputs_)
 
     if _should_squeeze:
         grad_in = grad_in.squeeze_()
 
-    return grad_in
+    del grad_act
+
+    return grad_in, grad_weight, grad_bias