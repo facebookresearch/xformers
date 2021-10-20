9691fbaa21c3615c5fcc892fcae1295921eaca8a
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-10-20 10:43:37 -0700

    Adding some hopefully reasonable unit tests

--- tests/test_triton_dropout.py
+++ tests/test_triton_dropout.py
@@ -10,14 +10,19 @@ import pytest
 import torch
 from torch.cuda.amp.autocast_mode import autocast
 
-try:
-    from xformers.triton import dropout
-    from xformers.triton.utils import gpu_capabilities_older_than_70
+_triton_available = torch.cuda.is_available()
 
-    _triton_available = True
-except ImportError:
-    logging.warning("Triton is not available, some optimizations will not be tested.")
-    _triton_available = False
+if _triton_available:
+    try:
+        from xformers.triton import dropout
+        from xformers.triton.utils import gpu_capabilities_older_than_70
+
+        _triton_available = True
+    except ImportError:
+        logging.warning(
+            "Triton is not available, some optimizations will not be tested."
+        )
+        _triton_available = False
 
 # Testing odd shapes on purpose
 SHAPES = [
@@ -44,13 +49,27 @@ def test_dropout(shape, amp):
     x = torch.normal(0, 1, size=shape, device="cuda", requires_grad=True)
 
     with autocast(enabled=amp):
-        tol = 1e-3 if amp else 1e-5  # AMP rounding causes issues, 1e-5 is the default
+        tol = 1e-2 if amp else 1e-5  # AMP rounding causes issues, 1e-5 is the default
 
+        # Check that 0 means no dropout
         y = dropout(x, p=0)
         assert torch.allclose(x.to(y.dtype), y, rtol=tol)
 
+        # Check that 1 means dropout for sure
         y = dropout(x, p=1)
         assert not torch.allclose(x.to(y.dtype), y, rtol=tol)
 
-        # FIXME - add way more tests
-        pass
+        # Check that the drops are different for every row (could catch broken seeds per row)
+        y = dropout(x, p=0.5)
+
+        y = y.flatten(0, 1) if y.ndim == 3 else y
+        assert not torch.sum(torch.eq(y[0, :] == 0.0, y[1, :] == 0.0)) == y.shape[1]
+
+        # Check that the drops are different over time, for the same line
+        y_a = dropout(x, p=0.5)
+        y_b = dropout(x, p=0.5)
+
+        y_a = y_a.flatten(0, 1) if y_a.ndim == 3 else y_a
+        y_b = y_b.flatten(0, 1) if y_b.ndim == 3 else y_b
+
+        assert not torch.sum(torch.eq(y_a[0, :] == 0.0, y_b[0, :] == 0.0)) == y.shape[1]
