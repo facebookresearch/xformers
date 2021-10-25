e19cd62271709017404038985b9eea5b61abe007
blefaudeux <blefaudeux@users.noreply.github.com>
2021-10-25 21:35:08 +0000

    deploy: f06213696d1805b7d94757e25afe6da5f80ea9f4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '0.0.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
+    BUILDER: 'singlehtml',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
