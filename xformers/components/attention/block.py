fix block and sinkhorn attn scaling bug

xformers/components/attention/block.py
@@ -1,52 +1,53 @@
+
+
+
+
+"""
+Simple non-ovelapping local block attention. To test how strong are the locality inductive bias in these tasks
+"""
+
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import logging
+import math
 from dataclasses import dataclass
 from typing import Optional
+
+import torch
+from torch import nn
+from torch import Tensor
+import torch.nn.functional as F
 from functools import partial, reduce
 from inspect import isfunction
 from operator import mul
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import Tensor
-
 from xformers.components.attention import Attention, AttentionConfig, register_attention
 
+
 @dataclass
-class SinkhornSelfAttentionConfig(AttentionConfig):
-    block_size: int
-    temperature: float
-    sinkhorm_iter: int
+class BlockAttentionConfig(AttentionConfig):
+    window_size: int
     num_heads: int
     require_key_mask: bool
 
-@register_attention("sinkhorn", SinkhornSelfAttentionConfig)
-class SinkhornAttention(Attention):
+@register_attention("block", BlockAttentionConfig)
+class BlockAttention(Attention):
     def __init__(
         self, 
         dropout: float, 
         num_heads: int,
-        block_size: int = 128, 
-        temperature: float = 0.7, 
-        sinkhorm_iter: int = 7, 
+        window_size: int = 256, # 
         *args, **kwargs
     ):
-        """
-        Sparse Sinkhorn Attention
-        https://arxiv.org/abs/2002.11296
 
-        Code largely based on https://github.com/lucidrains/sinkhorn-transformer
-
-        The paper's notation are kept wherever possible
-
-        #TODO only support encoding only settings
-        """
         super().__init__()
-        self.bucket_size = block_size
-        self.sinkhorn_iter = sinkhorm_iter
-        self.temperature = temperature
+        self.bucket_size = window_size
         self.drop_attn = nn.Dropout(dropout)
         self.num_head = num_heads
-        self.sort_net = AttentionSortNet(block_size, temperature, sinkhorm_iter)
 
     def forward(
         self, 
@@ -62,7 +63,6 @@ class SinkhornAttention(Attention):
         orig_seq_len = q.size(1)
         bsz = bh // self.num_head
         head_dim = q.size(-1)
-
 
         assert key_padding_mask is not None
         key_padding_mask = key_padding_mask.to(q)
@@ -111,7 +111,7 @@ class SinkhornAttention(Attention):
 
             selected_k[selection_padding_mask_nonzeros] = k_splited[extra_attention_mask_nonzeros]
             # (bsz, seq_len, num_heads, max_num_extra_indices_per_batch)
-            selected_attn_weights = torch.einsum('blhd,bshd->blhs', (q_splited, selected_k)) * (head_dim ** -0.5)
+            selected_attn_weights = torch.einsum('blhd,bshd->blhs', (q_splited, selected_k))
             selected_attn_weights[selection_padding_mask_zeros[0], :, :, selection_padding_mask_zeros[1]] = -10000
             attn_weights_over_g_tokens = selected_attn_weights.transpose(1,2)
 
@@ -123,15 +123,6 @@ class SinkhornAttention(Attention):
         buckets = q.shape[1] // self.bucket_size
         b_q = bucket(buckets, q)
         b_k, b_v = map(partial(bucket, buckets), (k, v)) # BH * bct * n_b * D
-
-        R = self.sort_net(q, k)
-        R = R.type_as(q).to(q)
-
-        b_k_r = reorder_buckets(b_k, R).reshape(bh, buckets, -1, head_dim) 
-        b_v_r = reorder_buckets(b_v, R).reshape(bh, buckets, -1, head_dim) 
-
-        b_k = torch.cat((b_k_r, b_k), dim=2)
-        b_v = torch.cat((b_v_r, b_v), dim=2)
 
         dots = torch.einsum('buie,buje->buij', b_q, b_k) * (head_dim ** -0.5)
         mask_value = -10000
@@ -145,26 +136,23 @@ class SinkhornAttention(Attention):
         mq, mk = bucket(buckets, q_mask), bucket(buckets, kv_mask) # B * bkt * n_b
         expand_head_and_merge_into_batch = lambda x: merge_dims(0, 1, expand_dim(x.unsqueeze(1), 1, self.num_head))
         mq, mk = map(expand_head_and_merge_into_batch, (mq, mk)) # BH * bkt * n_b
-        mk_r = batched_index_select(mk, R.abs().argmax(dim=-1))
-        mk_r = mk_r.reshape(bh, buckets, -1)
-        mk = torch.cat((mk_r, mk), dim=2)
         mask = mq[:, :, :, None] * mk[:, :, None, :]
 
         dots.masked_fill_(~mask, mask_value)
         del mask
         
-        sinkhorn_attn_weights = dots.view(bsz*self.num_head, -1, self.bucket_size*2)
+        block_attn_weights = dots.view(bsz*self.num_head, -1, self.bucket_size)
 
         attn_weights_over_g_tokens  = attn_weights_over_g_tokens.view(bsz*self.num_head, -1, max_num_extra_indices_per_batch)
-        all_attn = torch.cat([sinkhorn_attn_weights, attn_weights_over_g_tokens], dim=-1)
+        all_attn = torch.cat([block_attn_weights, attn_weights_over_g_tokens], dim=-1)
 
         all_attn_probs = all_attn.softmax(dim=-1)
         all_attn_probs = self.drop_attn(all_attn_probs)
 
         C = 0
         # calculate block attention
-        block_attn_probs = all_attn_probs[:, :, :sinkhorn_attn_weights.shape[-1]]
-        block_attn_probs = block_attn_probs.view(bsz*self.num_head, -1, self.bucket_size, 2*self.bucket_size)
+        block_attn_probs = all_attn_probs[:, :, :block_attn_weights.shape[-1]]
+        block_attn_probs = block_attn_probs.view(bsz*self.num_head, -1, self.bucket_size, self.bucket_size)
         C += block_attn_probs.matmul(b_v).view(bsz*self.num_head, -1, head_dim)
         
         if extra_attention_mask is not None:
@@ -175,7 +163,7 @@ class SinkhornAttention(Attention):
             selected_q = q_splited.new_zeros(bsz, max_num_extra_indices_per_batch, self.num_head, head_dim)
             selected_q[selection_padding_mask_nonzeros] = q_splited[extra_attention_mask_nonzeros]
 
-            g2all_attn_weights = selected_q.transpose(1,2).matmul(k_splited.permute(0,2,3,1)) * (head_dim ** -0.5)
+            g2all_attn_weights = selected_q.transpose(1,2).matmul(k_splited.permute(0,2,3,1))
             g2all_attn_weights[selection_padding_mask_zeros[0], :, selection_padding_mask_zeros[1], :] = -10000.0
 
             if hard_mask is not None:
@@ -196,10 +184,6 @@ class SinkhornAttention(Attention):
 
         C = C[:,:orig_seq_len]
         return C
-
-def batched_index_select(values, indices):
-    last_dim = values.shape[-1]
-    return values.gather(1, indices[:, :, None].expand(-1, -1, last_dim))
 
 def expand_dim(t, dim, k):
     expand_shape = [-1] * len(t.shape)
@@ -222,53 +206,7 @@ def bucket(buckets, t, dim=1):
     shape[dim:dim+1] = [buckets, -1]
     return t.reshape(*shape)
 
-def reorder_buckets(t, r):
-    return torch.einsum('buv,bvtd->butd', r, t)
-
 def unbucket(t, dim=1):
     shape = list(t.shape)
     shape[dim:dim+2] = [-1]
     return t.reshape(*shape)
-
-class AttentionSortNet(nn.Module):
-    def __init__(self, bucket_size, temperature, sinkhorn_iter):
-        super().__init__()
-        self.bucket_size = bucket_size
-        self.temperature = temperature
-        self.sinkhorn_iter = sinkhorn_iter
-
-    def forward(self, q, k, topk=1):
-        dim = q.size(-1)
-
-        buckets = q.shape[1] // self.bucket_size
-        kv_buckets = k.shape[1] // self.bucket_size
-
-        b_q = bucket(buckets, q)
-        b_k = bucket(kv_buckets, k)
-
-        sq = b_q.mean(dim=2) # TODO original paper uses sum
-        sk = b_k.mean(dim=2) 
-
-        R = torch.einsum('bie,bje->bij', sq, sk).to(q) * (dim ** -0.5)
-
-        return gumbel_sinkhorn(F.relu(R), self.sinkhorn_iter, self.temperature)
-
-def log(t, eps = 1e-6):
-    return torch.log(t + eps)
-
-def gumbel_sinkhorn(r, n_iters=8, temperature=0.7):
-    r = log(r)
-    gumbel = sample_gumbel(r.shape, r.device, r.dtype)
-    r = (r + gumbel) / temperature
-    return sinkhorn_sorting_operator(r, n_iters)
-
-def sample_gumbel(shape, device, dtype, eps=1e-6):
-    u = torch.empty(shape, device=device, dtype=dtype).uniform_(0, 1)
-    return -log(-log(u, eps), eps)
-
-def sinkhorn_sorting_operator(r, n_iters=8):
-    n = r.shape[1]
-    for _ in range(n_iters):
-        r = r - torch.logsumexp(r, dim=2, keepdim=True)
-        r = r - torch.logsumexp(r, dim=1, keepdim=True)
-    return torch.exp(r)

xformers/components/attention/lsh_global.py
@@ -109,20 +109,35 @@ class ReformerAttention(LSHSelfAttention):
 
         # @xwhan reformer needs the key and query vectors to be the same
         # project hidden_states to query_key and value
-        query_key_vectors = q.view(batch_size, self.num_attention_heads, sequence_length, head_dim)
-        value_vectors = v.view(batch_size, self.num_attention_heads, sequence_length, head_dim)
+        orig_query = q.view(batch_size, self.num_attention_heads, sequence_length, head_dim)
+        orig_value = v.view(batch_size, self.num_attention_heads, sequence_length, head_dim)
 
         do_standard_self_attention = (sequence_length <= self.chunk_length)
 
         # xformer format, att_mask: B x seq_len x seq_len #TODO: better solution?
-        if key_padding_mask is not None:
-            attention_mask = key_padding_mask == 0
-        elif att_mask is not None:
-            assert len(att_mask.shape) == 3
-            att_mask = att_mask.reshape(batch_size, self.num_heads, -1, sequence_length)[:,0,:,:]
-            attention_mask = att_mask.sum(1) > 0
+        assert key_padding_mask is not None
+        key_padding_mask = key_padding_mask.to(q)
+        key_padding_mask[:,0] = -1
+
+        # for LSH attention, unmasked tokens are True
+        attention_mask = key_padding_mask == 0 
+
+        # resolve mask that specify global tokens
+        extra_attention_mask = key_padding_mask < 0
+        num_extra_indices_per_batch = extra_attention_mask.long().sum(dim=1)
+        max_num_extra_indices_per_batch = num_extra_indices_per_batch.max()
+        if max_num_extra_indices_per_batch <= 0:
+            extra_attention_mask = None
         else:
-            attention_mask = None
+            extra_attention_mask_nonzeros = extra_attention_mask.nonzero(as_tuple=True)
+            zero_to_max_range = torch.arange(0, max_num_extra_indices_per_batch, device=extra_attention_mask.device)
+            # mask indicating which values are actually going to be padding
+            num_extra_indices_per_batch = extra_attention_mask.long().sum(dim=1)
+            selection_padding_mask = zero_to_max_range < num_extra_indices_per_batch.unsqueeze(dim=-1)
+            # 2) location of the non-padding values in the selected global attention
+            selection_padding_mask_nonzeros = selection_padding_mask.nonzero(as_tuple=True)
+            # 3) location of the padding values in the selected global attention
+            selection_padding_mask_zeros = (selection_padding_mask == 0).nonzero(as_tuple=True)
 
         # LSH attention only makes sense if chunked attention should be performed
         if not do_standard_self_attention:
@@ -131,7 +146,7 @@ class ReformerAttention(LSHSelfAttention):
                 self._set_num_buckets(sequence_length)
 
             # hash query key vectors into buckets
-            buckets = self._hash_vectors(query_key_vectors, num_hashes, attention_mask)
+            buckets = self._hash_vectors(orig_query, num_hashes, attention_mask)
 
             assert (
                 int(buckets.shape[-1]) == num_hashes * sequence_length
@@ -144,9 +159,12 @@ class ReformerAttention(LSHSelfAttention):
             # make sure bucket idx is not longer then sequence length
             sorted_bucket_idx_per_hash = sorted_bucket_idx % sequence_length
 
+            # query_key_vectors: (B, H, L, D)
+            # sorted_bucket_idx_per_hash: (B. H, L x num_hash)
             # cluster query key value vectors according to hashed buckets
-            query_key_vectors = self._gather_by_expansion(query_key_vectors, sorted_bucket_idx_per_hash, num_hashes)
-            value_vectors = self._gather_by_expansion(value_vectors, sorted_bucket_idx_per_hash, num_hashes)
+            query_key_vectors = self._gather_by_expansion(orig_query, sorted_bucket_idx_per_hash, num_hashes)
+            value_vectors = self._gather_by_expansion(orig_value, sorted_bucket_idx_per_hash, num_hashes)
+            # (B. H, L x num_hash, D)
             query_key_vectors = self._split_seq_length_dim_to(
                 query_key_vectors,
                 -1,
@@ -161,6 +179,7 @@ class ReformerAttention(LSHSelfAttention):
                 self.num_attention_heads,
                 self.attention_head_size,
             )
+            # (B. H, (L x num_hash) // chunk, chunk, D)
 
             if self.chunk_length is None:
                 assert (
@@ -169,11 +188,14 @@ class ReformerAttention(LSHSelfAttention):
 
         else:
             # get sequence length indices
-            sorted_bucket_idx_per_hash = torch.arange(sequence_length, device=query_key_vectors.device).repeat(
+            sorted_bucket_idx_per_hash = torch.arange(sequence_length, device=orig_query.device).repeat(
                 batch_size, self.num_attention_heads, 1
             )
+            query_key_vectors = orig_query
+            value_vectors = orig_value
 
         # scale key vectors
+        # why length normalization
         key_vectors = self._len_and_dim_norm(query_key_vectors)
 
         # set query_vectors to query key vectors if LSH self attention
@@ -181,6 +203,17 @@ class ReformerAttention(LSHSelfAttention):
 
         # free memory
         del query_key_vectors
+
+        # global tokens' key and value vectors with indexing
+        if extra_attention_mask is not None:
+            selected_k = orig_query.new_zeros(batch_size, max_num_extra_indices_per_batch, self.num_attention_heads, head_dim)
+            orig_query_t = orig_query.transpose(1,2)
+            selected_k[selection_padding_mask_nonzeros] = orig_query_t[extra_attention_mask_nonzeros]
+            selected_k = self._len_and_dim_norm(selected_k)
+
+            orig_value_t = orig_value.transpose(1,2)
+            selected_v = orig_value.new_zeros(batch_size, max_num_extra_indices_per_batch, self.num_attention_heads, head_dim)
+            selected_v[selection_padding_mask_nonzeros] = orig_value_t[extra_attention_mask_nonzeros]
 
         # get attention probs
         out_vectors, logits, attention_probs = self.attend(
@@ -191,8 +224,12 @@ class ReformerAttention(LSHSelfAttention):
             attention_mask=attention_mask,
             head_mask=None,
             do_standard_self_attention=do_standard_self_attention,
-            do_cached_attention=False
+            do_cached_attention=False,
+            global_token_keys=selected_k,
+            global_token_values=selected_v,
+            selection_padding_mask_zeros=selection_padding_mask_zeros
         )
+
 
         # free memory
         del key_vectors, value_vectors
@@ -220,6 +257,8 @@ class ReformerAttention(LSHSelfAttention):
                     self.attention_head_size,
                 ).unsqueeze(-1)
 
+                # TODO @xwhan How is logits used here? looks wired as logits is already logsumexp
+
                 probs_vectors = torch.exp(logits - torch.logsumexp(logits, dim=2, keepdim=True))
                 out_vectors = torch.sum(out_vectors * probs_vectors, dim=2)
                 # free memory
@@ -227,6 +266,30 @@ class ReformerAttention(LSHSelfAttention):
 
             # free memory
             del logits
+
+            # replace the calculation of global tokens attending to all other tokens
+            selected_q = orig_query.new_zeros(batch_size, max_num_extra_indices_per_batch, self.num_attention_heads, head_dim)
+            selected_q[selection_padding_mask_nonzeros] = orig_query_t[extra_attention_mask_nonzeros]
+            # LSH attention, query and keys are the same
+            # need to do some normalization like LSH attention
+            g2all_attn_weights = selected_q.transpose(1,2).matmul(self._len_and_dim_norm(orig_query).transpose(-1, -2))
+            hard_mask = key_padding_mask == 1
+            g2all_attn_weights[selection_padding_mask_zeros[0], :, selection_padding_mask_zeros[1], :] = -10000.0
+            if hard_mask is not None:
+                g2all_attn_weights = g2all_attn_weights.masked_fill(
+                    hard_mask.unsqueeze(1).unsqueeze(2),
+                    -10000.0,
+                )
+            # still need to avoid the model attend to itself
+            g2all_attn_weights[extra_attention_mask_nonzeros[0],:,:,extra_attention_mask_nonzeros[1]] = -10000.0
+            g2all_attn_probs_float = F.softmax(g2all_attn_weights, dim=-1, dtype=torch.float32)
+            g2all_attn_probs = nn.functional.dropout(g2all_attn_probs_float.type_as(g2all_attn_weights), p=self.dropout, training=self.training)
+            g2all_attn = g2all_attn_probs.matmul(orig_value)
+            # (B, H, max_num_extra_indices_per_batch, L)
+            nonzero_global_attn = g2all_attn[selection_padding_mask_nonzeros[0], :, selection_padding_mask_nonzeros[1]].type_as(out_vectors)
+
+            out_vectors[extra_attention_mask_nonzeros[0], :, extra_attention_mask_nonzeros[1]] = nonzero_global_attn
+            # still need to avoid the model attend to itself
 
         assert out_vectors.shape == (
             batch_size,
@@ -249,6 +312,9 @@ class ReformerAttention(LSHSelfAttention):
         head_mask,
         do_standard_self_attention,
         do_cached_attention,
+        global_token_keys=None,
+        global_token_values=None,
+        selection_padding_mask_zeros=None
     ):
         # look at previous and following chunks if chunked attention
         if not do_standard_self_attention:
@@ -256,12 +322,11 @@ class ReformerAttention(LSHSelfAttention):
             value_vectors = self._look_adjacent(value_vectors, self.num_chunks_before, self.num_chunks_after)
 
 
-        # get logits and dots
-        # (BS, NumAttn, NumHash x NumChunk, Chunk_L x Hidden),(BS, NumAttn, NumHash x NumChunk, Chunk_L * (Num_bef + Num_aft + 1) x Hidden) -> (BS, NumAttn, NumHash x NumChunk, Chunk_L, Chunk_L * (1 + Num_bef + Num_aft))
         query_key_dots = torch.matmul(query_vectors, key_vectors.transpose(-1, -2))
+        # (B, H, (L x num_hash) // chunk, chunk, 2*chunk)
 
         # free memory
-        del query_vectors, key_vectors
+        # del query_vectors, key_vectors
 
         # if chunked attention split bucket idxs to query and key
         if not do_standard_self_attention:
@@ -282,6 +347,7 @@ class ReformerAttention(LSHSelfAttention):
         else:
             query_bucket_idx = key_value_bucket_idx = sorted_bucket_idx_per_hash
 
+
         # get correct mask values depending on precision
         if query_key_dots.dtype == torch.float16:
             self_mask_value = self.self_mask_value_float16.half()
@@ -291,6 +357,7 @@ class ReformerAttention(LSHSelfAttention):
             mask_value = self.mask_value_float32
 
         if not do_cached_attention:
+            # (B, H, (L x num_hash) // chunk, chunk, 2 x chunk)
             mask = self._compute_attn_mask(
                 query_bucket_idx,
                 key_value_bucket_idx,
@@ -315,8 +382,6 @@ class ReformerAttention(LSHSelfAttention):
         # to forbid a token from attending to itself, except in situations
         # where a token has no other valid attention targets (e.g. the first token in a sequence) "
 
-        breakpoint()
-
         self_mask = torch.ne(query_bucket_idx.unsqueeze(-1), key_value_bucket_idx.unsqueeze(-2)).to(
             query_bucket_idx.device
         )
@@ -324,12 +389,24 @@ class ReformerAttention(LSHSelfAttention):
         # apply self_mask
         query_key_dots = torch.where(self_mask, query_key_dots, self_mask_value)
 
+        if not do_standard_self_attention:
+            # @xwhan trying to attend global tokens here
+            global_token_keys = global_token_keys.transpose(1,2).unsqueeze(2).repeat(1,1,query_vectors.shape[2],1,1)
+            query2global_dots = torch.matmul(query_vectors, global_token_keys.transpose(-1, -2))
+            query2global_dots[selection_padding_mask_zeros[0],:,:,:,selection_padding_mask_zeros[1]] = self_mask_value
+            query_key_dots = torch.cat([query_key_dots, query2global_dots], dim=-1)
+            global_token_values = global_token_values.transpose(1,2).unsqueeze(2).repeat(1,1,query_vectors.shape[2],1,1)
+            value_vectors = torch.cat([value_vectors, global_token_values], dim=3)
+
         # free memory
         del self_mask
 
         logits = torch.logsumexp(query_key_dots, dim=-1, keepdim=True)
         # dots shape is `[batch_size, num_attn_heads, num_hashes * seq_len // chunk_length, chunk_length, chunk_length * (1 + num_chunks_before + num_chunks_after)]`
-        attention_probs = torch.exp(query_key_dots - logits)
+        # attention_probs = torch.exp(query_key_dots - logits)
+
+        # Is this causing training issues while using fp16?
+        attention_probs = F.softmax(query_key_dots, dim=-1, dtype=torch.float32).type_as(query_key_dots)
 
         # free memory
         del query_key_dots
@@ -341,10 +418,9 @@ class ReformerAttention(LSHSelfAttention):
         if head_mask is not None:
             attention_probs = attention_probs * head_mask
 
-        # breakpoint()
-
         # attend values
         out_vectors = torch.matmul(attention_probs, value_vectors)
+
 
         # free memory
         del value_vectors

xformers/components/attention/nystrom.py
@@ -53,7 +53,7 @@ class NystromAttention(Attention):
         self,
         dropout: float,
         num_heads: int,
-        num_landmarks: int = 64,
+        num_landmarks: int = 256,
         landmark_pooling: Optional[nn.Module] = None,
         causal: bool = False,
         use_razavi_pinverse: bool = True,