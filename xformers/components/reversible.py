0ae331affe5665ec197ec8fcb8476d41e084239b
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2021-11-01 10:35:39 -0700

    [feat] add split_dim arg to reversible, remove retain_grad, add benchmark_reversible (#45)
    * perf(revnet): remove/fuse cat-split
    
    * style(reversible): run pre-commit, remove unused variables
    
    * feat(tbenchmark): add basic revnet time benchmark
    
    * style(benchmark): run black over revnet bench
    
    * feat(reversible): make split dim a parameter
    
    * revert(reversible): readd original code/remove split fusion
    
    * feat(reversible): readd split dim arg
    
    * perf(reversible): remove retain graph
    
    It's not needed but makes the execution up to 20% slower.
    
    * style(reversible): remove one singular newline
    seriously?

--- xformers/components/reversible.py
+++ xformers/components/reversible.py
@@ -52,10 +52,11 @@ class Deterministic(nn.Module):
 
 
 class ReversibleBlock(nn.Module):
-    def __init__(self, f: nn.Module, g: nn.Module):
+    def __init__(self, f: nn.Module, g: nn.Module, split_dim: int = -1):
         super().__init__()
         self.f = Deterministic(f)
         self.g = Deterministic(g)
+        self.split_dim = split_dim
 
     def forward(self, x: torch.Tensor, f_args={}, g_args={}):
         x1, x2 = torch.chunk(x, 2, dim=-1)
@@ -65,13 +66,13 @@ class ReversibleBlock(nn.Module):
             y1 = x1 + self.f(x2, record_rng=self.training, **f_args)
             y2 = x2 + self.g(y1, record_rng=self.training, **g_args)
 
-        return torch.cat([y1, y2], dim=-1)
+        return torch.cat([y1, y2], dim=self.split_dim)
 
     def backward_pass(self, y: torch.Tensor, dy: torch.Tensor, f_args={}, g_args={}):
-        y1, y2 = torch.chunk(y, 2, dim=-1)
+        y1, y2 = torch.chunk(y, 2, dim=self.split_dim)
         del y
 
-        dy1, dy2 = torch.chunk(dy, 2, dim=-1)
+        dy1, dy2 = torch.chunk(dy, 2, dim=self.split_dim)
         del dy
 
         with torch.enable_grad():
@@ -90,7 +91,7 @@ class ReversibleBlock(nn.Module):
         with torch.enable_grad():
             x2.requires_grad = True
             fx2 = self.f(x2, set_rng=True, **f_args)
-            torch.autograd.backward(fx2, dx1, retain_graph=True)
+            torch.autograd.backward(fx2, dx1)
 
         with torch.no_grad():
             x1 = y1 - fx2
@@ -100,8 +101,8 @@ class ReversibleBlock(nn.Module):
             del dy2
             x2.grad = None
 
-            x = torch.cat([x1, x2.detach()], dim=2)
-            dx = torch.cat([dx1, dx2], dim=2)
+            x = torch.cat([x1, x2.detach()], dim=self.split_dim)
+            dx = torch.cat([dx1, dx2], dim=self.split_dim)
 
         return x, dx
 
