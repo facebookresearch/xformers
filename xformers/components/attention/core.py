[Fix] the causal handling for SDP (#68)
* Fixing the causal handling
* converting on the fly to additive mask

xformers/components/attention/core.py
@@ -80,13 +80,20 @@ def _matmul_with_mask(
         if mask.is_sparse:
             # perform broadcasting if needed
             mask = _broadcast_batch(mask, a.shape[0])
+
             # coalesced is not implemented for bool tensors, so need to cast
             mask = mask.to(dtype=a.dtype)  # type: ignore  # mypy is missing the catch above
+
         return torch.ops.xformers.matmul_with_mask(a, b, mask)
 
     # Non optimized codepath
     att = a @ b
-    att[~mask] = float("-inf")
+    if mask.dtype == torch.bool:
+        # mask is presumed false == ignore
+        att[~mask] = float("-inf")
+    else:
+        # mask is presumed additive
+        att += mask
     return att
 
 

xformers/components/attention/scaled_dot_product.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
+from dataclasses import dataclass
 from typing import Optional
 
 import torch
@@ -12,7 +13,14 @@ from xformers.components.attention import Attention, AttentionConfig, register_a
 from xformers.components.attention.core import scaled_dot_product_attention
 
 
-@register_attention("scaled_dot_product", AttentionConfig)
+@dataclass
+class ScaledDotProductConfig(AttentionConfig):
+    causal: Optional[bool]
+    seq_len: Optional[int]
+    to_seq_len: Optional[int]
+
+
+@register_attention("scaled_dot_product", ScaledDotProductConfig)
 class ScaledDotProduct(Attention):
     r"""
     Implementing the Scaled Dot-Product attention proposed in
@@ -62,7 +70,22 @@ class ScaledDotProduct(Attention):
         """
         # Mask-aware attention
         if self.mask is not None:
-            att_mask = self.mask if att_mask is None else self.mask & att_mask
+            self.mask = self.mask.to(q.dtype)
+
+            if att_mask is not None:
+                if att_mask.ndim == 2:
+                    att_mask.unsqueeze_(0)
+
+                if att_mask.dtype == torch.bool:
+                    # bool mask
+                    att_mask_ = self.mask
+                    att_mask_[~att_mask] = float("-inf")
+                    att_mask = att_mask_
+                else:
+                    # additive mask
+                    att_mask = self.mask + att_mask
+
+                att_mask = att_mask.to(q.dtype)
 
         # Self-attend: (B x nh, S, hs) x (B x nh, hs, S) -> (B x nh, S, S)
         y = scaled_dot_product_attention(