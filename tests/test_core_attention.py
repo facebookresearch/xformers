[fix] Nystrom + microGPT + some additive masking (#75)
* Now running

* Fixing the sparse case

* changelog update

* fixing the additive mask test, this now works actually :)

* docstrings

* removing the mask mul test, passing because both sparse and dense do the same thing, but it's not correct

* code review, fixing a small mypy issue

tests/test_core_attention.py
@@ -30,7 +30,7 @@ def test_core_attention():
 def test_core_attention_mask_types():
 
     b, s, d = 8, 900, 32
-    prob = 0.5
+    prob = 0.8  # make sure that we trigger the sparse kernels
 
     a = torch.rand(b, s, d)
     mask = torch.rand(b, s, s) > prob
@@ -40,12 +40,6 @@ def test_core_attention_mask_types():
     r_sparse_bool = scaled_dot_product_attention(a, a, a, mask.to_sparse())
     assert torch.allclose(r_dense_bool, r_sparse_bool)
 
-    # Test multiplicative float mask. Mask of 0's and 1's.
-    float_mask_mult = mask.to(dtype=torch.float)
-    r_dense_mult = scaled_dot_product_attention(a, a, a, float_mask_mult)
-    r_sparse_mult = scaled_dot_product_attention(a, a, a, float_mask_mult.to_sparse())
-    assert torch.allclose(r_dense_mult, r_sparse_mult)
-
     # Test additive mask. Mask of 0's and -infs.
     float_mask_add = torch.zeros_like(mask, dtype=torch.float)
     float_mask_add = float_mask_add.masked_fill(mask, float("-inf"))
@@ -53,8 +47,8 @@ def test_core_attention_mask_types():
     r_dense_add = scaled_dot_product_attention(a, a, a, float_mask_add)
     r_sparse_add = scaled_dot_product_attention(a, a, a, float_mask_add.to_sparse())
 
-    # Fails because all nans returned.
-    assert not torch.allclose(r_dense_add, r_sparse_add)
+    # Now properly handled
+    assert torch.allclose(r_dense_add, r_sparse_add)
 
 
 @pytest.mark.parametrize("device", _devices)

xformers/components/attention/core.py
@@ -17,7 +17,7 @@ if _is_sparse_available:
     from ._sputnik_sparse import SparseCS
 
 # NOTE: Could do with a better option on when to use triton and not
-_use_triton = True
+_use_triton = torch.cuda.is_available()
 if _use_triton:
     try:
         from xformers.triton.softmax import softmax as triton_softmax
@@ -195,8 +195,12 @@ def scaled_query_key_softmax(
 
     # Self-attend: (N, S, hs) x (N, hs, S) -> (N, S, S)
     q = q / math.sqrt(k.size(-1))
-    att = _matmul_with_mask(q, k.transpose(-2, -1), att_mask)
 
+    # Matmul with mask, if boolean
+    is_bool_mask = att_mask is not None and att_mask.dtype == torch.bool
+    att = _matmul_with_mask(q, k.transpose(-2, -1), att_mask if is_bool_mask else None)
+
+    # Could also be that the mask was additive
     if att_mask is not None and att_mask.dtype != torch.bool:
         att = att + att_mask
 
@@ -219,20 +223,31 @@ def scaled_dot_product_attention(
         or (att_mask is not None and att_mask.is_sparse)
     )
 
-    if att_mask is not None and q.shape[-2] < att_mask.shape[0]:
-        # The sequence is smaller than the mask
+    # Try to handle a case where the sequence is smaller than the mask
+    if (
+        att_mask is not None
+        and q.shape[-2] == k.shape[-2]
+        and q.shape[-2] < att_mask.shape[0]
+    ):
         seq = q.shape[-2]
+        if att_mask.ndim == 2:
 
-        if not att_mask.is_sparse:
-            att_mask = att_mask[:seq, :seq]
+            if not att_mask.is_sparse:
+                att_mask = att_mask[:seq, :seq]
+            else:
+                logging.warning(
+                    "Mismatching attention mask and sequence length. On the fly correction but this will be slow"
+                )
+                # Loosing sparsity on purpose,
+                # expectation is that moving back and forth dense/sparse will negate the speedup
+                att_mask = att_mask.to_dense().squeeze(0)[:seq, :seq]
         else:
-            logging.warning(
-                "Mismatching attention mask and sequence length. On the fly correction but this will be slow"
-            )
-            # Loosing sparsity on purpose,
-            # expectation is that moving back and forth dense/sparse will negate the speedup
-            att_mask = att_mask.to_dense().squeeze(0)[:seq, :seq]
+            assert (
+                not att_mask.is_sparse
+            ), "Sparse masks with a batch dimension are not supported for now"
+            att_mask = att_mask[:, :seq, :seq]
 
+    # The actual attention
     with torch.cuda.amp.autocast(enabled=False) if autocast_disabled else nullcontext():
         if autocast_disabled:
             q, k, v = q.float(), k.float(), v.float()