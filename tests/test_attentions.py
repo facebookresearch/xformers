fixing causal mask being skipped

tests/test_attentions.py
@@ -29,7 +29,15 @@ GLOBAL_ATTENTION_RATIO = (
 assert ATTENTION_REGISTRY.keys(), "Attention layers should have been registered"
 
 
-def _get_multihead(attention_name, attn_dropout, res_dropout, causal, heads, device):
+def _get_multihead(
+    attention_name,
+    attn_dropout,
+    res_dropout,
+    causal,
+    heads,
+    device,
+    skip_output_projection=False,
+):
     test_config = {
         "name": attention_name,
         "dropout": attn_dropout,
@@ -40,6 +48,13 @@ def _get_multihead(attention_name, attn_dropout, res_dropout, causal, heads, dev
         "num_heads": heads,
         "dim_head": MODEL / heads,
     }
+
+    if skip_output_projection:
+
+        def noop(x):
+            return x
+
+        test_config["out_proj"] = noop
 
     # Add some blocksparse layout to test the corresponding attention
     block_size = 16
@@ -81,7 +96,9 @@ def test_order_invariance(
     )
 
     # Check that a shuffled input produces the same results
-    seqs = [SEQ, SEQ - 16] if attention_name != "blocksparse" else [SEQ]
+    seqs = (
+        [SEQ, SEQ - 16] if (attention_name != "blocksparse" and not causal) else [SEQ]
+    )
 
     for seq in seqs:
         # Check that we can pass a smaller sequence
@@ -169,6 +186,60 @@ def test_different_kq_dimensions(
 
     res = multi_head(query=q, key=k, value=v)
     assert res.shape == torch.Size([BATCH, seq_q, MODEL])
+
+
+@pytest.mark.parametrize("heads", [1, 4])
+@pytest.mark.parametrize("attention_name", ["scaled_dot_product"])
+@pytest.mark.skipif(torch.cuda.is_available(), reason="requires a CUDA gpu")
+def test_scaled_dot_product_causal(
+    attention_name: str,
+    heads: int,
+):
+    """
+    Make sure that the causal flag is respected.
+    The input data is orthogonal by design if causal is respected, but if the attention looks ahead this will fail
+    """
+
+    device = torch.device("cuda")
+
+    multi_head = _get_multihead(
+        attention_name,
+        0.0,
+        0.0,
+        causal=True,
+        heads=heads,
+        device=device,
+        skip_output_projection=True,
+    )
+
+    k = (
+        torch.tril(torch.ones((SEQ, SEQ), device=device), diagonal=0)
+        .unsqueeze(0)
+        .expand(1, -1, -1)
+    )
+    q = (
+        torch.triu(torch.ones((SEQ, SEQ), device=device), diagonal=1)
+        .unsqueeze(0)
+        .expand(1, -1, -1)
+    )
+    v = (
+        torch.arange(SEQ, device=device)
+        .float()
+        .unsqueeze(0)
+        .unsqueeze(-1)
+        .expand(1, -1, SEQ)
+    )
+
+    # Make sure that we don´t project, to keep the embeddings orthogonal
+    multi_head.attention.requires_input_projection = False
+
+    res = multi_head(query=q, key=k, value=v).squeeze(0)
+
+    # Consolidate along the embedding, if causal was respected the amplitude should be sorted already
+    res_sum = torch.sum(res, dim=1)
+    assert torch.allclose(torch.sort(res_sum)[0], res_sum) or torch.allclose(
+        torch.sort(res_sum, descending=True)[0], res_sum
+    ), res_sum
 
 
 # TODO: way more unit tests..

tests/test_favor.py
@@ -66,7 +66,7 @@ def _plot_distribution(ortho_feature_map):
 
 def _get_rng_data(device):
     emb = 10
-    batch_size = 1
+    batch_size = 2
     seq_len = 20
     num_heads = 1
 
@@ -125,7 +125,7 @@ def test_feature_map_redraw():
 @pytest.mark.parametrize("causal", [True, False])
 @pytest.mark.parametrize("normalize_inputs", [True, False])
 @pytest.mark.parametrize("device", [_device])
-def test_approximation_accuracy(feature, causal, normalize_inputs, device):
+def test_favor_approximation_accuracy(feature, causal, normalize_inputs, device):
     # Run two attentions in parallel, the normal scaled dot product and the favor approximation
 
     torch.random.manual_seed(0)
@@ -134,6 +134,9 @@ def test_approximation_accuracy(feature, causal, normalize_inputs, device):
         _get_rng_data(device),
         _get_rng_data(device),
     )
+
+    for x in (query, key, value):
+        x.requires_grad = True
 
     # Build the two attention heads
     sdp_attention = ScaledDotProduct(dropout=0.0, causal=causal).to(device)
@@ -159,6 +162,9 @@ def test_approximation_accuracy(feature, causal, normalize_inputs, device):
             assert mismatch < 0.5
         else:
             assert mismatch < 0.23
+
+        # Check trainability
+        torch.sum(approx_attention_result).backward()
 
 
 if __name__ == "__main__":

xformers/components/attention/favor.py
@@ -24,6 +24,7 @@ from xformers.components.attention.feature_maps import (
 
 @dataclass
 class FavorAttentionConfig(AttentionConfig):
+    causal: Optional[bool]
     dim_features: Optional[int] = None  # The dimensions of the random features
     dim_head: Optional[
         int
@@ -109,33 +110,28 @@ class FavorAttention(Attention):
     def _causal_attention(
         k_prime: torch.Tensor, q_prime: torch.Tensor, v: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        # TODO(@lefaudeux): Rewrite as suggested in
-        # https://github.com/fairinternal/xformers/pull/61#discussion_r628420093
         # Algorithm 1 in the paper
-        ref_v = torch.ones_like(v[:, 0, :].unsqueeze(1))
+        ref_v = torch.ones_like(v.unsqueeze(2))  # BATCH x SEQ x 1 x EMB
+        Gps = k_prime.unsqueeze(3) * v.unsqueeze(2)
+        Grenorm = k_prime.unsqueeze(3) * ref_v
 
-        Gps = k_prime[:, 0, :].unsqueeze(2) * v[:, 0, :].unsqueeze(1)
-        Grenorm = k_prime[:, 0, :].unsqueeze(2) * ref_v
+        # Consolidate against the feature dimension
+        att_raw = torch.einsum("bcfe,bcf->bce", Gps, q_prime)
+        att_norm = torch.einsum("bcfe,bcf->bce", Grenorm, q_prime)
 
-        att_raw = [torch.bmm(q_prime[:, 0, :].unsqueeze(1), Gps)]
-        att_norm = [torch.bmm(q_prime[:, 0, :].unsqueeze(1), Grenorm)]
+        # Cumulative sum over the sequence
+        att_raw = att_raw.cumsum(2)
+        att_norm = att_norm.cumsum(2)
 
-        for i in range(1, k_prime.shape[1]):
-            Gps += k_prime[:, i, :].unsqueeze(2) * v[:, i, :].unsqueeze(1)
-            Grenorm += k_prime[:, i, :].unsqueeze(2) * ref_v
-
-            att_raw.append(torch.bmm(q_prime[:, i, :].unsqueeze(1), Gps))
-            att_norm.append(torch.bmm(q_prime[:, i, :].unsqueeze(1), Grenorm))
-
-        return torch.cat(att_raw, dim=1), torch.cat(att_norm, dim=1)
+        return att_raw, att_norm
 
     def forward(
         self,
         q: torch.Tensor,
         k: torch.Tensor,
         v: torch.Tensor,
-        *args,
-        **kwargs,
+        *_,
+        **__,
     ):
 
         # Project key and queries onto the feature map space