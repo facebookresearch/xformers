9499a7a210a3ec24bdfad8cd61ce050b7990697c
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-11-16 16:09:33 -0800

    adding profiling

--- xformers/benchmarks/benchmark_vit_timm.py
+++ xformers/benchmarks/benchmark_vit_timm.py
@@ -11,14 +11,18 @@
 
 # Orignal author: Sean Naren
 
+import argparse
 import math
 from enum import Enum
+from typing import Optional
 
 import pytorch_lightning as pl
 import torch
-from pl_bolts.datamodules import CIFAR10DataModule
+import torchvision
+from pytorch_lightning.profiler import AdvancedProfiler, PyTorchProfiler
 from timm.models.vision_transformer import VisionTransformer as TimmVisionTransformer
 from torch import nn
+from torch.utils.data import DataLoader, random_split
 from torchmetrics import Accuracy
 from torchvision import transforms
 
@@ -28,6 +32,77 @@ from xformers.factory import xFormer, xFormerConfig
 class Classifier(str, Enum):
     GAP = "gap"
     TOKEN = "token"
+
+
+class FakeDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        batch_size: int,
+        pic_size: int = 224,
+        num_samples: int = 1000,
+        num_classes=10,
+        num_workers: int = 2,
+        pin_memory: bool = True,
+    ):
+        super().__init__()
+
+        self.transform = torchvision.transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+        )
+        self.dataset = torchvision.datasets.FakeData(
+            size=num_samples,
+            image_size=(3, pic_size, pic_size),
+            num_classes=num_classes,
+            transform=self.transform,
+        )
+        self.num_samples = num_samples
+        self.pic_size = pic_size
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+
+    def setup(self, stage: Optional[str] = None):
+        if stage == "fit" or stage is None:
+            train_samples = int(self.num_samples * 0.9)
+            val_samples = self.num_samples - train_samples
+
+            self.data_train, self.data_val = random_split(
+                self.dataset, [train_samples, val_samples]
+            )
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.data_test = torchvision.datasets.FakeData(
+                size=int(self.num_samples * 0.1),
+                image_size=(3, self.pic_size, self.pic_size),
+                num_classes=self.num_classes,
+                transform=self.transform,
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.data_train,
+            batch_size=32,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.data_val,
+            batch_size=32,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.data_test,
+            batch_size=32,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
 
 
 class VisionTransformer(pl.LightningModule):
@@ -231,60 +306,90 @@ class VisionTransformer(pl.LightningModule):
         self.evaluate(batch, "val")
 
     def test_step(self, batch, batch_idx):
-        self.evaluate(batch, "test")
+        pass
 
 
 if __name__ == "__main__":
+    # Get the user requests
+    parser = argparse.ArgumentParser("Benchmark xformers and timm ViTs")
+    parser.add_argument(
+        "-timm",
+        "--timm",
+        help="Use the ViT implementation from Timm",
+        action="store_true",
+        default=False,
+    )
+
+    parser.add_argument(
+        "-profile",
+        "--profile",
+        help="Pofile the runtime and memory",
+        action="store_true",
+        default=False,
+    )
+
+    parser.add_argument(
+        "-chrome_trace",
+        "--chrome_trace",
+        help="Pofile the runtime and memory using a chrome trace",
+        action="store_true",
+        default=False,
+    )
+
+    args = parser.parse_args()
+
     pl.seed_everything(42)
-    BATCH_SIZE = 512
-    MAX_EPOCHS = 10
+    BATCH_SIZE = 16
+    MAX_EPOCHS = 1
     NUM_WORKERS = 4
     GPUS = 1
+    IMG_SIZE = 224
+    NUM_SAMPLES = 128
+    NUM_CLASSES = 10
+    N_LAYERS = 4
 
-    train_transforms = transforms.Compose(
-        [
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ]
-    )
-
-    test_transforms = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ]
-    )
-
-    # We'll use a datamodule here, which already handles dataset/dataloader/sampler
-    # See https://pytorchlightning.github.io/lightning-tutorials/notebooks/lightning_examples/cifar10-baseline.html
-    # for a full tutorial
-    dm = CIFAR10DataModule(
-        data_dir="data",
+    # We'll use a dummy datamodule here, which already handles dataset/dataloader/sampler
+    dm = FakeDataModule(
+        pic_size=IMG_SIZE,
+        num_samples=NUM_SAMPLES,
+        num_classes=NUM_CLASSES,
         batch_size=BATCH_SIZE,
         num_workers=NUM_WORKERS,
         pin_memory=True,
-        train_transforms=train_transforms,
-        test_transforms=test_transforms,
-        val_transforms=test_transforms,
     )
-
-    image_size = dm.size(-1)  # 32 for CIFAR
-    num_classes = dm.num_classes  # 10 for CIFAR
 
     # compute total number of steps
     batch_size = BATCH_SIZE * GPUS
     steps = dm.num_samples // batch_size * MAX_EPOCHS
     lm = VisionTransformer(
         steps=steps,
-        image_size=image_size,
-        num_classes=num_classes,
+        image_size=IMG_SIZE,
+        num_classes=NUM_CLASSES,
         attention="scaled_dot_product",
-        use_timm=False,
+        use_timm=args.timm,
+        n_layer=N_LAYERS,
+        patch_size=16,
     )
+    profiler = (
+        PyTorchProfiler(
+            filename="timm_trace" if args.timm else "xFormers_trace",
+            export_to_chrome=True,
+            record_functions={"training_step_and_backward"},
+        )
+        if args.profile and args.chrome_trace
+        else AdvancedProfiler(
+            filename="timm_profile" if args.timm else "xFormers_profile",
+        )
+        if args.profile
+        else None
+    )
+
     trainer = pl.Trainer(
-        gpus=GPUS, max_epochs=MAX_EPOCHS, detect_anomaly=True, precision=16
+        gpus=GPUS,
+        max_epochs=MAX_EPOCHS,
+        detect_anomaly=True,
+        precision=16,
+        profiler=profiler,
     )
     trainer.fit(lm, dm)
 
