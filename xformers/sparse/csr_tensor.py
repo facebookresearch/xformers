ffd8397430b6f6b6bc24cb125b50b948b926aa2a
Francisco Massa <fvsmassa@gmail.com>
2021-11-26 11:22:01 +0100

    Start using Tensor subclasses

---
+++ xformers/sparse/csr_tensor.py
@@ -0,0 +1,332 @@
+import torch
+from .utils import _diffsort, _get_transpose_info, _transpose_with_info, _csr_to_coo
+from . import _csr_ops
+
+
+def masked_matmul(a, b, mask=None):
+    if torch.overrides.has_torch_function((a, b, mask)):
+        return torch.overrides.handle_torch_function(masked_matmul, (a, b, mask), a, b, mask)
+
+    att = a @ b
+
+    if mask is None:
+        return att
+
+    if mask.dtype == torch.bool:
+        if mask.ndim == 2:
+            mask = mask.unsqueeze(0).expand(att.shape[0], -1, -1)
+        # mask is presumed false == ignore
+        att[~mask] = float("-inf")
+    else:
+        # mask is presumed additive
+        att += mask
+    return att
+
+
+
+class _SparseSoftmax(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        print(x)
+        _, m, n = x.shape
+        row_indices = x._csr_row_indices
+        values = x._csr_values
+        row_offsets = x._csr_row_offsets
+        column_indices = x._csr_column_indices
+        out = torch.ops.xformers.sparse_softmax_sputnik(
+            m, n, row_indices, values, row_offsets, column_indices
+        )
+        res = SparseCSRTensor._wrap(x.shape, values, row_indices, row_offsets, column_indices, x._csr_transp_info)
+        # note: save out and not values, as an optimization step
+        ctx.save_for_backward(res)
+        #ctx.save_for_backward(row_indices, out, row_offsets, column_indices)
+        #ctx.size = (m, n)
+        return res
+
+    @staticmethod
+    def backward(ctx, grad):
+        x, = ctx.saved_tensors
+
+        _, m, n = x.shape
+        row_indices = x._csr_row_indices
+        values = x._csr_values
+        row_offsets = x._csr_row_offsets
+        column_indices = x._csr_column_indices
+
+        # gradients w.r.t. values
+        grad = grad.__values.contiguous()
+        ga = torch.ops.xformers.sparse_softmax_backward_sputnik(
+            m, n, row_indices, out, grad, row_offsets, column_indices
+        )
+
+        res = SparseCSRTensor._wrap(res.shape, ga, row_indices, row_offsets, column_indices, res._csr_transp_info)
+
+        return res
+
+
+class SparseCSRTensor(torch.Tensor):
+    @staticmethod
+    def __new__(cls, row_offsets, column_indices, values, shape):
+        # Use a Tensor that of the give size for the wrapper.
+        kwargs = {}
+        kwargs["device"] = values.device
+        kwargs["dtype"] = values.dtype
+        kwargs["layout"] = values.layout
+        kwargs["requires_grad"] = values.requires_grad
+        return torch.Tensor._make_wrapper_subclass(cls, shape, **kwargs)
+
+    def __init__(self, row_offsets, column_indices, values, shape):
+        self.__row_offsets = row_offsets.contiguous()
+        self.__row_indices = _diffsort(row_offsets).to(row_offsets.dtype)
+        self.__column_indices = column_indices.contiguous()
+        self.__values = values.contiguous()
+
+        self.__transp_info = _get_transpose_info(
+            self.shape[1], self.shape[2],
+            self.__row_indices, self.__row_offsets, self.__column_indices
+        )
+
+    def __repr__(self):
+        return f"sparse_csr_tensor(shape={self.shape}, values={self.__values})"
+
+    @classmethod
+    def _wrap(
+        cls, shape, values, row_indices, row_offsets, column_indices, _transp_info
+    ):
+        matrix = cls.__new__(cls, row_offsets, column_indices, values, shape)
+        matrix.__values = values
+        matrix.__row_indices = row_indices
+        matrix.__row_offsets = row_offsets
+        matrix.__column_indices = column_indices
+        matrix.__transp_info = _transp_info
+        return matrix
+
+    @property
+    def _csr_values(self):
+        return self.__values
+
+    @property
+    def _csr_row_indices(self):
+        return self.__row_indices
+
+    @property
+    def _csr_row_offsets(self):
+        return self.__row_offsets
+
+    @property
+    def _csr_column_indices(self):
+        return self.__column_indices
+
+    @property
+    def _csr_transp_info(self):
+        return self.__transp_info
+
+    @classmethod
+    def _bmm(cls, arg0, arg1):
+        if not (isinstance(arg0, cls) and type(arg1) == torch.Tensor):
+            return NotImplemented
+
+        self = arg0
+        b = arg1
+
+        _, m, n = self.shape
+        row_indices = self.__row_indices
+        values = self.__values
+        row_offsets = self.__row_offsets
+        column_indices = self.__column_indices
+
+        out = _csr_ops._spmm.apply(
+            b, row_indices, values, row_offsets, column_indices, m, self.__transp_info
+        )
+        return out
+
+    @classmethod
+    def _softmax(cls, arg0, dim):
+        if not (dim == -1 or dim == 2):
+            return NotImplemented
+        #return _SparseSoftmax.apply(arg0)
+        
+        self = arg0
+        _, m, n = self.shape
+        row_indices = self.__row_indices
+        values = self.__values
+        row_offsets = self.__row_offsets
+        column_indices = self.__column_indices
+        out = _csr_ops._SparseSoftmax.apply(
+            m, n, row_indices, values, row_offsets, column_indices
+        )
+        return cls._wrap(
+            self.shape, out, row_indices, row_offsets, column_indices, self.__transp_info
+        )
+        
+
+    @classmethod
+    def _transpose(cls, arg0, dim0, dim1):
+        # TODO: check if need to return this or not
+        if not (dim0 == 1 or dim0 == -2):
+            return NotImplemented
+        if not (dim1 == 2 or dim1 == -1):
+            return NotImplemented
+
+        B, m, n = arg0.shape
+        values = arg0.__values
+
+        (
+            output_row_indices,
+            output_values,
+            output_row_offsets,
+            output_column_indices,
+        ) = _transpose_with_info(values, arg0.__transp_info)
+        new_transp_info = _get_transpose_info(
+            n, m, output_row_indices, output_row_offsets, output_column_indices
+        )
+
+        return cls._wrap(
+            (B, n, m),
+            output_values,
+            output_row_indices,
+            output_row_offsets,
+            output_column_indices,
+            new_transp_info,
+        )
+
+    @classmethod
+    def _masked_matmul(cls, a, b, mask):
+        if not (type(a) == torch.Tensor and type(b) == torch.Tensor):
+            return NotImplemented
+        assert mask.shape[1] == a.shape[1]
+        assert mask.shape[2] == b.shape[2]
+        row_indices = mask.__row_indices
+        row_offsets = mask.__row_offsets
+        column_indices = mask.__column_indices
+        out = _csr_ops._sddmm.apply(
+            a,
+            b.transpose(-2, -1),
+            row_indices,
+            row_offsets,
+            column_indices,
+            mask.__transp_info,
+        )
+        # TODO add bias here
+        return cls._wrap(
+            mask.shape, out, row_indices, row_offsets, column_indices, mask.__transp_info
+        )
+
+    @classmethod
+    def _to(cls, arg0, device):
+        assert isinstance(device, torch.device)
+        return cls._wrap(
+            arg0.shape,
+            arg0.__values.to(device=device),
+            arg0.__row_indices.to(device=device),
+            arg0.__row_offsets.to(device=device),
+            arg0.__column_indices.to(device=device),
+            tuple(t.to(device=device) for t in arg0.__transp_info),
+        )
+
+    @classmethod
+    def _to_dense(cls, arg0):
+        _, m, n = arg0.shape
+        shape = arg0.shape
+        matrix = torch.zeros(shape, dtype=arg0.dtype, device=arg0.device)
+        row_offsets = arg0.__row_offsets.long()
+        column_indices = arg0.__column_indices.long()
+        row_coo, _ = _csr_to_coo(m, n, row_offsets, column_indices)
+        b_idxs = torch.arange(len(arg0.__values), device=arg0.device)[:, None]
+        matrix[b_idxs, row_coo, column_indices] = arg0.__values
+        return matrix
+
+    @classmethod
+    def _binary_op(cls, func, arg0, arg1):
+        if not (isinstance(arg0, cls) and isinstance(arg1, cls)):
+            return NotImplemented
+        assert arg0.shape == arg1.shape
+        # TODO add cheap assert for indices
+        out = func(arg0.__values, arg1.__values)
+        return cls._wrap(
+            arg0.shape, out, arg0.__row_indices, arg0.__row_offsets, arg0.__column_indices, self.__transp_info
+        )
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        # print('here', func)
+        if kwargs is None:
+            kwargs = {}
+        if func in [torch.Tensor.bmm, torch.bmm]:
+            assert len(args) == 2
+            return cls._bmm(args[0], args[1])
+
+        if func in [torch.Tensor.softmax, torch.nn.functional.softmax]:
+            return cls._softmax(args[0], kwargs["dim"])
+
+        if func in [torch.Tensor.transpose, torch.transpose]:
+            assert len(kwargs) == 0
+            return cls._transpose(args[0], args[1], args[2])
+
+        if func == masked_matmul:
+            assert len(args) == 3
+            return cls._masked_matmul(args[0], args[1], args[2])
+
+        if func in [torch.Tensor.add, torch.add]:
+            assert len(args) == 2
+            return cls._binary_op(args[0], args[1])
+
+        if func == torch.Tensor.to:
+            assert len(args) == 2
+            return cls._to(args[0], args[1])
+            #return cls._to(args[0], kwargs["device"])
+
+        if func == torch.Tensor.to_dense:
+            assert len(args) == 1
+            return cls._to_dense(args[0])
+        
+        if func in [torch.Tensor.grad.__get__, torch.Tensor._grad.__get__]:
+            assert len(args) == 1
+            assert len(kwargs) == 0
+            x = args[0]
+            return cls._wrap(
+                x.shape, x.__values.grad, x.__row_indices, x.__row_offsets, x.__column_indices, x.__transp_info
+            )
+
+        with torch._C.DisableTorchFunction():
+        #with no_dispatch():
+            ret = func(*args, **kwargs)
+            # TODO: check this
+            if func in torch.overrides.get_default_nowrap_functions():
+                return ret
+            return torch._tensor._convert(ret, cls)
+
+        return NotImplemented
+
+    
+    @classmethod
+    def __torch_dispatch__(cls, func, types, args, kwargs):
+        """
+        if func in [torch.ops.aten._softmax]:
+            print(args, kwargs)
+            #return cls._softmax(args[0], kwargs["dim"])
+            return cls._softmax(args[0], args[1])
+
+        if func == torch.ops.aten.detach:
+            assert len(args) == 1
+            assert len(kwargs) == 0
+            x = args[0]
+            return cls._wrap(
+                x.shape, x.__values.detach(), x.__row_indices, x.__row_offsets, x.__column_indices, x.__transp_info
+            )
+
+        if func == torch.ops.aten._softmax_backward_data:
+            print("what are you doing here?")
+        """
+        return NotImplemented
+    
+
+
+import contextlib
+@contextlib.contextmanager
+def no_dispatch():
+    guard = torch._C._DisableTorchDispatch()  # type: ignore[attr-defined]
+    try:
+        yield
+    finally:
+        del guard
---
+++ xformers/sparse/utils.py
@@ -0,0 +1,126 @@
+import torch
+
+
+def _coo_to_csr(m, n, row_indices, column_indices):
+    # assumes coalesced coo
+    row_offsets = row_indices.bincount(minlength=n).cumsum(0, dtype=row_indices.dtype)
+    row_offsets = torch.nn.functional.pad(row_offsets, (1, 0))
+    return row_offsets, column_indices
+
+
+def _csr_to_coo(m, n, row_offsets, column_indices):
+    # convert from compressed rows to uncompressed
+    indices = torch.arange(m, dtype=row_offsets.dtype, device=row_offsets.device)
+    row_sizes = torch.diff(row_offsets)
+    row_coo = torch.repeat_interleave(indices, row_sizes.long())
+    return row_coo, column_indices
+
+
+def _diffsort(a):
+    return torch.argsort(torch.diff(a), dim=0, descending=True)
+
+
+def _get_transpose_info(m, n, row_indices, row_offsets, column_indices):
+    # strategy:
+    # - uncompress the rows to have data in COO format
+    # - get permutation for stable sort of the columns to get the rows for the transposed matrix
+    # - compress the new rows and return the permutation to be applied on the values
+
+    # convert from compressed rows to uncompressed
+    row_coo, _ = _csr_to_coo(m, n, row_offsets, column_indices)
+
+    # get the permutation for the stable sort
+    # unfortunately PyTorch sort is not stable, so we need to
+    # do it in two steps
+    row_offsets_t, perm1 = column_indices.sort(0)
+    new_columns = row_coo[perm1]
+
+    # workaround the lack of stable sorting in PyTorch
+    perm2 = torch.argsort(new_columns + row_offsets_t * m)
+    column_indices_t = new_columns[perm2]
+
+    # find the final permutation corresponding to the indices of the stable sort
+    perm = perm1[perm2]
+
+    row_offsets_t, _ = _coo_to_csr(m, n, row_offsets_t, column_indices)
+    row_indices_t = _diffsort(row_offsets_t).int()
+
+    return row_indices_t, row_offsets_t, column_indices_t, perm
+
+
+def _transpose_with_info(values, _transpose_info):
+    row_indices_t, row_offsets_t, column_indices_t, perm = _transpose_info
+    values_t = values[:, perm]
+    return row_indices_t, values_t, row_offsets_t, column_indices_t
+
+
+def _transpose(m, n, row_indices, values, row_offsets, column_indices):
+    _transpose_info = _get_transpose_info(
+        m, n, row_indices, row_offsets, column_indices
+    )
+    return _transpose_with_info(values, _transpose_info)
+
+
+def _nonzero_mask_to_sparse_csr_indices(mask, device):
+    """Converts dense 2d matrix to a csr sparse matrix."""
+
+    assert len(mask.shape) == 2
+    index_dtype = torch.int32
+
+    # Calculate the offset of each row.
+    row_offsets = mask.sum(dim=-1, dtype=index_dtype).cumsum(dim=-1, dtype=index_dtype)
+    row_offsets = torch.nn.functional.pad(row_offsets, (1, 0))
+
+    # Create the row indices and sort them.
+    row_indices = _diffsort(row_offsets).to(index_dtype)
+
+    # Extract the column indices for the nonzero values.
+    column_indices = torch.where(mask)[1].to(index_dtype).contiguous()
+
+    row_indices = row_indices.to(device)
+    row_offsets = row_offsets.to(device)
+    column_indices = column_indices.to(device)
+    return row_indices, row_offsets, column_indices
+
+
+def _dense_to_sparse(matrix, device):
+    """Converts dense 2d matrix to a csr sparse matrix."""
+
+    assert len(matrix.shape) == 2
+    value_dtype = torch.float32
+
+    # Extract the nonzero values.
+    mask = matrix != 0
+    values = matrix[mask].to(dtype=value_dtype, device=device)
+
+    row_indices, row_offsets, column_indices = _nonzero_mask_to_sparse_csr_indices(
+        mask, device
+    )
+    return values, row_indices, row_offsets, column_indices
+
+
+def _round_nnz(mask, divisible_by=4):
+    nonzero = torch.where(mask)
+    nnz = nonzero[0].shape[0]
+    nonzero = tuple(n[: (nnz - nnz % divisible_by)] for n in nonzero)
+    nm = torch.zeros_like(mask)
+    nm[nonzero] = True
+    return nm
+
+
+def _dense3d_to_sparse(matrix, device):
+    assert len(matrix.shape) == 3
+    mask = matrix != 0
+    if not torch.all(mask == mask[0]):
+        raise ValueError("Expected the same sparsity pattern over the batch dimension")
+
+    # for now, our kernels assume that we have the number of
+    # nnz to be divisible by 4
+    mask = _round_nnz(mask[0], divisible_by=4)
+    mask = mask[None].expand(matrix.shape)
+
+    values = matrix[mask].reshape(matrix.shape[0], -1).to(device)
+    row_indices, row_offsets, column_indices = _nonzero_mask_to_sparse_csr_indices(
+        mask[0], device
+    )
+    return values, row_indices, row_offsets, column_indices
