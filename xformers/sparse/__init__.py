3353fe671a55c034613edc8e8b5d6914f9c9cd04
Francisco Massa <fvsmassa@gmail.com>
2021-11-29 10:05:30 +0100

    Add headers

--- xformers/sparse/__init__.py
+++ xformers/sparse/__init__.py
@@ -1,1 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 from .csr_tensor import SparseCSRTensor  # noqa: F401
--- xformers/sparse/_csr_ops.py
+++ xformers/sparse/_csr_ops.py
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import torch
 
 from .utils import _csr_to_coo, _transpose_with_info
