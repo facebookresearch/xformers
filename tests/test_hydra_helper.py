570c73d6a3299bd73f82de35e9ef8a5b3f846fab
dianaml0 <82468439+dianaml0@users.noreply.github.com>
2021-12-02 12:00:41 -0500

    add copyright headers (#137)
    Co-authored-by: Diana Liskovich <dianaml@devfair0471.h2.fair>

--- tests/test_hydra_helper.py
+++ tests/test_hydra_helper.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
 from hydra.core.config_store import ConfigStore
 
 from xformers.factory.hydra_helper import import_xformer_config_schema
