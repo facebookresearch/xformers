97ab4e13a453a89ff98f5684a1b12667a2f2456b
Diana Liskovich <dianaml@devfair0471.h2.fair>
2021-12-02 08:35:00 -0800

    add copyright headers

--- xformers/factory/hydra_helper.py
+++ xformers/factory/hydra_helper.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
 # register components configs into Hydra ConfigStore
 # component config classes could be used to validate configs
 import logging
