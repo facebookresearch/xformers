5b00fd6408a930c67758c477d8a90480294d9fac
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2021-12-01 20:25:34 -0500

    [chore][minor] Adding some testing of different projection options (#136)

--- xformers/components/__init__.py
+++ xformers/components/__init__.py
@@ -12,6 +12,7 @@ from xformers.utils import import_all_modules
 
 from .activations import Activation, build_activation  # noqa
 from .attention import Attention, build_attention  # noqa
+from .in_proj_container import InProjContainer, InProjParams  # noqa
 from .multi_head_dispatch import MultiHeadDispatch, MultiHeadDispatchConfig  # noqa
 from .residual import LayerNormStyle, PostNorm, PreNorm, Residual  # noqa
 
