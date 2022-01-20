5da4659ffcf40cc2420fa55eeb5343b6b06b4f16
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-01-19 16:08:11 -0800

    matmul code subtly buggy, does not seem indexing related

--- tests/test_ragged_qk_dotprod.py
+++ tests/test_ragged_qk_dotprod.py
@@ -8,7 +8,7 @@ import math
 import pytest
 import torch
 
-from xformers.triton.k_ragged_qk_dotprod import mem_efficient_fw
+from xformers.triton.k_ragged_qk_dotprod import ragged_qk_dotprod
 from xformers.triton.utils import gpu_capabilities_older_than_70
 
 _triton_available = True
@@ -25,20 +25,38 @@ _triton_available = True
 # Shapes correspond to (B, M&N, L)
 # TODO: test with M & N different from each other
 SHAPES = [
-    (1, 384, 128),
+    (1, 16, 16),
+    (1, 384, 64),
     (8, 384, 128),
     (8, 784, 512),
     (16, 1024, 1024),
     (2, 2048, 384),
-    # (1, 18, 128), # FIXME
+    (1, 18, 128),
 ]
+
+DEBUG = True
+
+
+def debug_plot_diff(a, b):
+    if DEBUG:
+        if a.ndim == 3:
+            a = a[0]
+
+        if b.ndim == 3:
+            b = b[0]
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+
+        sns.heatmap(a.cpu() - b.cpu())
+        plt.show()
+
+    return torch.max(torch.abs(a - b))
 
 
 def attention_pytorch(q, k):
     # attention matrix
     q = q / math.sqrt(q.size(-1))
-    a = q @ k.transpose(-2, -1)
-    return a
+    return q @ k.transpose(-2, -1)
 
 
 @pytest.mark.skipif(not _triton_available, reason="Triton is not available")
@@ -48,18 +66,18 @@ def attention_pytorch(q, k):
 )
 @pytest.mark.parametrize("shape", SHAPES)
 @pytest.mark.parametrize("dtype", [torch.float32])
-def test_mem_efficient_attention_parity(shape, dtype):
+def test_ragged_qk_parity(shape, dtype):
     q = torch.rand(shape, dtype=dtype, device=torch.device("cuda"))
     k = torch.rand(shape, dtype=dtype, device=torch.device("cuda"))
 
     res_pytorch = attention_pytorch(q, k)
-    res_me = mem_efficient_fw(q=q, k=k)
-    # print(res_me)
-    assert torch.mean(torch.abs(res_pytorch - res_me)) < 0.2
+    res_me = ragged_qk_dotprod(q=q, k=k)
+    print(torch.abs(res_pytorch - res_me))
 
-    # assert torch.allclose(res_pytorch, res_me, rtol=1e-1) FIXME
+    assert torch.allclose(res_pytorch, res_me, rtol=1e-1), debug_plot_diff(
+        res_pytorch, res_me
+    )
     # TODO: test different sequence lengths for q and k
-    # TODO: check parity with normal attention
 
 
 @pytest.mark.skipif(not _triton_available, reason="Triton is not available")
@@ -69,7 +87,7 @@ def test_mem_efficient_attention_parity(shape, dtype):
 )
 @pytest.mark.parametrize("shape", SHAPES)
 @pytest.mark.parametrize("dtype", [torch.float32])
-def test_mem_efficient_attention_memory_use(shape, dtype):
+def test_ragged_qk_memory_use(shape, dtype):
     # FW a random bunch of data
     q = torch.rand(shape, dtype=dtype, device=torch.device("cuda"))
     k = torch.rand(shape, dtype=dtype, device=torch.device("cuda"))
@@ -87,7 +105,7 @@ def test_mem_efficient_attention_memory_use(shape, dtype):
     torch.cuda.empty_cache()
     torch.cuda.reset_peak_memory_stats()
 
-    mem_efficient_fw(
+    ragged_qk_dotprod(
         q=q,
         k=k,
     )
@@ -97,7 +115,7 @@ def test_mem_efficient_attention_memory_use(shape, dtype):
     print(f"Memory efficient - Peak memory use: {max_memory_me}MB")
 
     fudge_factor = 3.0
-    assert max_memory_me <= max_memory_torch * fudge_factor
+    assert max_memory_torch == 0 or max_memory_me <= max_memory_torch * fudge_factor
 
 
 """
--- xformers/triton/k_ragged_qk_dotprod.py
+++ xformers/triton/k_ragged_qk_dotprod.py
@@ -9,118 +9,119 @@ import torch
 import triton
 import triton.language as tl
 
-_DEBUG = 0  # 1 to see the kernel PTX assembly
-
 
 # fmt: off
 @triton.jit
-def k_me_attention_fw(
-    OUT,    # TODO: rename to OUT_PTR etc
-    Q, K,            # in ptrs
-    M,  # N_QUERY_CTX
-    N,  # N_KEY_CTX
-    L,  # D_MODEL
-    stride_m,
-    stride_n,
+def k_ragged_attention_fw(
+    OUT_PTR,
+    Q_PTR, K_PTR,
+    N_QUERY_CTX,
+    N_KEY_CTX,
+    D_MODEL,
+    stride_o_b, stride_o_m,
+    stride_q_b, stride_q_m,
+    stride_k_b, stride_k_n,
     **META,
 ):
     # fmt: on
 
     # extract metaparameters
     BLOCK_M = META["BLOCK_M"]
-    BLOCK_N, BLOCK_L = META["BLOCK_N"], META["BLOCK_L"]
-
+    BLOCK_N = META["BLOCK_N"]
+    BLOCK_L = META["BLOCK_L"]
     scale = META["SCALE"]
 
-    # *within groups*, programs are ordered in a column-major order
-    # row-id /col-id of the program in the *launch grid*
-    pid_m = tl.program_id(axis=0)
-    pid_n = tl.program_id(axis=1)
+    # -----------------------------------------------------------
+    # Map program ids `pid` to the block of C it should compute.
+    # This is done in a grouped ordering to promote L2 data reuse
+    # See above `L2 Cache Optimizations` section for details
+    pid_b = tl.program_id(axis=0)
+    pid = tl.program_id(axis=1)
+    GROUP_SIZE_M = 8
 
-    # Compute QKt
-    # block level matrix multiplication.
-    # We fetch a block memory block from both inputs, matmul and accumulate, then repeat
+    num_pid_m = tl.cdiv(N_QUERY_CTX, BLOCK_M)
+    num_pid_n = tl.cdiv(N_KEY_CTX, BLOCK_N)
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_SIZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    pid_m = first_pid_m + (pid % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
+
+    # Compute QKt, block level matrix multiplication.
+    # We fetch a block memory from both inputs, matmul and accumulate, then repeat
+    # This step walks over the model dimension
     qkt = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
 
-    i = 0
     rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
     rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
     rl = tl.arange(0, BLOCK_L)
 
-    for _ in range(L, 0, -BLOCK_L):
-        rl_i = rl + i * BLOCK_L  # keep track of the masking
-        q_ptrs = Q + rm[:, None] * L + rl_i[None, :]    # (BLOCK_M, BLOCK_L)
-        k_ptrs = K + rn[None, :] * L + rl_i[:, None]    # (BLOCK_L, BLOCK_N)
+    l_offset = 0
+    q_bm_ptrs = Q_PTR + pid_b * stride_q_b + rm[:, None] * stride_q_m
+    k_bn_ptrs = K_PTR + pid_b * stride_k_b + rn[None, :] * stride_k_n
 
-        q = tl.load(q_ptrs, mask=((rm[:, None] < M) & (rl_i[None, :] < L)), other=0.0)    # (BLOCK_M, BLOCK_L)
-        k = tl.load(k_ptrs, mask=((rl_i[:, None] < L) & (rn[None, :] < N)), other=0.0)    # (BLOCK_L, BLOCK_N)
+    for _ in range(0, D_MODEL, BLOCK_L):
+        rl_i = rl + l_offset
+        q_ptrs = q_bm_ptrs + rl_i[None, :]    # (BLOCK_M, BLOCK_L)
+        k_ptrs = k_bn_ptrs + rl_i[:, None]    # (BLOCK_L, BLOCK_N)
 
-        q *= scale  # q /= sqrt(dim)
-        qkt += tl.dot(q, k).to(tl.float32)              # (BLOCK_M, BLOCK_N)
+        q_mask = ((rm[:, None] < N_QUERY_CTX) & (rl_i[None, :] < D_MODEL))
+        q = tl.load(q_ptrs, mask=q_mask, other=0.0)  # (BLOCK_M, BLOCK_L)
+
+        k_mask = ((rl_i[:, None] < D_MODEL) & (rn[None, :] < N_KEY_CTX))
+        kt = tl.load(k_ptrs, mask=k_mask, other=0.0)   # (BLOCK_L, BLOCK_N)
+
+        q *= scale.to(q.dtype)  # q /= sqrt(dim)
+        qkt += tl.dot(q, kt).to(tl.float32)              # (BLOCK_M, BLOCK_N)
 
         # Update the pointers and counter
-        i += 1
+        l_offset += BLOCK_L
 
     # Final output tile is [BLOCK_M, BLOCK_N]
-    m_offset = rm[:, None] * stride_m
-    n_offset = rn[None, :] * stride_n
-    scores_ptrs = OUT + m_offset + n_offset
+    m_offset = rm[:, None] * stride_o_m
+    n_offset = rn[None, :]
+    scores_ptrs = OUT_PTR + pid_b * stride_o_b + m_offset + n_offset
 
-    mask = (rm[:, None] < M)
-    tl.store(scores_ptrs, qkt, mask=mask)
+    tl.store(scores_ptrs, qkt, mask=(rm[:, None] < N_QUERY_CTX) & (rn[None, :] < N_KEY_CTX))
 
 
-def mem_efficient_fw(q: torch.Tensor, k: torch.Tensor) -> torch.Tensor:
+def ragged_qk_dotprod(q: torch.Tensor, k: torch.Tensor) -> torch.Tensor:
 
     assert q.ndim == 3
-    assert q.shape[-1] == k.shape[-1]
+    assert q.shape[-1] == k.shape[-1] and q.shape[0] == k.shape[0]
     assert q.is_contiguous() and k.is_contiguous()
     B, M, L = q.shape
     B, N, L = k.shape
 
     # TODO: autotune these
-    BLOCK_M = 8
-    BLOCK_N = 8
-    # BLOCK_N = min(triton.next_power_of_2(N), 1024)  # increase the ceiling to save more memory
-    BLOCK_L = 8
-
-    tiles_n = triton.cdiv(N, BLOCK_N)
-    tiles_m = triton.cdiv(M, BLOCK_M)
-    # print(f"{tiles_m=}, {tiles_n=}") # 3, 1
+    BLOCK_M = 64
+    BLOCK_N = 64
+    BLOCK_L = 32
 
     # We put parameters into the grid() function to allow for auto-tuning
     def grid(META):
-        # tiles_m = triton.cdiv(M, META["BLOCK_M"])
         return (
-            tiles_m,
-            tiles_n
+            B,
+            triton.cdiv(M, META["BLOCK_M"]) * triton.cdiv(N, META["BLOCK_N"]),
         )
 
-    # out_scores = torch.ones((B, M, N), dtype=q.dtype, device=q.device) * 55
     out_scores = torch.empty((B, M, N), dtype=q.dtype, device=q.device)
 
-    # TODO: improve on the batch dimension handling ?
-
-    for i_b in range(B):
-
-        # Use a dedicated kernel to process the attention by blocks
-        # fmt: off
-        bin = k_me_attention_fw[grid](
-            out_scores[i_b],       # outputs
-            q[i_b],
-            k[i_b],     # inputs
-            M, N, L,                            # dimensions
-            out_scores.stride(1),
-            out_scores.stride(2),
-            BLOCK_M=BLOCK_M,
-            BLOCK_N=BLOCK_N,
-            BLOCK_L=BLOCK_L,
-            SCALE=1. / math.sqrt(L),
-            num_warps=1
-        )
-        # fmt: onx
-
-        if _DEBUG:
-            print(bin.asm['ptx'])
+    # Use a dedicated kernel to process the attention by blocks
+    # fmt: off
+    k_ragged_attention_fw[grid](
+        out_scores,         # outputs
+        q, k,               # inputs
+        M, N, L,            # dimensions
+        out_scores.stride(0), out_scores.stride(1),
+        q.stride(0), q.stride(1),
+        k.stride(0), k.stride(1),
+        BLOCK_M=BLOCK_M,
+        BLOCK_N=BLOCK_N,
+        BLOCK_L=BLOCK_L,
+        SCALE=1. / math.sqrt(L),
+    )
+    # fmt: onx
 
     return out_scores
