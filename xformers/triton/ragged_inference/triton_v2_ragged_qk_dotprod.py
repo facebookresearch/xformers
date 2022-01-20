ac68902f5adc3d6f717738360f7c57a51ddbd24d
Tom B Brown <nottombrown@gmail.com>
2022-01-19 22:18:25 -0800

    WIP

--- xformers/triton/ragged_inference/triton_v2_ragged_qk_dotprod.py
+++ xformers/triton/ragged_inference/triton_v2_ragged_qk_dotprod.py
@@ -8,15 +8,15 @@ def init_to_zero(name):
     return lambda nargs: nargs[name].zero_()
 
 
-BLOCK_Q = 16
-BLOCK_K = 16
+BLOCK_Q = 32
+BLOCK_K = 32
 BLOCK_D = 64
 
 
 def get_fast_dev_configs():
     return [
         triton.Config(
-            {"BLOCK_Q": 64, "BLOCK_K": 32, "BLOCK_D": 32},
+            {"BLOCK_Q": BLOCK_Q, "BLOCK_K": BLOCK_K, "BLOCK_D": BLOCK_D},
             num_stages=5,
             num_warps=2,
         )
@@ -45,6 +45,12 @@ def _qk_dotprod_kernel(
     stride_ctx_k,
     stride_out_q,
     stride_out_k,
+    # These are lookup tables (sometimes referred to as a "lut")
+    # pid_to_q_in_token_offset,
+    # pid_to_k_in_token_offset,
+    # pid_to_out_q_block,
+    # pid_to_out_k_block,
+    # These get populated from the triton.Config
     BLOCK_Q: tl.constexpr,
     BLOCK_K: tl.constexpr,
     BLOCK_D: tl.constexpr,
@@ -67,7 +73,8 @@ def _qk_dotprod_kernel(
     q_in_token_offset = q_block_idx * BLOCK_Q
     k_in_token_offset = k_block_idx * BLOCK_K
 
-    # do matrix multiplication
+    # Define pointer ranges, we follow the triton convention of prefixing
+    # with "r" to denote a range, like "rq" is the range for queries
     rq = q_in_token_offset + tl.arange(0, BLOCK_Q)
     rq = tl.max_contiguous(tl.multiple_of(rq % n_ctx_q, BLOCK_Q), BLOCK_Q)
 
@@ -128,15 +135,36 @@ def qk_dotprod_v2(query, key):
     assert key.stride(1) == 1, f"{key.stride(1)}"
 
     # We need to know the grid to make our lookup tables
-    grid = (triton.cdiv(n_ctx_q, BLOCK_Q) * triton.cdiv(n_ctx_k, BLOCK_K),)
 
-    # Create lookup tables
-    pid_to_query_input_token_offset = [0, 4, 5, 8, 12]
-    pid_to_key_input_token_offset = ...
+
+    # Create lookup tables for rectangular tensors
+
+
+    grid_q = triton.cdiv(n_ctx_q, BLOCK_Q)
+    grid_k = triton.cdiv(n_ctx_k, BLOCK_K)
+    n_pids_total = grid_q * grid_k
+
+    pid_to_q_in_token_offset = torch.zeros(n_pids_total)
+    pid_to_k_in_token_offset = torch.zeros(n_pids_total)
+    pid_to_out_q_block = torch.zeros(n_pids_total)
+    pid_to_out_k_block = torch.zeros(n_pids_total)
+
+    for pid in range(n_pids_total):
+        q_block_idx = pid // grid_k
+        k_block_idx = pid % grid_k
+
+        q_in_token_offset = q_block_idx * BLOCK_Q
+        k_in_token_offset = k_block_idx * BLOCK_K
+
+        pid_to_out_q_block[pid] = q_block_idx
+        pid_to_out_k_block[pid] = k_block_idx
+        pid_to_q_in_token_offset[pid] = q_in_token_offset
+        pid_to_k_in_token_offset[pid] = k_in_token_offset
+
+
     # pid_to_seq_idx = [0, 0, 1, 2, 2]
 
-    # pid_to_out_k_block
-
+    grid = (n_pids_total,)
     _qk_dotprod_kernel[grid](
         query,
         key,
@@ -144,6 +172,10 @@ def qk_dotprod_v2(query, key):
         n_ctx_q,
         n_ctx_k,
         d_head,
+        # pid_to_q_in_token_offset.cuda(),
+        # pid_to_k_in_token_offset.cuda(),
+        # pid_to_out_q_block.cuda(),
+        # pid_to_out_k_block.cuda(),
         query.stride(0),  # stride_ctx_q
         key.stride(0),  # stride_ctx_k
         scores_out.stride(0),  # stride_out_q
