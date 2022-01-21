be39f20dac9a76840bbf23ac8e9a2f21ed4047c0
Tom B Brown <nottombrown@gmail.com>
2022-01-20 22:52:50 -0800

    Add new directory

---
+++ experimental/triton_v2_ragged_inference/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="triton_v2_ragged_inference",
+    author="Facebook AI Research",
+    version="0.0.0",
+    packages=["triton_v2_ragged_inference"],
+    install_requires=[],
+    scripts=[],
+    python_requires=">=3.6",
+)
