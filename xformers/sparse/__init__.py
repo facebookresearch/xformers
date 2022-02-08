143318fcd357031155480004d6cb359464ad6bc4
dianaml0 <82468439+dianaml0@users.noreply.github.com>
2022-02-08 15:37:10 -0500

    catch error importing blocksparse tensor when triton not installed (#213)
    Co-authored-by: Diana Liskovich <dianaml@devfair0471.h2.fair>

--- xformers/sparse/__init__.py
+++ xformers/sparse/__init__.py
@@ -3,6 +3,13 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
+import logging
 
-from .blocksparse_tensor import BlockSparseTensor  # noqa: F401
+try:
+    from .blocksparse_tensor import BlockSparseTensor  # noqa: F401
+except ImportError as e:
+    logging.warning(
+        f"Triton is not available, some optimizations will not be enabled.\nError {e}"
+    )
+
 from .csr_tensor import SparseCSRTensor  # noqa: F401
