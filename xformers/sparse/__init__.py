6c4cfbed70b4d2a4de906d961a4d1f4524c641b8
Francisco Massa <fvsmassa@gmail.com>
2022-02-09 10:20:23 +0100

    Make triton optional in BlockSparseTensor (#214)

--- xformers/sparse/__init__.py
+++ xformers/sparse/__init__.py
@@ -3,13 +3,5 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-import logging
-
-try:
-    from .blocksparse_tensor import BlockSparseTensor  # noqa: F401
-except ImportError as e:
-    logging.warning(
-        f"Triton is not available, some optimizations will not be enabled.\nError {e}"
-    )
-
+from .blocksparse_tensor import BlockSparseTensor  # noqa: F401
 from .csr_tensor import SparseCSRTensor  # noqa: F401
