5583cbac85fe4ee885b799ab4a8f858336ed6a8e
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2022-03-14 12:55:43 -0700

    v0.0.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ copyright = "Copyright © 2021 Meta Platforms, Inc"
 author = "Facebook AI Research"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.9"
+release = "0.0.10"
 
 
 # -- General configuration ---------------------------------------------------
