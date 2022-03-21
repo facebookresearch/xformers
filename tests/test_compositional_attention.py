ffa5e11a58ebff1a81cb6eca7d49a37b1d0369a9
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2022-03-21 12:27:51 -0400

    [minor] Speeding up comp attention test on CPU (#242)

--- tests/test_compositional_attention.py
+++ tests/test_compositional_attention.py
@@ -16,19 +16,18 @@ DEVICES = (
 )
 
 BATCH = 2
-SEQ = 128 if torch.cuda.is_available() else 32
-MODEL = 128 if torch.cuda.is_available() else 64
+SEQ = 128 if torch.cuda.is_available() else 16
+MODEL = 128 if torch.cuda.is_available() else 32
 
 assert ATTENTION_REGISTRY.keys(), "Attention layers should have been registered"
 
 
+@pytest.mark.parametrize("heads", [1, 4])
 @pytest.mark.parametrize("attn_dropout", [0.0, 0.3])
 @pytest.mark.parametrize("causal", [True, False])
-@pytest.mark.parametrize("heads", [1, 4])
 @pytest.mark.parametrize("rules", [1, 4])
 @pytest.mark.parametrize("q_compose", [False, True])
 @pytest.mark.parametrize("dim_selection", [MODEL // 2, None])
-@pytest.mark.parametrize("bias", [True, False])
 @pytest.mark.parametrize("num_rules", [1, 2])
 @pytest.mark.parametrize("qk_rule", [True, False])
 @pytest.mark.parametrize("nonlinear", [True, False])
@@ -40,7 +39,6 @@ def test_build_and_run(
     rules: int,
     q_compose: bool,
     dim_selection: int,
-    bias: bool,
     num_rules: int,
     qk_rule: bool,
     nonlinear: bool,
@@ -60,17 +58,9 @@ def test_build_and_run(
         "q_compose": q_compose,
         "rules": rules,
         "dim_selection": dim_selection,
-        "bias": bias,
         "qk_rule": qk_rule,
         "nonlinear": nonlinear,
     }
-
-    # Add some blocksparse layout to test the corresponding attention
-    block_size = 16
-    test_config["layout"] = torch.eye(
-        SEQ // block_size, SEQ // block_size, dtype=torch.long
-    )
-    test_config["block_size"] = block_size
 
     attention = build_attention(test_config)
 
@@ -96,7 +86,7 @@ def test_build_and_run(
         results_shuffled = multi_head(inputs_shuffled, inputs_shuffled, inputs_shuffled)
 
         if attn_dropout == 0.0 and num_rules == 1 and not causal:
-            assert (results[:, shuffle, :] - results_shuffled).abs().max() < 1e-5
+            assert (results[:, shuffle, :] - results_shuffled).abs().max() < 1e-3
 
         # Test the non-self-attention codepath
         att = multi_head(inputs, inputs_shuffled, inputs)
