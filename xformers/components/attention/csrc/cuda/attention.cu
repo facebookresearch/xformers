62c3009f9eaad5dba9f2acb3683cfe05dd10d672
Francisco Massa <fvsmassa@gmail.com>
2022-04-04 04:54:31 -0700

    Get extra 25% speedup

--- xformers/components/attention/csrc/cuda/attention.cu
+++ xformers/components/attention/csrc/cuda/attention.cu
@@ -36,35 +36,50 @@ __global__ void attention_kernel(
   int64_t N = key.size(1);
 
   int64_t i = blockIdx.y;
-  int64_t j = blockIdx.x;
+  //int64_t j = blockIdx.x;
+  int64_t j = blockIdx.x * blockDim.y + threadIdx.y;
 
       {{
-        auto aar = query[i][j].data();
+        //auto aar = query[i][j].data();
+        auto aar = reinterpret_cast<float4 *>(query[i][j].data());
 
-        auto oo = output[i][j].data();
+        //auto oo = output[i][j].data();
+        auto oo = reinterpret_cast<float4 *>(output[i][j].data());
         scalar_t s_prime = 0;
         scalar_t m_prime = -std::numeric_limits<scalar_t>::infinity();
+        //for (int64_t l = threadIdx.x * BLOCK; l < N; l+=BLOCK * blockDim.x) {
         for (int64_t l = 0; l < N; l+=BLOCK) {
-          auto bar = key[i][l].data();
+          //auto bar = key[i][l].data();
+          auto bar = reinterpret_cast<float4 *>(key[i][l].data());
           scalar_t si[BLOCK] = {0};
-          for (int64_t k = threadIdx.x; k < K; k+=32) {
-            auto aaar = aar[k];
-            for (int64_t rr = 0; rr < BLOCK; rr++)
-              si[rr] += aaar * bar[k + K * rr];
-          }
+          //for (int64_t k = threadIdx.x; k < K; k+=32) {
+          for (int64_t k = 0; k < K / 4; k+=1) {
+            //auto aaar = aar[k];
+            //auto aaar = __ldg(aar + k);
+            float4 aaar = __ldg(aar + k);
+            for (int64_t rr = 0; rr < BLOCK; rr++) {
+              float4 bbb = bar[k + K / 4 * rr];
+              si[rr] += aaar.x * bbb.x + aaar.y * bbb.y + aaar.z * bbb.z + aaar.w * bbb.w;
 
-          for (int64_t rr = 0; rr < BLOCK; rr++) {
-            for (int stride = 16; stride > 0; stride >>= 1) {
-              si[rr] += __shfl_xor_sync(0xffffffff, si[rr], stride, 32);
+              //si[rr] += aaar * bar[k + K * rr];
+              //si[rr] += aaar * __ldg(bar + k + K * rr);
             }
           }
+
+          //for (int64_t rr = 0; rr < BLOCK; rr++) {
+          //  for (int stride = 16; stride > 0; stride >>= 1) {
+          //    si[rr] += __shfl_xor_sync(0xffffffff, si[rr], stride, 32);
+          //  }
+          //}
 
           scalar_t m_i = si[0] > m_prime ? si[0] : m_prime;
           for (int64_t rr = 1; rr < BLOCK; rr++) {
             m_i = si[rr] > m_i ? si[rr] : m_i;
           }
+          //s_prime = m_i;  // TODO: only for testing, remove!!!
 
-          auto vi = value[i][l].data();
+          //auto vi = value[i][l].data();
+          auto vi = reinterpret_cast<float4 *>(value[i][l].data());
 
           scalar_t m_delta;
           scalar_t s_delta[BLOCK];
@@ -73,10 +88,24 @@ __global__ void attention_kernel(
           for (int64_t rr = 0; rr < BLOCK; rr++)
             s_delta[rr] = std::exp(si[rr] - m_i);
 
-          for (int64_t k = threadIdx.x; k < K; k+=blockDim.x) {
-            oo[k] = oo[k] * m_delta;
-            for (int64_t rr = 0; rr < BLOCK; rr++)
-              oo[k] += vi[k + K * rr] * s_delta[rr];
+          //for (int64_t k = threadIdx.x; k < K; k+=blockDim.x) {
+          for (int64_t k = 0; k < K/4; k+=1) {
+            //oo[k] = oo[k] * m_delta;
+            float4 tmp = oo[k];
+            tmp.x = tmp.x * m_delta;
+            tmp.y = tmp.y * m_delta;
+            tmp.z = tmp.z * m_delta;
+            tmp.w = tmp.w * m_delta;
+            for (int64_t rr = 0; rr < BLOCK; rr++) {
+              //oo[k] += vi[k + K * rr] * s_delta[rr];
+              float4 tmp2 = vi[k + K / 4 * rr];
+              tmp.x += tmp2.x * s_delta[rr];
+              tmp.y += tmp2.y * s_delta[rr];
+              tmp.z += tmp2.z * s_delta[rr];
+              tmp.w += tmp2.w * s_delta[rr];
+              //oo[k] += __ldg(vi + k + K * rr) * s_delta[rr];
+            }
+            oo[k] = tmp;
           }
           s_prime = s_prime * m_delta;
           for (int64_t rr = 0; rr < BLOCK; rr++)
@@ -85,8 +114,15 @@ __global__ void attention_kernel(
           m_prime = m_i;
         }
 
-        for (int64_t k = threadIdx.x; k < K; k+=blockDim.x) {
-          oo[k] /= s_prime;
+        //for (int64_t k = threadIdx.x; k < K; k+=blockDim.x) {
+        for (int64_t k = 0; k < K / 4; k+=1) {
+          //oo[k] /= s_prime;
+          float4 tmp = oo[k];
+          tmp.x /= s_prime;
+          tmp.y /= s_prime;
+          tmp.z /= s_prime;
+          tmp.w /= s_prime;
+          oo[k] = tmp;
         }
       }
   }
@@ -139,8 +175,11 @@ at::Tensor attention(
   //    ceil_div(static_cast<int64_t>(B), static_cast<int64_t>(512)),
   //    static_cast<int64_t>(4096)));
   //dim3 block(512);
-  dim3 grid(M, B);
-  dim3 block(32, 1);
+  //dim3 grid(M / 32, B);
+  dim3 grid(M / 32, B);
+  //dim3 block(32, 32);
+  //dim3 block(4, 32);
+  dim3 block(1, 32);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
   AT_DISPATCH_FLOATING_TYPES(
