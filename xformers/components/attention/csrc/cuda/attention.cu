73eaaa42080c9468a4766d5546d8b038a87f3f10
Francisco Massa <fvsmassa@gmail.com>
2022-04-05 09:03:32 -0700

    Use vec_t + cleanups

--- xformers/components/attention/csrc/cuda/attention.cu
+++ xformers/components/attention/csrc/cuda/attention.cu
@@ -11,10 +11,6 @@
 
 namespace {
 
-#define CUDA_1D_KERNEL_LOOP(i, n)                                \
-  for (int i = (blockIdx.x * blockDim.x) + threadIdx.x; i < (n); \
-       i += (blockDim.x * gridDim.x))
-
 template <typename integer>
 constexpr __host__ __device__ inline integer ceil_div(integer n, integer m) {
   return (n + m - 1) / m;
@@ -64,38 +60,38 @@ __device__ __forceinline__ scalar_t warpMax(scalar_t val) {
   return val;
 }
 
-template <typename scalar_t, int BLOCK, int BLOCK2>
+template <typename scalar_t, typename vec_t, int BLOCK, int BLOCK2>
 __device__ void compute_dot(
-    float4* aar[BLOCK2],
-    float4* bar,
+    vec_t* aar[BLOCK2],
+    vec_t* bar,
     scalar_t si[BLOCK2][BLOCK],
     int64_t K) {
-  constexpr int kVecSize = sizeof(float4) / sizeof(float);
+  constexpr int kVecSize = sizeof(vec_t) / sizeof(scalar_t);
   for (int64_t k = 0; k < K / kVecSize; k += 1) {
-    float4 aaar[BLOCK2];
+    vec_t aaar[BLOCK2];
 #pragma unroll
     for (int64_t rr = 0; rr < BLOCK2; rr++) {
       aaar[rr] = __ldg(aar[rr] + k);
     }
 #pragma unroll
     for (int64_t rr = 0; rr < BLOCK; rr++) {
-      float4 bbb = bar[k + K / kVecSize * rr];
+      vec_t bbb = bar[k + K / kVecSize * rr];
 #pragma unroll
       for (int64_t rr2 = 0; rr2 < BLOCK2; rr2++) {
-        sputnik::VectorCompute<float4>::Dot(aaar[rr2], bbb, &si[rr2][rr]);
+        sputnik::VectorCompute<vec_t>::Dot(aaar[rr2], bbb, &si[rr2][rr]);
       }
     }
   }
 }
 
-template <typename scalar_t, int BLOCK, int BLOCK2>
+template <typename scalar_t, typename vec_t, int BLOCK, int BLOCK2>
 __device__ void compute_final_mult(
-    float4* vi,
+    vec_t* vi,
     scalar_t s_delta[BLOCK2][BLOCK],
     scalar_t m_delta[BLOCK2],
-    float4 buffer[BLOCK2][8] /*TODO fix me*/,
+    vec_t buffer[BLOCK2][8] /*TODO fix me*/,
     int64_t K) {
-  constexpr int kVecSize = sizeof(float4) / sizeof(float);
+  constexpr int kVecSize = sizeof(vec_t) / sizeof(scalar_t);
 
   for (int64_t k = 0; k < K / kVecSize; k += 1) {
 #pragma unroll
@@ -104,11 +100,11 @@ __device__ void compute_final_mult(
     }
 #pragma unroll
     for (int64_t rr = 0; rr < BLOCK; rr++) {
-      float4 tmp2 = vi[k + K / kVecSize * rr];
+      vec_t tmp2 = vi[k + K / kVecSize * rr];
 
 #pragma unroll
       for (int64_t rr2 = 0; rr2 < BLOCK2; rr2++) {
-        sputnik::VectorCompute<float4>::FMA(
+        sputnik::VectorCompute<vec_t>::FMA(
             s_delta[rr2][rr], tmp2, &buffer[rr2][k]);
       }
     }
@@ -165,13 +161,13 @@ __device__ __forceinline__ void update_scaling_coeffs(
   }
 }
 
-template <typename scalar_t, int BLOCK = 32, int BLOCK2 = 2>
+template <typename scalar_t, typename vec_t = float4, int BLOCK = 32, int BLOCK2 = 2, int WARP_SIZE = 4>
 __global__ void attention_kernel(
     at::PackedTensorAccessor<scalar_t, 3> output,
     at::PackedTensorAccessor<scalar_t, 3> query,
     at::PackedTensorAccessor<scalar_t, 3> key,
     at::PackedTensorAccessor<scalar_t, 3> value) {
-  constexpr int kVecSize = sizeof(float4) / sizeof(float);
+  constexpr int kVecSize = sizeof(vec_t) / sizeof(scalar_t);
   int64_t K = query.size(2);
   int64_t B = query.size(0);
   int64_t M = query.size(1);
@@ -180,25 +176,25 @@ __global__ void attention_kernel(
   int64_t i = blockIdx.y;
   int64_t j = blockIdx.x * (blockDim.y * BLOCK2) + threadIdx.y * BLOCK2;
 
-  float4* aar[BLOCK2];
-  float4* oo[BLOCK2];
-  float4 buffer[BLOCK2][8] = {0}; // TODO == K / 4
+  vec_t* aar[BLOCK2];
+  vec_t* oo[BLOCK2];
+  vec_t buffer[BLOCK2][8] = {0}; // TODO == K / 4
   scalar_t s_prime[BLOCK2] = {0};
   scalar_t m_prime[BLOCK2] = {-std::numeric_limits<scalar_t>::infinity()};
   for (int64_t rr = 0; rr < BLOCK2; rr++) {
-    aar[rr] = reinterpret_cast<float4*>(query[i][j + rr].data());
-    oo[rr] = reinterpret_cast<float4*>(output[i][j + rr].data());
+    aar[rr] = reinterpret_cast<vec_t*>(query[i][j + rr].data());
+    oo[rr] = reinterpret_cast<vec_t*>(output[i][j + rr].data());
   }
 
   for (int64_t l = threadIdx.x * BLOCK; l < N; l += BLOCK * blockDim.x) {
-    auto bar = reinterpret_cast<float4*>(key[i][l].data());
+    auto bar = reinterpret_cast<vec_t*>(key[i][l].data());
     scalar_t si[BLOCK2][BLOCK] = {0};
-    compute_dot<scalar_t, BLOCK, BLOCK2>(aar, bar, si, K);
+    compute_dot<scalar_t, vec_t, BLOCK, BLOCK2>(aar, bar, si, K);
 
     scalar_t m_i[BLOCK2];
     compute_max<scalar_t, BLOCK, BLOCK2>(si, m_prime, m_i);
 
-    auto vi = reinterpret_cast<float4*>(value[i][l].data());
+    auto vi = reinterpret_cast<vec_t*>(value[i][l].data());
 
     scalar_t m_delta[BLOCK2];
     scalar_t s_delta[BLOCK2][BLOCK];
@@ -206,7 +202,7 @@ __global__ void attention_kernel(
     compute_scaling_coeffs<scalar_t, BLOCK, BLOCK2>(
         m_i, m_prime, si, m_delta, s_delta);
 
-    compute_final_mult<scalar_t, BLOCK, BLOCK2>(
+    compute_final_mult<scalar_t, vec_t, BLOCK, BLOCK2>(
         vi, s_delta, m_delta, buffer, K);
 
     update_scaling_coeffs<scalar_t, BLOCK, BLOCK2>(
@@ -216,21 +212,21 @@ __global__ void attention_kernel(
   for (int64_t rr = 0; rr < BLOCK2; rr++) {
     scalar_t m_i = m_prime[rr];
     scalar_t s_i = s_prime[rr];
-    m_prime[rr] = warpMax<scalar_t, 4>(m_prime[rr]);
+    m_prime[rr] = warpMax<scalar_t, WARP_SIZE>(m_prime[rr]);
     scalar_t m_delta = std::exp(m_i - m_prime[rr]);
     scalar_t s_delta = s_i * m_delta;
-    s_delta = warpSum<scalar_t, 4>(s_delta);
+    s_delta = warpSum<scalar_t, WARP_SIZE>(s_delta);
     s_prime[rr] = s_delta;
     for (int64_t k = 0; k < K / kVecSize; k += 1) {
-      float4 tmp = buffer[rr][k];
+      vec_t tmp = buffer[rr][k];
       iMul<scalar_t>(m_delta, &tmp);
-      tmp = warpSum<float4, 4>(tmp);
+      tmp = warpSum<vec_t, WARP_SIZE>(tmp);
       buffer[rr][k] = tmp;
     }
   }
 
   for (int64_t k = threadIdx.x; k < K / kVecSize; k += blockDim.x) {
-    float4 tmp;
+    vec_t tmp;
 
 #pragma unroll
     for (int64_t rr2 = 0; rr2 < BLOCK2; rr2++) {
@@ -279,26 +275,23 @@ at::Tensor attention(
 
   at::Tensor res = at::zeros({B, M, K}, query.options());
 
-  int64_t grain_size = 32; // TODO: tune this
-  // at::Tensor buffer = at::empty({B, grain_size, K}, query.options());
-  // at::Tensor buffer = at::empty({at::get_num_threads(), 1, K},
-  // query.options());
 
-  // dim3 grid(std::min(
-  //    ceil_div(static_cast<int64_t>(B), static_cast<int64_t>(512)),
-  //    static_cast<int64_t>(4096)));
-  // dim3 block(512);
-  // dim3 grid(M / 32, B);
-  dim3 grid(M / 32, B);
-  // dim3 block(32, 32);
-  // dim3 block(4, 32);
-  dim3 block(4, 16);
+  constexpr int WARP_SIZE = 4;
+
+  constexpr int BLOCK = 32;
+  constexpr int BLOCK2 = 2;
+
+  constexpr int TILE_SIZE = 32;
+
+
+  dim3 grid(M / TILE_SIZE, B);
+  dim3 block(WARP_SIZE, TILE_SIZE / BLOCK2);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
   using scalar_t = float;
   // AT_DISPATCH_FLOATING_TYPES(
   // query.scalar_type(), "attention_kernel", [&] {
-  attention_kernel<scalar_t><<<grid, block, 0, stream>>>(
+  attention_kernel<scalar_t, float4, BLOCK, BLOCK2, WARP_SIZE><<<grid, block, 0, stream>>>(
       res.packed_accessor<scalar_t, 3>(),
       query.packed_accessor<scalar_t, 3>(),
       key.packed_accessor<scalar_t, 3>(),
