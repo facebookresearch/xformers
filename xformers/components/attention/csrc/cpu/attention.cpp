df605156811945e5d5b586ee86596dfa64955cde
Francisco Massa <fvsmassa@gmail.com>
2022-04-13 02:27:46 -0700

    Add naive CPU implementation for memory-efficient attention backward
    Needs cleanup

--- xformers/components/attention/csrc/cpu/attention.cpp
+++ xformers/components/attention/csrc/cpu/attention.cpp
@@ -147,10 +147,189 @@ at::Tensor attention(
   return res;
 }
 
+template <typename scalar_t>
+void attention_backward_kernel(
+    at::TensorAccessor<scalar_t, 3> grad_q,
+    at::TensorAccessor<scalar_t, 3> grad_k,
+    at::TensorAccessor<scalar_t, 3> grad_v,
+    at::TensorAccessor<scalar_t, 3> grad_out,
+    at::TensorAccessor<scalar_t, 3> q,
+    at::TensorAccessor<scalar_t, 3> k,
+    at::TensorAccessor<scalar_t, 3> v,
+    at::TensorAccessor<scalar_t, 2> logsumexp_normalizer,
+    at::TensorAccessor<scalar_t, 3> buffer,
+    at::TensorAccessor<scalar_t, 3> buffer2,
+    at::TensorAccessor<scalar_t, 3> buffer3 //,
+    // at::TensorAccessor<int64_t, 2> mask
+) {
+  int64_t K = q.size(2);
+  int64_t B = q.size(0);
+  int64_t M = q.size(1);
+  int64_t N = k.size(1);
+  int64_t grain_size = 1; // buffer.size(1);
+  at::parallel_for(0, B, grain_size, [&](int64_t start, int64_t end) {
+    auto buf = buffer[at::get_thread_num()][0];
+    auto buf2 = buffer2[at::get_thread_num()];
+    auto buf3 = buffer3[at::get_thread_num()][0];
+    for (int64_t i = start; i < end; i++) {
+      for (int64_t l = 0; l < N; l++) {
+        for (int64_t k = 0; k < K; k++) {
+          buf2[l][k] = 0;
+        }
+      }
+
+      for (int64_t j = 0; j < M; j++) {
+        for (int64_t k = 0; k < K; k++) {
+          buf[k] = 0;
+        }
+        auto aar = q[i][j];
+        auto normalizer = logsumexp_normalizer[i][j];
+        scalar_t tmp_sum = 0;
+        for (int64_t l = 0; l < N; l++) {
+          auto bar = k[i][l];
+          scalar_t si = 0;
+          for (int64_t k = 0; k < K; k++) {
+            si += aar[k] * bar[k];
+          }
+          scalar_t attn_v = std::exp(si - normalizer);
+
+          for (int64_t k = 0; k < K; k++) {
+            grad_v[i][l][k] += attn_v * grad_out[i][j][k];
+          }
+
+          // now compute grad_q and grad_k
+          scalar_t g_attn = 0;
+          for (int64_t k = 0; k < K; k++) {
+            g_attn += grad_out[i][j][k] * v[i][l][k];
+            // g_attn[i][j][l] += grad_out[i][j][k] * v[i][l][k];
+          }
+          scalar_t tmp = attn_v * g_attn;
+          tmp_sum += tmp;
+
+          // grad_q is easy
+          for (int64_t k = 0; k < K; k++) {
+            grad_q[i][j][k] += tmp * bar[k]; // bar == key
+            buf[k] += attn_v * bar[k];
+          }
+
+          // scalar_t factor = tmp_sum / (tmp_sum - tmp);
+          // if (tmp_sum == tmp)
+          //  factor = 1;
+
+          //  but grad_k is trickier
+          for (int64_t k = 0; k < K; k++) {
+            grad_k[i][l][k] += tmp * aar[k];
+            // buf2[l][k] = buf2[l][k] * factor + attn_v * aar[k] * tmp_sum;
+          }
+        }
+        buf3[j] = tmp_sum;
+        for (int64_t k = 0; k < K; k++) {
+          grad_q[i][j][k] -= buf[k] * tmp_sum;
+        }
+      }
+
+      // TODO: try to make this folded in the previous loop
+      for (int64_t j = 0; j < M; j++) {
+        auto aar = q[i][j];
+        auto normalizer = logsumexp_normalizer[i][j];
+        scalar_t tmp = buf3[j];
+        for (int64_t l = 0; l < N; l++) {
+          auto bar = k[i][l];
+          scalar_t si = 0;
+          for (int64_t k = 0; k < K; k++) {
+            si += aar[k] * bar[k];
+          }
+          scalar_t attn_v = std::exp(si - normalizer);
+
+          for (int64_t k = 0; k < K; k++) {
+            buf2[l][k] += attn_v * aar[k] * tmp;
+          }
+        }
+      }
+
+      for (int64_t l = 0; l < N; l++) {
+        for (int64_t k = 0; k < K; k++) {
+          grad_k[i][l][k] -= buf2[l][k];
+        }
+      }
+    }
+  });
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> attention_backward(
+    const at::Tensor& grad_out,
+    const at::Tensor& query,
+    const at::Tensor& key,
+    const at::Tensor& value
+    // const at::Tensor& mask
+) {
+  TORCH_CHECK(query.dim() == key.dim());
+  // TORCH_CHECK(query.dim() == mask.dim());
+  TORCH_CHECK(query.dim() == 3);
+  TORCH_CHECK(query.size(2) == key.size(2));
+  TORCH_CHECK(query.size(0) == key.size(0));
+  // TORCH_CHECK(query.size(1) == mask.size(1));
+  // TORCH_CHECK(query.size(2) == mask.size(2));
+  // TORCH_CHECK(query.size(0) == mask.size(0));
+
+  /*
+  TORCH_CHECK(!a.is_cuda(), "a must be a CPU tensor");
+  TORCH_CHECK(!b.is_cuda(), "b must be a CPU tensor");
+  TORCH_CHECK(!mask.is_cuda(), "mask must be a CPU tensor");
+  TORCH_CHECK(!a.is_sparse(), "a must be a dense tensor");
+  TORCH_CHECK(!b.is_sparse(), "b must be a dense tensor");
+  //TORCH_CHECK(mask.is_sparse(), "mask must be a sparse tensor");
+  */
+
+  int64_t B = query.size(0);
+  int64_t M = query.size(1);
+  int64_t N = key.size(1);
+  int64_t K = query.size(2);
+
+  at::Tensor res = at::empty({B, M, K}, query.options());
+  at::Tensor grad_q = at::zeros_like(query);
+  at::Tensor grad_k = at::zeros_like(key);
+  at::Tensor grad_v = at::zeros_like(value);
+
+  int64_t grain_size = 32; // TODO: tune this
+  // at::Tensor buffer = at::empty({B, grain_size, K}, query.options());
+  at::Tensor buffer = at::empty({at::get_num_threads(), 1, K}, query.options());
+  at::Tensor buffer2 =
+      at::zeros({at::get_num_threads(), N, K + 1}, query.options());
+  at::Tensor buffer3 =
+      at::zeros({at::get_num_threads(), 1, M}, query.options());
+
+  // TODO this should be an argument from the function
+  at::Tensor logsumexp = query.bmm(key.transpose(-2, -1)).logsumexp(-1);
+
+  AT_DISPATCH_FLOATING_TYPES(
+      query.scalar_type(), "attention_backward_kernel", [&] {
+        attention_backward_kernel<scalar_t>(
+            grad_q.accessor<scalar_t, 3>(),
+            grad_k.accessor<scalar_t, 3>(),
+            grad_v.accessor<scalar_t, 3>(),
+            grad_out.accessor<scalar_t, 3>(),
+            query.accessor<scalar_t, 3>(),
+            key.accessor<scalar_t, 3>(),
+            value.accessor<scalar_t, 3>(),
+            logsumexp.accessor<scalar_t, 2>(),
+            buffer.accessor<scalar_t, 3>(),
+            buffer2.accessor<scalar_t, 3>(),
+            buffer3.accessor<scalar_t, 3>()
+            // idxs.accessor<int64_t, 2>()
+        );
+      });
+
+  return std::make_tuple(grad_q, grad_k, grad_v);
+}
+
 } // namespace
 
 TORCH_LIBRARY_IMPL(xformers, CPU, m) {
   m.impl(
       TORCH_SELECTIVE_NAME("xformers::efficient_attention"),
       TORCH_FN(attention));
+  m.impl(
+      TORCH_SELECTIVE_NAME("xformers::efficient_attention_backward"),
+      TORCH_FN(attention_backward));
 }
