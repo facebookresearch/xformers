476aa58596a7e846f60ef741f1511e23b412344a
blefaudeux <blefaudeux@users.noreply.github.com>
2022-04-21 16:53:13 +0000

    deploy: b9f21114824c676b4c5d72df87117ac2adfb7d36

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.10',
+    VERSION: '0.0.11.dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
