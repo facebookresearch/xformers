b9a2a2e818f259bd6465229ae004f7bde1d9a632
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2022-05-30 14:26:46 -0700

    [chore] Release v0.0.11, switch version to 0.0.12.dev (#313)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ copyright = "Copyright © 2021 Meta Platforms, Inc"
 author = "Facebook AI Research"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.11.dev"
+release = "0.0.12.dev"
 
 # -- General configuration ---------------------------------------------------
 
