ee2954066fced90a8ae2ce9a64ab44916c352ce6
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-06-01 15:20:42 -0700

    new commit to trigger codecov

--- xformers/triton/layer_norm.py
+++ xformers/triton/layer_norm.py
@@ -190,12 +190,11 @@ class FusedLayerNorm(nn.Module):
         return layer_norm(x, self.weight, self.bias, self.epsilon)
 
     def init_weights(self, *args, **kwargs):
-        with torch.no_grad():
-            if self.weight is not None:
-                self.weight.fill_(1.0)
+        if self.weight is not None:
+            self.weight.fill_(1.0)
 
-            if self.bias is not None:
-                self.bias.fill_(0.0)
+        if self.bias is not None:
+            self.bias.fill_(0.0)
 
 
 def layer_norm(
