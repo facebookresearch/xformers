b942065a48a9e107000305756fe0aba400fcaa8c
igormolybogFB <106693515+igormolybogFB@users.noreply.github.com>
2022-06-01 18:59:23 -0700

    Fix block_size for Dense SC

--- xformers/components/attention/sparsity_config.py
+++ xformers/components/attention/sparsity_config.py
@@ -76,18 +76,20 @@ class DenseSparsityConfig(SparsityConfig):
     comprehension.
     """
 
-    def __init__(self, num_heads, block=16, different_layout_per_head=False):
+    def __init__(self, num_heads, block_size=16, different_layout_per_head=False):
         """Initialize the Dense Sparsity Pattern Config.
         In reality, this is not sparse and all blocks are used. We keep it for the sake of comparison
         and comprehension.
         Arguments:
              num_heads: required: an integer determining number of attention heads of the layer.
-             seq_len: required: an integer determining number of attention heads of the layer.
+             block_size: optional: an integer determining the block size. Current implementation of
+             sparse self-attention is based on blocked sparse matrices. In which this parameter
+             defines size of such blocks, `Block X Block`.
              different_layout_per_head: optional: this is just for the sake of consistency with
              other sparsity formats; can ignore it for DenseSparsityConfig
         """
 
-        super().__init__(num_heads, block, different_layout_per_head)
+        super().__init__(num_heads, block_size, different_layout_per_head)
 
     def make_layout(self, seq_len):
         """Set 1 to all blocks of the layout meanins the pattern is dense; not sparse.
