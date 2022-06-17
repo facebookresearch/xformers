d21a4383f7fb3f9a5d14b772b0234ce82efe5ae5
Francisco Massa <fvsmassa@gmail.com>
2022-06-17 11:09:33 -0700

    Add new kernel for memory-efficient attention
    Based on top of the sparse variant.
    
    So far it gives competitive results compared to the cutlass-based
    kernel.
    On a V100, we have (new column):
    [---------------- attention (use_attn_bias=False) -----------------]
                                    |  optimized  |  vanilla  |    new
    1 threads: ---------------------------------------------------------
          f32 B=32, M=128, K=32     |      44.4   |    125.2  |     31.8
          f32 B=32, M=128, K=64     |      44.1   |    124.9  |     44.5
          f32 B=32, M=128, K=128    |      57.6   |    125.5  |     59.2
          f32 B=32, M=128, K=256    |     109.4   |    129.3  |    108.6
          f32 B=32, M=512, K=32     |     389.7   |    250.5  |    318.8
          f32 B=32, M=512, K=64     |     431.8   |    310.0  |    475.7
          f32 B=32, M=512, K=128    |     628.8   |    486.7  |    695.7
          f32 B=32, M=512, K=256    |    1199.7   |    863.5  |   1412.1
          f32 B=32, M=1024, K=32    |    1314.1   |    815.9  |   1229.4
          f32 B=32, M=1024, K=64    |    1527.9   |   1113.1  |   1728.7
          f32 B=32, M=1024, K=128   |    2189.2   |   1788.1  |   2367.4
          f32 B=32, M=1024, K=256   |    4370.7   |   3132.0  |   4674.8
          f32 B=256, M=128, K=32    |     172.0   |    142.8  |    167.4
          f32 B=256, M=128, K=64    |     219.7   |    199.7  |    254.8
          f32 B=256, M=128, K=128   |     325.3   |    329.4  |    373.9
          f32 B=256, M=128, K=256   |     695.9   |    611.6  |    767.7
          f32 B=256, M=512, K=32    |    2383.8   |   1922.6  |   2382.1
          f32 B=256, M=512, K=64    |    2938.5   |   2243.0  |   3273.4
          f32 B=256, M=512, K=128   |    4226.9   |   3585.0  |   4670.7
          f32 B=256, M=512, K=256   |    8337.1   |   6289.1  |   9515.0
          f32 B=256, M=1024, K=32   |    9116.5   |   7651.5  |   9302.7
          f32 B=256, M=1024, K=64   |   11359.6   |   8920.2  |  12516.9
          f32 B=256, M=1024, K=128  |   16419.0   |  14365.4  |  17816.7
          f32 B=256, M=1024, K=256  |   32120.3   |  25055.4  |  36434.2
    
    Times are in microseconds (us).

---
+++ xformers/components/attention/csrc/cuda/mem_eff_attention/attention2.cu
@@ -0,0 +1,371 @@
+#include <ATen/ATen.h>
+#include <torch/library.h>
+#include <cmath>
+#include <vector>
+
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAGeneratorImpl.h>
+#include <c10/cuda/CUDAGuard.h>
+#include <ATen/cuda/Atomic.cuh>
+#include <ATen/cuda/CUDAGraphsUtils.cuh>
+
+#include <curand_kernel.h>
+
+#include "sputnik/vector_utils.h"
+
+namespace {
+
+template <typename integer>
+constexpr __host__ __device__ inline integer ceil_div(integer n, integer m) {
+  return (n + m - 1) / m;
+}
+
+template <typename scalar_t>
+__device__ __forceinline__ void iMul(scalar_t x1, float4* out) {
+  out[0].x *= x1;
+  out[0].y *= x1;
+  out[0].z *= x1;
+  out[0].w *= x1;
+}
+
+template <typename scalar_t>
+__device__ __forceinline__ void iMul(scalar_t x1, float2* out) {
+  out[0].x *= x1;
+  out[0].y *= x1;
+}
+
+template <typename scalar_t>
+__device__ __forceinline__ void iMul(scalar_t x1, float* out) {
+  out[0] *= x1;
+}
+
+template <typename scalar_t, int WARP_SIZE>
+__device__ __forceinline__ scalar_t warpSum(scalar_t val) {
+  for (int stride = WARP_SIZE / 2; stride > 0; stride >>= 1) {
+    val += __shfl_xor_sync(0xffffffff, val, stride, WARP_SIZE);
+  }
+  return val;
+}
+
+template <typename scalar_t, int WARP_SIZE>
+__device__ __forceinline__ float2 warpSum(float2 val) {
+  for (int stride = WARP_SIZE / 2; stride > 0; stride >>= 1) {
+    val.x += __shfl_xor_sync(0xffffffff, val.x, stride, WARP_SIZE);
+    val.y += __shfl_xor_sync(0xffffffff, val.y, stride, WARP_SIZE);
+  }
+  return val;
+}
+
+template <typename scalar_t, int WARP_SIZE>
+__device__ __forceinline__ float4 warpSum(float4 val) {
+  for (int stride = WARP_SIZE / 2; stride > 0; stride >>= 1) {
+    val.x += __shfl_xor_sync(0xffffffff, val.x, stride, WARP_SIZE);
+    val.y += __shfl_xor_sync(0xffffffff, val.y, stride, WARP_SIZE);
+    val.z += __shfl_xor_sync(0xffffffff, val.z, stride, WARP_SIZE);
+    val.w += __shfl_xor_sync(0xffffffff, val.w, stride, WARP_SIZE);
+  }
+  return val;
+}
+
+template <typename scalar_t, int WARP_SIZE>
+__device__ __forceinline__ scalar_t warpMax(scalar_t val) {
+  for (int stride = WARP_SIZE / 2; stride > 0; stride >>= 1) {
+    scalar_t tmp = __shfl_xor_sync(0xffffffff, val, stride, WARP_SIZE);
+    val = tmp > val ? tmp : val;
+  }
+  return val;
+}
+
+template <
+    typename scalar_t,
+    int kBlockWidth,
+    int kBlockItemsY,
+    int kBlockItemsX,
+    int kBlockItemsK>
+__global__ void attention_kernel(
+    at::PackedTensorAccessor<scalar_t, 3> output,
+    at::PackedTensorAccessor<scalar_t, 3> query,
+    at::PackedTensorAccessor<scalar_t, 3> key,
+    at::PackedTensorAccessor<scalar_t, 3> value) {
+  int64_t K = query.size(2);
+  int64_t B = query.size(0);
+  int64_t M = query.size(1);
+  int64_t N = key.size(1);
+
+  int64_t batch_idx = blockIdx.y;
+  int64_t query_idx = blockIdx.x * kBlockItemsY + threadIdx.y;
+
+  if (query_idx >= M)
+    return;
+
+  using vec_t = float4;
+
+  constexpr int kVecSize = sizeof(vec_t) / sizeof(scalar_t);
+
+  constexpr int kThreadItemsK = kBlockItemsK / kBlockWidth / kVecSize;
+  constexpr int kThreadItemsX = kBlockItemsX / kBlockWidth / kVecSize;
+
+  int KK = K / kVecSize;
+
+  scalar_t s_prime = 0;
+  scalar_t m_prime = -std::numeric_limits<scalar_t>::infinity();
+
+  vec_t lhs_fragment[kBlockItemsK / kBlockWidth / kVecSize];
+  vec_t rhs_fragment[kBlockItemsK * kBlockItemsX / kBlockWidth / kVecSize];
+
+  auto out_i = reinterpret_cast<vec_t*>(output[batch_idx][query_idx].data());
+  int nnz = N;
+
+  scalar_t scale = 1.0 / std::sqrt(scalar_t(K));
+
+  for (int kv_idx = 0; kv_idx < nnz; kv_idx += kBlockItemsX) {
+    scalar_t attn_fragment[kBlockItemsX] = {};
+
+    auto query_i = reinterpret_cast<vec_t*>(query[batch_idx][query_idx].data());
+    auto kkey_j = reinterpret_cast<vec_t*>(key[batch_idx].data());
+
+    /*
+    int nonzeros = min(nnz - kv_idx, kBlockItemsX) - threadIdx.x;
+    int* column_idxs_tile = col_idx + kv_idx + threadIdx.x;
+    int* ci = column_indices_tile + threadIdx.x;
+#pragma unroll
+    for (int x_item_idx = 0; x_item_idx < kBlockItemsX;
+         x_item_idx += kBlockWidth) {
+      if (nonzeros > 0) {
+        *ci = *column_idxs_tile * KK;
+      } else {
+        *ci = 0;
+      }
+      nonzeros -= kBlockWidth;
+      column_idxs_tile += kBlockWidth;
+      ci += kBlockWidth;
+    }
+    __syncthreads();*/
+
+    for (int k = K; k >= kBlockItemsK; k -= kBlockItemsK) {
+      // load queries
+#pragma unroll
+      for (int k_item_idx = 0; k_item_idx < kThreadItemsK; ++k_item_idx) {
+        lhs_fragment[k_item_idx] = __ldg(query_i + threadIdx.x);
+        iMul(scale, lhs_fragment + k_item_idx);
+        query_i += kBlockWidth;
+      }
+
+      // load keys
+#pragma unroll
+      for (int x_item_idx = 0; x_item_idx < kBlockItemsX; ++x_item_idx) {
+        //int offset = column_indices_tile[x_item_idx];
+        int offset = (kv_idx + x_item_idx) * KK;
+        auto key_j = kkey_j + offset;
+#pragma unroll
+        for (int k_item_idx = 0; k_item_idx < kThreadItemsK; ++k_item_idx) {
+          int fragment_offset = x_item_idx * kThreadItemsK + k_item_idx;
+          rhs_fragment[fragment_offset] = __ldg(key_j + threadIdx.x);
+          key_j += kBlockWidth;
+        }
+      }
+      kkey_j += kBlockItemsK / kVecSize;
+
+#pragma unroll
+      for (int k_item_idx = 0; k_item_idx < kThreadItemsK; ++k_item_idx) {
+        const vec_t lhs_value = lhs_fragment[k_item_idx];
+#pragma unroll
+        for (int x_item_idx = 0; x_item_idx < kBlockItemsX; ++x_item_idx) {
+          const vec_t rhs_value =
+              rhs_fragment[k_item_idx + x_item_idx * kThreadItemsK];
+          sputnik::VectorCompute<vec_t>::Dot(
+              lhs_value, rhs_value, attn_fragment + x_item_idx);
+        }
+      }
+    }
+
+    int end_iter = nnz - kv_idx;
+    scalar_t m_i = m_prime;
+
+#pragma unroll
+    for (int x_item_idx = 0; x_item_idx < kBlockItemsX; ++x_item_idx) {
+      attn_fragment[x_item_idx] =
+          warpSum<scalar_t, kBlockWidth>(attn_fragment[x_item_idx]);
+
+      if (x_item_idx >= end_iter)
+        attn_fragment[x_item_idx] =
+            -std::numeric_limits<scalar_t>::infinity();
+
+      m_i = max(attn_fragment[x_item_idx], m_i);
+    }
+
+    scalar_t m_delta = std::exp(m_prime - m_i);
+    m_delta = isfinite(m_delta) ? m_delta : scalar_t(0);
+    s_prime = s_prime * m_delta;
+    m_prime = m_i;
+    for (int x_item_idx = 0; x_item_idx < kBlockItemsX; x_item_idx++) {
+      attn_fragment[x_item_idx] = std::exp(attn_fragment[x_item_idx] - m_i);
+      attn_fragment[x_item_idx] = isfinite(attn_fragment[x_item_idx]) ? attn_fragment[x_item_idx] : scalar_t(0);
+      s_prime += attn_fragment[x_item_idx];
+    }
+
+    vec_t* out_i_tmp = out_i;
+    auto value_j = reinterpret_cast<vec_t*>(value[batch_idx].data());
+    for (int k = K; k >= kBlockItemsK; k -= kBlockItemsK) {
+      // load output
+#pragma unroll
+      for (int k_item_idx = 0; k_item_idx < kThreadItemsK; ++k_item_idx) {
+        lhs_fragment[k_item_idx] = __ldg(out_i_tmp + threadIdx.x);
+        iMul(m_delta, lhs_fragment + k_item_idx);
+      }
+
+      // load values
+#pragma unroll
+      for (int x_item_idx = 0; x_item_idx < kBlockItemsX; ++x_item_idx) {
+        //int offset = column_indices_tile[x_item_idx];
+        int offset = (kv_idx + x_item_idx) * KK;
+        auto key_j = value_j + offset;
+#pragma unroll
+        for (int k_item_idx = 0; k_item_idx < kThreadItemsK; ++k_item_idx) {
+          int fragment_offset = x_item_idx * kThreadItemsK + k_item_idx;
+          rhs_fragment[fragment_offset] = __ldg(key_j + threadIdx.x);
+          key_j += kBlockWidth;
+        }
+      }
+      value_j += kBlockItemsK / kVecSize;
+
+      // perform computation
+#pragma unroll
+      for (int k_item_idx = 0; k_item_idx < kThreadItemsK; ++k_item_idx) {
+        vec_t lhs_value = lhs_fragment[k_item_idx];
+#pragma unroll
+        for (int x_item_idx = 0; x_item_idx < kBlockItemsX; ++x_item_idx) {
+          const vec_t rhs_value =
+              rhs_fragment[k_item_idx + x_item_idx * kThreadItemsK];
+          sputnik::VectorCompute<vec_t>::FMA(
+              attn_fragment[x_item_idx], rhs_value, &lhs_value);
+        }
+        out_i_tmp[threadIdx.x] = lhs_value;
+      }
+      out_i_tmp += kBlockWidth;
+    }
+  }
+  // avoid division by 0 when row is fully masked
+  if (s_prime > 0)
+    s_prime = 1.0 / s_prime;
+  for (int k = threadIdx.x; k < KK; k += blockDim.x) {
+    iMul(s_prime, out_i + k);
+  }
+}
+
+at::Tensor attention(
+    const at::Tensor& query,
+    const at::Tensor& key,
+    const at::Tensor& value) {
+
+  TORCH_CHECK(query.dim() == key.dim());
+  TORCH_CHECK(query.dim() == value.dim());
+  TORCH_CHECK(query.dim() == 3);
+  TORCH_CHECK(query.size(2) == key.size(2));
+  TORCH_CHECK(query.size(0) == key.size(0));
+
+  TORCH_CHECK(query.size(0) == value.size(0));
+  TORCH_CHECK(key.size(1) == value.size(1));
+  TORCH_CHECK(
+      query.size(2) ==
+      value.size(2)); // TODO: drop this limitation in the future
+
+  // TODO drop this limitation in the future
+  TORCH_CHECK(query.is_contiguous());
+  TORCH_CHECK(key.is_contiguous());
+  TORCH_CHECK(value.is_contiguous());
+
+  // TODO: support other dtypes in the future
+  TORCH_CHECK(
+      query.scalar_type() == at::ScalarType::Float,
+      "Only float32 type is supported for now");
+
+  at::cuda::CUDAGuard device_guard(query.device());
+
+  int64_t B = query.size(0);
+  int64_t M = query.size(1);
+  int64_t N = key.size(1);
+  int64_t K = query.size(2);
+
+  TORCH_CHECK(K % 32 == 0, "For now only K % 32 == 0 is supported. Let us know if you hit this and we will fix it");
+
+  at::Tensor res = at::zeros({B, M, K}, query.options());
+
+  using scalar_t = float;
+
+  if (K % 128 == 0) {
+  constexpr int kBlockItemsY = 4;
+  constexpr int kBlockItemsX = 32;
+  constexpr int kBlockItemsK = 128;
+  constexpr int kBlockWidth = 8;
+
+  dim3 grid(ceil_div(M, int64_t(kBlockItemsY)), B);
+  dim3 block(kBlockWidth, kBlockItemsY);
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+  attention_kernel<
+      scalar_t,
+      kBlockWidth,
+      kBlockItemsY,
+      kBlockItemsX,
+      kBlockItemsK><<<grid, block, 0, stream>>>(
+      res.packed_accessor<scalar_t, 3>(),
+      query.packed_accessor<scalar_t, 3>(),
+      key.packed_accessor<scalar_t, 3>(),
+      value.packed_accessor<scalar_t, 3>());
+
+  } else if (K % 64 == 0) {
+  constexpr int kBlockItemsY = 4;
+  constexpr int kBlockItemsX = 32;
+  constexpr int kBlockItemsK = 64;
+  constexpr int kBlockWidth = 8;
+
+  dim3 grid(ceil_div(M, int64_t(kBlockItemsY)), B);
+  dim3 block(kBlockWidth, kBlockItemsY);
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+  attention_kernel<
+      scalar_t,
+      kBlockWidth,
+      kBlockItemsY,
+      kBlockItemsX,
+      kBlockItemsK><<<grid, block, 0, stream>>>(
+      res.packed_accessor<scalar_t, 3>(),
+      query.packed_accessor<scalar_t, 3>(),
+      key.packed_accessor<scalar_t, 3>(),
+      value.packed_accessor<scalar_t, 3>());
+
+  } else {
+  constexpr int kBlockItemsY = 4;
+  constexpr int kBlockItemsX = 32;
+  constexpr int kBlockItemsK = 32;
+  constexpr int kBlockWidth = 8;
+
+  dim3 grid(ceil_div(M, int64_t(kBlockItemsY)), B);
+  dim3 block(kBlockWidth, kBlockItemsY);
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+  attention_kernel<
+      scalar_t,
+      kBlockWidth,
+      kBlockItemsY,
+      kBlockItemsX,
+      kBlockItemsK><<<grid, block, 0, stream>>>(
+      res.packed_accessor<scalar_t, 3>(),
+      query.packed_accessor<scalar_t, 3>(),
+      key.packed_accessor<scalar_t, 3>(),
+      value.packed_accessor<scalar_t, 3>());
+  }
+
+  AT_CUDA_CHECK(cudaGetLastError());
+
+  return res;
+}
+
+} // namespace
+
+TORCH_LIBRARY_IMPL(xformers, CUDA, m) {
+  m.impl(
+      TORCH_SELECTIVE_NAME("xformers::efficient_attention2"),
+      TORCH_FN(attention));
+}
