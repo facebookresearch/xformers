a104b97dd21488b57a00d8582d24c113134b8734
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-08-07 23:37:30 +0200

    small test editing, not sure that it's xformers' scope really, this is triton op parity with torch

--- tests/test_triton_fused_linear.py
+++ tests/test_triton_fused_linear.py
@@ -14,11 +14,12 @@ from xformers.components import Activation, build_activation
 _triton_available = torch.cuda.is_available()
 if _triton_available:
     try:
+        from triton.testing import assert_almost_equal
+
         from xformers.triton import FusedLinear
         from xformers.triton.k_activations import get_triton_activation_kernel
         from xformers.triton.k_fused_matmul_fw import fused_matmul
         from xformers.triton.utils import gpu_capabilities_older_than_70
-
     except ImportError:
         logging.warning(
             "Triton is not available, some optimizations will not be tested."
@@ -34,12 +35,14 @@ SHAPES = [(128, 256), (8, 384, 128), (8, 784, 512)]
     reason="Triton requires a SM70+ GPU",
 )
 @pytest.mark.parametrize("shape", SHAPES)
-@pytest.mark.parametrize(
-    "dtype", [torch.float32]
-)  # Triton use tensor cores, which return slightly different results to pytorch mm
+@pytest.mark.parametrize("dtype", [torch.float16])
 def test_fused_matmul(shape, dtype):
     """Check that the matrix multiply kernel and Pytorch's give the same results"""
     torch.random.manual_seed(0)
+
+    # Force pytorch to use TF32 accumulators (same as Triton)
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True  # type: ignore
 
     # Raw fused matrix multiply first, to catch gross errors
     a = torch.rand((shape[-2], shape[-1]), dtype=dtype, device="cuda")
@@ -48,20 +51,27 @@ def test_fused_matmul(shape, dtype):
     # Test that not passing any bias is fine
     res_torch = a @ b
     res_triton, _ = fused_matmul(a, b.transpose(0, 1).contiguous(), None)
-    assert torch.allclose(res_torch, res_triton), "Vanilla matmul is broken"
+
+    assert_almost_equal(
+        res_torch, res_triton, err_msg="Vanilla matmul is broken", decimal=1
+    )
 
     # Now test with a real FMA
     c = -torch.rand((shape[-2],), dtype=dtype, device="cuda")
     res_torch = torch.addmm(c, a, b)
     res_triton, _ = fused_matmul(a, b.transpose(1, 0).contiguous(), c)
 
-    assert torch.allclose(
-        res_torch, res_triton
-    ), f"Vanilla fused matmul is broken {torch.max(torch.abs(res_torch-res_triton)).item()}"
+    assert_almost_equal(
+        res_torch,
+        res_triton,
+        err_msg=f"Vanilla fused matmul is broken {torch.max(torch.abs(res_torch-res_triton)).item()}",
+        decimal=1,
+    )
 
     # Now check that adding an activation to the mix still produces valid results
     for activation in Activation:
         torch_activation = build_activation(activation.value)
+        a /= a.numel()
         res_torch = torch_activation(torch.addmm(c, a, b))
 
         triton_activation = get_triton_activation_kernel(activation)
@@ -69,15 +79,13 @@ def test_fused_matmul(shape, dtype):
             a, b.transpose(1, 0).contiguous(), c, triton_activation
         )
 
-        # NOTE: @lefaudeux
-        # GeLUs are not well handled for now, we use an approximation
-        # they're also slower than pytorch so not likely to be used
-        # Issue tracked with https://github.com/fairinternal/xformers/issues/238
-        tol = 1e-6 if activation != Activation.GeLU else 1e-2
-
-        assert torch.allclose(
-            res_torch, res_triton, atol=tol
-        ), f"Fused matmul broken with activation {activation}. Max diff: {torch.max(torch.abs(res_torch - res_triton))}"
+        assert_almost_equal(
+            res_torch,
+            res_triton,
+            err_msg=f"Fused matmul broken with activation {activation}. Max diff: \
+                {torch.max(torch.abs(res_torch - res_triton))}",
+            decimal=1,
+        )
 
 
 @pytest.mark.skipif(
@@ -93,6 +101,11 @@ def test_fused_linear_parity(shape, activation: Activation, bias: bool, amp: boo
 
     # Instantiate pytorch and fused layers, same initialization
     torch.random.manual_seed(0)
+
+    # Force pytorch to use TF32 accumulators (same as Triton)
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True  # type: ignore
+
     X = torch.normal(0, 1, size=shape, device="cuda")
     X.requires_grad_()
 
@@ -117,14 +130,10 @@ def test_fused_linear_parity(shape, activation: Activation, bias: bool, amp: boo
     torch_linear.zero_grad()
     triton_fused_linear.zero_grad()
 
-    assert torch.allclose(
-        triton_fused_linear.weight, torch_linear.weight
-    ), "Broken test setup"
-    assert torch.allclose(X, X_), "Broken test setup"
+    assert_almost_equal(triton_fused_linear.weight, torch_linear.weight)
+    assert_almost_equal(X, X_)
 
     with autocast(enabled=amp):
-        tolerance = 1e-3 if not amp else 1e-2
-
         y_torch = torch_sequence(X)
         y_triton = triton_fused_linear(X_)
 
@@ -135,13 +144,11 @@ def test_fused_linear_parity(shape, activation: Activation, bias: bool, amp: boo
         loss_triton = torch.norm(y_triton)
         loss_triton.backward()
 
-        assert torch.allclose(X, X_, atol=tolerance), f"{X} vs. {X_}"
+        assert_almost_equal(X, X_, err_msg=f"{X} vs. {X_}")
 
         # Input grad being correct checks both the loss + some of the backward pass
         assert X.grad is not None and X_.grad is not None
-        assert torch.allclose(
-            X.grad, X_.grad, atol=tolerance
-        ), f"{X.grad} vs. {X_.grad}"
+        assert_almost_equal(X.grad, X_.grad, err_msg=f"{X.grad} vs. {X_.grad}")
 
         # Check that the linear layer bias are also properly trainable
         if bias:
@@ -150,17 +157,19 @@ def test_fused_linear_parity(shape, activation: Activation, bias: bool, amp: boo
                 and triton_fused_linear.bias.grad is not None
             )
             assert torch_linear.bias is not None and torch_linear.bias.grad is not None
-            assert torch.allclose(
-                torch_linear.bias.grad, triton_fused_linear.bias.grad, atol=tolerance
-            ), f"{torch_linear.bias.grad} vs. {triton_fused_linear.bias.grad}"
+            assert_almost_equal(
+                torch_linear.bias.grad,
+                triton_fused_linear.bias.grad,
+                err_msg=f"{torch_linear.bias.grad} vs. {triton_fused_linear.bias.grad}",
+            )
 
         # Check that the linear layer weights are also properly trainable
         assert (
             torch_linear.weight.grad is not None
             and triton_fused_linear.weight.grad is not None
         )
-        assert torch.allclose(
+        assert_almost_equal(
             torch_linear.weight.grad,
             triton_fused_linear.weight.grad,
-            atol=tolerance,
-        ), f"{torch_linear.weight.grad} vs. {triton_fused_linear.weight.grad}"
+            err_msg=f"{torch_linear.weight.grad} vs. {triton_fused_linear.weight.grad}",
+        )
