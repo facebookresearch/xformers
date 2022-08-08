738d4d901e7ad2096dafaca0f1339d00ee522d5c
Diana Liskovich <dianaml@devfair0471.h2.fair>
2022-08-08 11:23:27 -0700

    update to 13.dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ copyright = "Copyright © 2021 Meta Platforms, Inc"
 author = "Facebook AI Research"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.12.dev"
+release = "0.0.13.dev"
 
 # -- General configuration ---------------------------------------------------
 
--- xformers/__init__.py
+++ xformers/__init__.py
@@ -8,7 +8,7 @@ import logging
 import torch
 
 # Please update the doc version in docs/source/conf.py as well.
-__version__ = "0.0.12.dev"
+__version__ = "0.0.13.dev"
 
 _is_sparse_available: bool = True
 _is_triton_available: bool = torch.cuda.is_available()
