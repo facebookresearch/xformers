276bd0ab7d7b009148f1faeb97ec0ae80ee5c78c
Simon Rouard <74627808+simonrouard@users.noreply.github.com>
2022-08-25 16:11:54 +0200

    Make the contiguous a bit more efficient

--- xformers/sparse/csr_tensor.py
+++ xformers/sparse/csr_tensor.py
@@ -183,9 +183,11 @@ class SparseCSRTensor(torch.Tensor):
         row_indices = mask.__row_indices
         row_offsets = mask.__row_offsets
         column_indices = mask.__column_indices
+        a = a.contiguous()
+        b = b.contiguous()
         out = _csr_ops._sddmm.apply(
-            a.contiguous(),
-            b.transpose(-2, -1).contiguous(),
+            a,
+            b.transpose(-2, -1),
             row_indices,
             row_offsets,
             column_indices,
