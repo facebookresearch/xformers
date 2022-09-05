6018600ec16246e074b672fd4e25718562b16245
danthe3rd <danthe3rd>
2022-09-05 14:58:44 +0000

    Group all archs in the same file

---
+++ xformers/components/attention/csrc/cuda/mem_eff_attention/kernels/backward_f16_aligned.cu
@@ -0,0 +1,6 @@
+// This file is auto-generated. See "generate_kernels.sh"
+#include "../kernel_backward.h"
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM50(cutlass::half_t, true);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM70(cutlass::half_t, true);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM75(cutlass::half_t, true);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM80(cutlass::half_t, true);
--- xformers/components/attention/csrc/cuda/mem_eff_attention/kernels/backward_f32_sm75_aligned.cu
+++
@@ -1,3 +0,0 @@
-// This file is auto-generated. See "generate_kernels.sh"
-#include "../kernel_backward.h"
-INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM75(float, true);
