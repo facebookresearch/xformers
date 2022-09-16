d796d5b34aaae292fc2ffeea4be46bdd4a2155fc
danthe3rd <danthe3rd>
2022-09-16 14:53:09 +0000

    Update base for Update on "BW: Store dK/dV in registers when possible"
    [ghstack-poisoned]

---
+++ xformers/components/attention/csrc/cuda/mem_eff_attention/kernels/backward_f32_k128.cu
@@ -0,0 +1,6 @@
+// This file is auto-generated. See "generate_kernels.sh"
+#include "../kernel_backward.h"
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM50(float, false, 128);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM70(float, false, 128);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM75(float, false, 128);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM80(float, false, 128);
