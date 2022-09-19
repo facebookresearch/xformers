8861e63d9068dd21ad8676aaf2fabe7929595887
danthe3rd <danthe3rd>
2022-09-19 13:22:55 +0000

    Update base for Update on "Pipeline loads in epilogue (~5% speedup)"
    [ghstack-poisoned]

--- xformers/components/attention/csrc/cuda/mem_eff_attention/kernel_backward.h
+++ xformers/components/attention/csrc/cuda/mem_eff_attention/kernel_backward.h
@@ -876,6 +876,16 @@ struct AttentionBackwardKernel {
                            : std::min(
                                  (int32_t)MatmulQK::Mma::Shape::kN,
                                  p.num_queries - query_start));
+      auto createEpilogueIter = [&]() {
+        return typename MatmulGradK::OutputTileIterator(
+            typename MatmulGradK::OutputTileIterator::Params{p.head_dim},
+            p.grad_key_ptr + key_start * p.head_dim + col,
+            {skipBoundsChecks
+                 ? MatmulGradK::ThreadblockShape::kM
+                 : std::min((int32_t)Mma::Shape::kM, p.num_keys - key_start),
+             false ? MatmulGradK::ThreadblockShape::kN : p.head_dim - col},
+            thread_id);
+      };
 
       // q_i
       typename Mma::IteratorB iterator_B(
@@ -895,6 +905,7 @@ struct AttentionBackwardKernel {
 
       if (!kOutputInRF) {
         output_frags.gradK.clear();
+        createEpilogueIter().prefetch_all();
       }
 
       auto gemm_k_iterations =
@@ -910,18 +921,10 @@ struct AttentionBackwardKernel {
 
       // Output results
       if (!kOutputInRF) {
-        typename MatmulGradK::OutputTileIterator output_it(
-            typename MatmulGradK::OutputTileIterator::Params{p.head_dim},
-            p.grad_key_ptr + key_start * p.head_dim + col,
-            {skipBoundsChecks
-                 ? MatmulGradK::ThreadblockShape::kM
-                 : std::min((int32_t)Mma::Shape::kM, p.num_keys - key_start),
-             false ? MatmulGradK::ThreadblockShape::kN : p.head_dim - col},
-            thread_id);
         accumulateInGmem<MatmulGradK>(
             shared_storage.after_qk.after_doivj.mm_gradK.epilogue,
             output_frags.gradK,
-            output_it,
+            createEpilogueIter(),
             query_start == 0);
       }
     }
