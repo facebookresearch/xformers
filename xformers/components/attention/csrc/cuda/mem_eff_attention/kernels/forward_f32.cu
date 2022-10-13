933804c987b8681740f7bcbf2b3da5424cc2d4c6
danthe3rd <danthe3rd>
2022-10-13 13:03:46 +0000

    Enable selective build for mem-efficient kernels

--- xformers/components/attention/csrc/cuda/mem_eff_attention/kernels/forward_f32.cu
+++ xformers/components/attention/csrc/cuda/mem_eff_attention/kernels/forward_f32.cu
@@ -1,4 +1,5 @@
 // This file is auto-generated. See "generate_kernels.sh"
+#ifndef XFORMERS_MEM_EFF_ATTENTION_DISABLE_FORWARD
 #include "../kernel_forward.h"
 INSTANTIATE_ATTENTION_KERNEL_FORWARD_SM50(float, false, 32, 128, true);
 INSTANTIATE_ATTENTION_KERNEL_FORWARD_SM50(float, false, 32, 128, false);
@@ -12,3 +13,4 @@ INSTANTIATE_ATTENTION_KERNEL_FORWARD_SM75(float, false, 64, 64, true);
 INSTANTIATE_ATTENTION_KERNEL_FORWARD_SM80(float, false, 32, 128, true);
 INSTANTIATE_ATTENTION_KERNEL_FORWARD_SM80(float, false, 32, 128, false);
 INSTANTIATE_ATTENTION_KERNEL_FORWARD_SM80(float, false, 64, 64, true);
+#endif
