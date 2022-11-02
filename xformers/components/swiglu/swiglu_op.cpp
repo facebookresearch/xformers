bdede235231001dcea97f95c2868b5b582e4aaf9
danthe3rd <danthe3rd>
2022-11-02 19:57:39 +0000

    Update base for Update on "Update FlashAttention"
    cc tridao
    
    
    [ghstack-poisoned]

--- xformers/components/swiglu/swiglu_op.cpp
+++ xformers/components/swiglu/swiglu_op.cpp
@@ -2,7 +2,7 @@
 
 TORCH_LIBRARY_FRAGMENT(xformers, m) {
   m.def(TORCH_SELECTIVE_SCHEMA(
-      "xformers::dual_gemm_silu_identity_mul(Tensor x, Tensor w1, Tensor b1, Tensor w2, Tensor b2) -> (Tensor, Tensor, Tensor)"));
+      "xformers::dual_gemm_silu_identity_mul(Tensor x, Tensor w1, Tensor? b1, Tensor w2, Tensor? b2) -> (Tensor, Tensor, Tensor)"));
   m.def(TORCH_SELECTIVE_SCHEMA(
       "xformers::silu_bw_fused(Tensor x1, Tensor x2, Tensor dx4) -> (Tensor, Tensor)"));
   m.def(TORCH_SELECTIVE_SCHEMA(
