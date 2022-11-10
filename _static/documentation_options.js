a2f5426897bf442b006514bcac4427f2e39fc963
danthe3rd <danthe3rd@users.noreply.github.com>
2022-11-10 18:13:41 +0000

    deploy: 836768506d990fa3985dafd69a0b4518b3ae12bd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.14.dev',
+    VERSION: '0.0.15.dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
