1370362f09100cd5bcc413da16002acf65bea5f8
danthe3rd <danthe3rd>
2022-12-08 13:51:39 +0000

    Update base for Update on "Refactor5: Generic way of registring operators for xformers.info"
    [ghstack-poisoned]

--- xformers/ops/fmha/common.py
+++ xformers/ops/fmha/common.py
@@ -103,11 +103,16 @@ class Context:
     lse: torch.Tensor
     out: torch.Tensor
 
-    def get_padded_lse(self, pad_to: int) -> torch.Tensor:
+    def get_padded_lse(self, pad_to: int, force_pad_inf: bool = False) -> torch.Tensor:
         pad_amount = (pad_to - (self.lse.shape[2] % pad_to)) % pad_to
         lse = self.lse
         if pad_amount > 0:
-            lse = torch.nn.functional.pad(self.lse, [0, pad_amount], value=math.inf)
+            if force_pad_inf:
+                lse = lse[:, :, : self.out.shape[1]]
+                pad_amount = (pad_to - (lse.shape[2] % pad_to)) % pad_to
+            lse = torch.nn.functional.pad(lse, [0, pad_amount], value=math.inf)
+        elif force_pad_inf and self.out.shape[1] != lse.shape[2]:
+            lse[:, :, self.out.shape[1] :].fill_(math.inf)
         return lse
 
 
