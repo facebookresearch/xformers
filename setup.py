6f0602ffc113867db458ead7c05425ec35cb940a
Dan Reif <881372+BMDan@users.noreply.github.com>
2023-06-22 07:35:51 -0700

    fix: handle CUDA arch major >= 10 (#766)
    * fix: handle CUDA arch major >= 10
    
    * Fix: handle +<foo> in version string

--- setup.py
+++ setup.py
@@ -123,7 +123,8 @@ def get_flash_attention_extensions(cuda_version: int, extra_compile_args):
     for arch in archs_list.replace(" ", ";").split(";"):
         assert len(arch) >= 3, f"Invalid sm version: {arch}"
 
-        num = 10 * int(arch[0]) + int(arch[2])
+        arch_arr = arch.split('.')
+        num = 10 * int(arch_arr[0]) + int(arch_arr[1].partition("+")[0])
         # Need at least 7.5
         if num < 75:
             continue
