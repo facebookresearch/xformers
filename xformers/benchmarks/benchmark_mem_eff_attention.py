5d895430c4368e1b43d20f8fd548d0993a8d8b18
danthe3rd <danthe3rd>
2023-08-18 13:19:41 +0000

    [fMHA/benchmark] Fix after changes to attn_bias
    ghstack-source-id: db63a7161b33d8fc78392b5e462809bdefd0b27b
    Pull Request resolved: https://github.com/fairinternal/xformers/pull/754
    
    __original_commit__ = fairinternal/xformers@00ad270d6cc9e887522599cdeccd8a7bf4fc176a

--- xformers/benchmarks/benchmark_mem_eff_attention.py
+++ xformers/benchmarks/benchmark_mem_eff_attention.py
@@ -32,11 +32,8 @@ def create_attn_bias(
     if bias_type is NoneType:
         return None
     if bias_type is torch.Tensor:
-        attn_bias = (
-            torch.randn((batch_size * num_heads, 1, kv_len), device=device, dtype=dtype)
-            * 3
-        )
-        return attn_bias.expand(batch_size * num_heads, q_len, kv_len)
+        attn_bias = torch.randn((1, 1, q_len, kv_len), device=device, dtype=dtype)
+        return attn_bias.expand(batch_size, num_heads, q_len, kv_len)
     if bias_type is xformers.ops.LowerTriangularMask:
         return bias_type()
     assert False, f"Unsupported bias type: {bias_type}"
@@ -64,12 +61,15 @@ def ref_attention_bmk(q, k, v, attn_bias=None, p=0.0):
 
 def ref_attention(q, k, v, attn_bias, p=0.0):
     assert q.ndim == 4
+    B, M, H, K = q.shape
 
     def T(t):
         return t.permute((0, 2, 1, 3)).reshape(
             [t.shape[0] * t.shape[2], t.shape[1], t.shape[3]]
         )
 
+    if isinstance(attn_bias, torch.Tensor):
+        attn_bias = attn_bias.reshape(B * H, M, M)
     out = ref_attention_bmk(T(q), T(k), T(v), attn_bias, p)
     out = out.reshape([q.shape[0], q.shape[2], q.shape[1], v.shape[3]])
     return out.permute((0, 2, 1, 3))
