c5cb52dbeb9fa9079b66afea70a5159bde7a99c3
Francisco Massa <fvsmassa@gmail.com>
2023-09-06 09:05:38 +0000

    Remove bad copy-paste

--- xformers/_deprecation_warning.py
+++ xformers/_deprecation_warning.py
@@ -3,10 +3,6 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-# CREDITS: Inspired by https://github.com/pytorch/text/blob/master/torchtext/nn/modules/multiheadattention.py
-# and the MultiHeadAttention implementation from PyTorch
-
-
 import warnings
 
 
