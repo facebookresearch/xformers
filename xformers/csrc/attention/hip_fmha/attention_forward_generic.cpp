15cff16a274252b5e142e35a14d5d77b5c6aef69
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2023-09-27 18:30:34 -0400

    add boilerplate for invoking the kernel

--- xformers/csrc/attention/hip_fmha/attention_forward_generic.cpp
+++ xformers/csrc/attention/hip_fmha/attention_forward_generic.cpp
@@ -408,6 +408,30 @@ efficient_attention_forward_ck(
   return std::make_tuple(out, logsumexp, philox_seed, philox_offset);
 }
 
+template<typename scalar_t>
+__global__ void
+efficient_attention_forward_decoder_ck_kernel(
+    at::PackedTensorAccessor32<scalar_t, 4, at::RestrictPtrTraits> XQ,
+    at::PackedTensorAccessor64<scalar_t, 4, at::RestrictPtrTraits> cache_K,
+    at::PackedTensorAccessor64<scalar_t, 4, at::RestrictPtrTraits> cache_V,
+    at::PackedTensorAccessor32<scalar_t, 4, at::RestrictPtrTraits> O,
+    at::PackedTensorAccessor32<int32_t, 1, at::RestrictPtrTraits> seq_positions,
+    float qk_scale
+) {
+  __syncthreads();
+}
+
+#define AT_DISPATCH_CASE_3(SCALARTYPE1, SCALARTYPE2, SCALARTYPE3, ...) \
+  AT_DISPATCH_CASE(SCALARTYPE1, __VA_ARGS__) \
+  AT_DISPATCH_CASE(SCALARTYPE2, __VA_ARGS__) \
+  AT_DISPATCH_CASE(SCALARTYPE3, __VA_ARGS__) 
+  
+#define AT_DISPATCH_SWITCH_3(SCALARTYPE1, SCALARTYPE2, SCALARTYPE3, TYPE, NAME, ...) \
+  AT_DISPATCH_SWITCH(                                               \
+      TYPE,                                                         \
+      NAME,                                                         \
+      AT_DISPATCH_CASE_3(SCALARTYPE1, SCALARTYPE2, SCALARTYPE3, __VA_ARGS__))
+
 at::Tensor
 efficient_attention_forward_decoder_ck(
     const at::Tensor& XQ, // [B, 1, H, D]
@@ -416,8 +440,8 @@ efficient_attention_forward_decoder_ck(
     const at::Tensor& seq_positions, // [B]
     double qk_scale) {
 
-  constexpr int32_t kThreadsPerWarp = 32;
-  constexpr int32_t kWarpsPerBlock = 32;
+  constexpr int32_t kThreadsPerWavefront = 32;
+  constexpr int32_t kWavefrontsPerBlock = 32;
   constexpr int32_t D_H = 128;
   constexpr int32_t T_MAX = 8192;
 
@@ -431,9 +455,43 @@ efficient_attention_forward_decoder_ck(
   TORCH_CHECK(cache_K.size(1) <= T_MAX);
   TORCH_CHECK(cache_K.size(3) == D_H);
 
-  auto O = at::randn_like(XQ);
+  auto O = at::empty_like(XQ);
+  auto B = XQ.size(0);
+  auto H = XQ.size(2);
+  dim3 blocks(B, H);
+  dim3 threads(kThreadsPerWavefront, kWavefrontsPerBlock);
+
+  int32_t smem_softmax = T_MAX * sizeof(float) + kWavefrontsPerBlock * sizeof(float);
+  int32_t smem_output = D_H * sizeof(float) * kWavefrontsPerBlock;
+  int32_t smem = max(smem_softmax, smem_output);
+  auto stream = at::cuda::getCurrentHIPStream().stream();
+
+  AT_DISPATCH_SWITCH_3(at::ScalarType::Half, at::ScalarType::BFloat16, at::ScalarType::Float, 
+    XQ.scalar_type(), "efficient_attention_forward_decoder_ck", [&] {
+      auto* kernel = &efficient_attention_forward_decoder_ck_kernel<scalar_t>;
+      if (smem > 48 * 1024) {
+        C10_CUDA_CHECK(hipFuncSetAttribute(
+            reinterpret_cast<void*&>(kernel),
+            hipFuncAttributeMaxDynamicSharedMemorySize,
+            smem));
+      }
+      kernel
+          <<<blocks, threads, smem, stream>>>(
+              XQ.packed_accessor32<scalar_t, 4, at::RestrictPtrTraits>(),
+              cache_K.packed_accessor64<scalar_t, 4, at::RestrictPtrTraits>(),
+              cache_V.packed_accessor64<scalar_t, 4, at::RestrictPtrTraits>(),
+              O.packed_accessor32<scalar_t, 4, at::RestrictPtrTraits>(),
+              seq_positions
+                  .packed_accessor32<int32_t, 1, at::RestrictPtrTraits>(),
+              qk_scale);
+      C10_CUDA_KERNEL_LAUNCH_CHECK();
+  });
+
   return O;
 }  
+
+#undef AT_DISPATCH_CASE_3
+#undef AT_DISPATCH_SWITCH_3
 
 } // namespace
 
