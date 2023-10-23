facebookresearch/xformers

commit ced8681ee5c20ab30ab2b24a94868233e489c355

Tagged versions will override version.txt (#881)
Co-authored-by: Daniel Haziza <dhaziza@meta.com>

.github/workflows/conda.yml
@@ -118,6 +118,7 @@ jobs:
           git config --global --add safe.directory "*"
           $PY -m pip install packaging
           version=`$PY packaging/compute_wheel_version.py`
+          echo $version > version.txt
           echo "BUILD_VERSION=$version" >> ${GITHUB_ENV}
           cat ${GITHUB_ENV}
       - name: Build & store/upload

packaging/compute_wheel_version.py
@@ -28,12 +28,6 @@ def get_tagged_version() -> Optional[str]:
 
     if not tag.startswith("v"):
         return None
-
-    # Should match the version in `version.txt`
-    # (except for the suffix like `rc` tag)
-    assert (
-        version.parse(version_from_file).release == version.parse(tag[1:]).release
-    ), f"The version in version.txt ({version_from_file}) does not match the given tag ({tag})"
     return tag[1:]
 
 

.github/workflows/wheels_reusable.yml
@@ -130,6 +130,7 @@ jobs:
           git config --global --add safe.directory "*"
           $PY -m pip install packaging
           version=`$PY packaging/compute_wheel_version.py`
+          echo $version > version.txt
           echo "BUILD_VERSION=$version${{ steps.cuda_info.outputs.CUDA_VERSION_SUFFIX }}" >> ${GITHUB_ENV}
           echo "BUILD_VERSION=$version${{ steps.cuda_info.outputs.CUDA_VERSION_SUFFIX }}" >> ${GITHUB_OUTPUT}
           cat ${GITHUB_ENV}

commit 98b325299bb6cde15389cb68c6ee0fb77cd24f6c

Fix conda build (regression introduced in ced8681ee5c20ab30ab2b24a94868233e489c355)

.github/workflows/conda.yml
@@ -118,7 +118,6 @@ jobs:
           git config --global --add safe.directory "*"
           $PY -m pip install packaging
           version=`$PY packaging/compute_wheel_version.py`
-          echo $version > version.txt
           echo "BUILD_VERSION=$version" >> ${GITHUB_ENV}
           cat ${GITHUB_ENV}
       - name: Build & store/upload

packaging/compute_wheel_version.py
@@ -30,6 +30,7 @@ def get_tagged_version() -> Optional[str]:
 
 
 def get_dev_version() -> str:
+    assert ".dev" not in version_from_file
     num_commits = subprocess.check_output(
         ["git", "rev-list", "--count", "HEAD"], text=True
     ).strip()