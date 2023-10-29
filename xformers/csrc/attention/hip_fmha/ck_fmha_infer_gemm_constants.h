50b829e8c07378e2d8e56c79c2747ae4341c26e8
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2023-10-29 19:09:30 +0000

    [Performance] update to the infer gemm constants

--- xformers/csrc/attention/hip_fmha/ck_fmha_infer_gemm_constants.h
+++ xformers/csrc/attention/hip_fmha/ck_fmha_infer_gemm_constants.h
@@ -13,27 +13,27 @@ struct GemmOpConstantsBatchedInfer {
   static constexpr ck::index_t KPerBlock = 32;
   // static constexpr ck::index_t Gemm1NPerBlock;
   static constexpr ck::index_t Gemm1KPerBlock = 32;
-  static constexpr ck::index_t AK1 = 8;
-  static constexpr ck::index_t BK1 = 8;
+  static constexpr ck::index_t AK1 = 4;
+  static constexpr ck::index_t BK1 = 4;
   static constexpr ck::index_t B1K1 = 2;
   static constexpr ck::index_t MPerXDL = 32;
   static constexpr ck::index_t NPerXDL = 32;
   static constexpr ck::index_t MXdlPerWave = 1;
   static constexpr ck::index_t NXdlPerWave = 4;
   // static constexpr ck::index_t Gemm1NXdlPerWave;
-  using ABlockTransferThreadClusterLengths_AK0_M_AK1 = S<4, 64, 1>;
+  using ABlockTransferThreadClusterLengths_AK0_M_AK1 = S<8, 32, 1>;
   using ABlockTransferThreadClusterArrangeOrder = S<1, 0, 2>;
   using ABlockTransferSrcAccessOrder = S<1, 0, 2>;
   static constexpr ck::index_t ABlockTransferSrcVectorDim = 2;
   // static constexpr ck::index_t ABlockTransferSrcScalarPerVector;
-  static constexpr ck::index_t ABlockTransferDstScalarPerVector_AK1 = 8;
+  static constexpr ck::index_t ABlockTransferDstScalarPerVector_AK1 = 4;
   static constexpr bool ABlockLdsExtraM = true;
-  using BBlockTransferThreadClusterLengths_BK0_N_BK1 = S<4, 64, 1>;
+  using BBlockTransferThreadClusterLengths_BK0_N_BK1 = S<8, 32, 1>;
   using BBlockTransferThreadClusterArrangeOrder = S<1, 0, 2>;
   using BBlockTransferSrcAccessOrder = S<1, 0, 2>;
   static constexpr ck::index_t BBlockTransferSrcVectorDim = 2;
   // static constexpr ck::index_t BBlockTransferSrcScalarPerVector;
-  static constexpr ck::index_t BBlockTransferDstScalarPerVector_BK1 = 8;
+  static constexpr ck::index_t BBlockTransferDstScalarPerVector_BK1 = 4;
   static constexpr bool BBlockLdsExtraN = true;
   // static constexpr ck::index_t Acc0BiasTransferSrcScalarPerVector;
   using B1BlockTransferThreadClusterLengths_BK0_N_BK1 = S<16, 16, 1>;
@@ -61,27 +61,27 @@ struct GemmOpConstantsGroupedInfer {
   static constexpr ck::index_t KPerBlock = 32;
   // static constexpr ck::index_t Gemm1NPerBlock;
   static constexpr ck::index_t Gemm1KPerBlock = 32;
-  static constexpr ck::index_t AK1 = 8;
-  static constexpr ck::index_t BK1 = 8;
+  static constexpr ck::index_t AK1 = 4;
+  static constexpr ck::index_t BK1 = 4;
   static constexpr ck::index_t B1K1 = 2;
   static constexpr ck::index_t MPerXDL = 32;
   static constexpr ck::index_t NPerXDL = 32;
   static constexpr ck::index_t MXdlPerWave = 1;
   static constexpr ck::index_t NXdlPerWave = 4;
   // static constexpr ck::index_t Gemm1NXdlPerWave;
-  using ABlockTransferThreadClusterLengths_AK0_M_AK1 = S<4, 64, 1>;
+  using ABlockTransferThreadClusterLengths_AK0_M_AK1 = S<8, 32, 1>;
   using ABlockTransferThreadClusterArrangeOrder = S<1, 0, 2>;
   using ABlockTransferSrcAccessOrder = S<1, 0, 2>;
   static constexpr ck::index_t ABlockTransferSrcVectorDim = 2;
   // static constexpr ck::index_t ABlockTransferSrcScalarPerVector,
-  static constexpr ck::index_t ABlockTransferDstScalarPerVector_AK1 = 8;
+  static constexpr ck::index_t ABlockTransferDstScalarPerVector_AK1 = 4;
   static constexpr bool ABlockLdsExtraM = true;
-  using BBlockTransferThreadClusterLengths_BK0_N_BK1 = S<4, 64, 1>;
+  using BBlockTransferThreadClusterLengths_BK0_N_BK1 = S<8, 32, 1>;
   using BBlockTransferThreadClusterArrangeOrder = S<1, 0, 2>;
   using BBlockTransferSrcAccessOrder = S<1, 0, 2>;
   static constexpr ck::index_t BBlockTransferSrcVectorDim = 2;
   // static constexpr ck::index_t BBlockTransferSrcScalarPerVector;
-  static constexpr ck::index_t BBlockTransferDstScalarPerVector_BK1 = 8;
+  static constexpr ck::index_t BBlockTransferDstScalarPerVector_BK1 = 4;
   static constexpr bool BBlockLdsExtraN = true;
   // static constexpr ck::index_t Acc0BiasTransferSrcScalarPerVector;
   using B1BlockTransferThreadClusterLengths_BK0_N_BK1 = S<16, 16, 1>;
@@ -98,7 +98,3 @@ struct GemmOpConstantsGroupedInfer {
   // static constexpr ck::index_t
   // CShuffleBlockTransferScalarPerVector_NPerBlock;
 };
-
-struct GemmOpConstantsForward {};
-
-struct GemmOpConstantsBackward {};
