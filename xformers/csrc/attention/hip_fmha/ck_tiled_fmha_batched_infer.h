00a407069b53daab1416059c658e6852e83cdb88
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2023-11-29 17:41:55 +0000

    Update to have ck-tiled group mode pass the unit-tests

--- xformers/csrc/attention/hip_fmha/ck_tiled_fmha_batched_infer.h
+++ xformers/csrc/attention/hip_fmha/ck_tiled_fmha_batched_infer.h
@@ -4,12 +4,12 @@
 #include <sstream>
 #include <stdexcept>
 
+#include <ck/utility/common_header.hpp>
 #include <ck/host_utility/device_prop.hpp>
 #include <ck/host_utility/kernel_launch.hpp>
-#include <ck/tensor/tensor_view.hpp>
 #include <ck/tensor_description/cluster_descriptor.hpp>
 #include <ck/tensor_description/tensor_descriptor_helper.hpp>
-#include <ck/utility/common_header.hpp>
+#include <ck/tensor/tensor_view.hpp>
 
 #include <ck/tile_program/block_tile_pipeline/block_fmha_pipeline_problem.hpp>
 #include <ck/tile_program/block_tile_pipeline/block_fmha_pipeline_qkvs.hpp>
@@ -24,161 +24,154 @@
 #include "ck_tiled_fmha_params.h"
 
 template <typename scalar_t, int32_t custom_mask_type>
-struct batched_infer_masktype_attnbias_dispatched {
-  using QDataType = scalar_t;
-  using KDataType = scalar_t;
-  using VDataType = scalar_t;
-  using BiasDataType = scalar_t;
-  using SaccDataType = float; // data type for first gemm accumulation
-  using SMPLComputeDataType = float; // data type for reduction, softmax
-  using PDataType = scalar_t; // data type for A matrix of second gemm
-  using OaccDataType = float; // data type for second gemm accumulation
-  using ODataType = scalar_t;
+struct batched_infer_masktype_attnbias_dispatched
+{
+    using QDataType           = scalar_t;
+    using KDataType           = scalar_t;
+    using VDataType           = scalar_t;
+    using BiasDataType        = scalar_t;
+    using SaccDataType        = float;    // data type for first gemm accumulation
+    using SMPLComputeDataType = float;    // data type for reduction, softmax
+    using PDataType           = scalar_t; // data type for A matrix of second gemm
+    using OaccDataType        = float;    // data type for second gemm accumulation
+    using ODataType           = scalar_t;
 
-  using VLayout = ck::tensor_layout::gemm::RowMajor;
+    using VLayout = ck::tensor_layout::gemm::RowMajor;
 
-  using FmhaBlockTileHdim64 = ck::Sequence<128, 64, 32, 64, 32, 64>;
-  using FmhaBlockTileHdim128 = ck::Sequence<128, 128, 32, 128, 32, 128>;
-  using FmhaBlockWarps = ck::Sequence<4, 1, 1>;
-  using FmhaWarpTile = ck::Sequence<32, 32, 16>;
-  using FmhaShapeHDim64 = ck::tile_program::TileFmhaShape<
-      FmhaBlockTileHdim64,
-      FmhaBlockWarps,
-      FmhaWarpTile,
-      FmhaBlockWarps,
-      FmhaWarpTile,
-      VLayout>;
-  using FmhaShapeHDim128 = ck::tile_program::TileFmhaShape<
-      FmhaBlockTileHdim128,
-      FmhaBlockWarps,
-      FmhaWarpTile,
-      FmhaBlockWarps,
-      FmhaWarpTile,
-      VLayout>;
+    using FmhaBlockTileHdim64  = ck::Sequence<128, 64, 32, 64, 32, 64>;
+    using FmhaBlockTileHdim128 = ck::Sequence<128, 128, 32, 128, 32, 128>;
+    using FmhaBlockWarps       = ck::Sequence<4, 1, 1>;
+    using FmhaWarpTile         = ck::Sequence<32, 32, 16>;
+    using FmhaShapeHDim64      = ck::tile_program::TileFmhaShape<FmhaBlockTileHdim64,
+                                                            FmhaBlockWarps,
+                                                            FmhaWarpTile,
+                                                            FmhaBlockWarps,
+                                                            FmhaWarpTile,
+                                                            VLayout>;
+    using FmhaShapeHDim128     = ck::tile_program::TileFmhaShape<FmhaBlockTileHdim128,
+                                                             FmhaBlockWarps,
+                                                             FmhaWarpTile,
+                                                             FmhaBlockWarps,
+                                                             FmhaWarpTile,
+                                                             VLayout>;
 
-  using FmhaTilePartitionerHDim64 = FmhaFwdTilePartitioner<FmhaShapeHDim64>;
-  using FmhaTilePartitionerHDim128 = FmhaFwdTilePartitioner<FmhaShapeHDim128>;
-  using FmhaPipelineProblemHDim64 =
-      ck::tile_program::block::BlockFmhaPipelineProblem<
-          QDataType,
-          KDataType,
-          VDataType,
-          SaccDataType,
-          SMPLComputeDataType,
-          BiasDataType,
-          PDataType,
-          OaccDataType,
-          ODataType,
-          256, // BlockSize
-          FmhaShapeHDim64>;
-  using FmhaPipelineProblemHDim128 =
-      ck::tile_program::block::BlockFmhaPipelineProblem<
-          QDataType,
-          KDataType,
-          VDataType,
-          SaccDataType,
-          SMPLComputeDataType,
-          BiasDataType,
-          PDataType,
-          OaccDataType,
-          ODataType,
-          256, // BlockSize
-          FmhaShapeHDim128>;
+    using FmhaTilePartitionerHDim64  = FmhaFwdTilePartitioner<FmhaShapeHDim64>;
+    using FmhaTilePartitionerHDim128 = FmhaFwdTilePartitioner<FmhaShapeHDim128>;
+    using FmhaPipelineProblemHDim64 =
+        ck::tile_program::block::BlockFmhaPipelineProblem<QDataType,
+                                                          KDataType,
+                                                          VDataType,
+                                                          SaccDataType,
+                                                          SMPLComputeDataType,
+                                                          BiasDataType,
+                                                          PDataType,
+                                                          OaccDataType,
+                                                          ODataType,
+                                                          256, // BlockSize
+                                                          FmhaShapeHDim64>;
+    using FmhaPipelineProblemHDim128 =
+        ck::tile_program::block::BlockFmhaPipelineProblem<QDataType,
+                                                          KDataType,
+                                                          VDataType,
+                                                          SaccDataType,
+                                                          SMPLComputeDataType,
+                                                          BiasDataType,
+                                                          PDataType,
+                                                          OaccDataType,
+                                                          ODataType,
+                                                          256, // BlockSize
+                                                          FmhaShapeHDim128>;
 
-  using FmhaPipelineHDim64 = ck::tile_program::block::BlockFmhaPipelineQRKSVS<
-      FmhaPipelineProblemHDim64>;
-  using FmhaPipelineHDim128 = ck::tile_program::block::BlockFmhaPipelineQRKSVS<
-      FmhaPipelineProblemHDim128>;
+    using FmhaPipelineHDim64 =
+        ck::tile_program::block::BlockFmhaPipelineQRKSVS<FmhaPipelineProblemHDim64>;
+    using FmhaPipelineHDim128 =
+        ck::tile_program::block::BlockFmhaPipelineQRKSVS<FmhaPipelineProblemHDim128>;
 
-  using FmhaEpilogue =
-      FmhaFwdEpilogue<FmhaFwdEpilogueProblem<OaccDataType, ODataType>>;
-  using FmhaKernelHDim64 = FmhaFwdKernel<
-      FmhaTilePartitionerHDim64,
-      FmhaPipelineHDim64,
-      FmhaEpilogue>;
-  using FmhaKernelHDim128 = FmhaFwdKernel<
-      FmhaTilePartitionerHDim128,
-      FmhaPipelineHDim128,
-      FmhaEpilogue>;
+    using FmhaEpilogue = FmhaFwdEpilogue<FmhaFwdEpilogueProblem<OaccDataType, ODataType>>;
+
+    // ToDo: define NeedPadding according to runtime lengths
+    static constexpr bool NeedPadding = true;
+
+    using FmhaKernelHDim64 =
+        FmhaFwdKernel<FmhaTilePartitionerHDim64, FmhaPipelineHDim64, FmhaEpilogue, NeedPadding>;
+    using FmhaKernelHDim128 =
+        FmhaFwdKernel<FmhaTilePartitionerHDim128, FmhaPipelineHDim128, FmhaEpilogue, NeedPadding>;
 
 #ifndef BATCHED_INFER_HEADDIM_SWITCH
-#define BATCHED_INFER_HEADDIM_SWITCH(HEAD_DIM1, HEAD_DIM2, ...)  \
-  [&] {                                                          \
-    if (HEAD_DIM1 == HEAD_DIM2 && HEAD_DIM2 == 64) {             \
-      using FmhaKernel = FmhaKernelHDim64;                       \
-      __VA_ARGS__();                                             \
-    } else if (HEAD_DIM1 == HEAD_DIM2 && HEAD_DIM2 == 128) {     \
-      using FmhaKernel = FmhaKernelHDim128;                      \
-      __VA_ARGS__();                                             \
-    } else {                                                     \
-      throw std::runtime_error("Head-dim sizes not supported!"); \
-    }                                                            \
-  }()
+#define BATCHED_INFER_HEADDIM_SWITCH(HEAD_DIM1, HEAD_DIM2, ...)        \
+    [&] {                                                              \
+        if(HEAD_DIM1 == HEAD_DIM2 && HEAD_DIM2 == 64)                  \
+        {                                                              \
+            using FmhaKernel = FmhaKernelHDim64;                       \
+            __VA_ARGS__();                                             \
+        }                                                              \
+        else if(HEAD_DIM1 == HEAD_DIM2 && HEAD_DIM2 == 128)            \
+        {                                                              \
+            using FmhaKernel = FmhaKernelHDim128;                      \
+            __VA_ARGS__();                                             \
+        }                                                              \
+        else                                                           \
+        {                                                              \
+            throw std::runtime_error("Head-dim sizes not supported!"); \
+        }                                                              \
+    }()
 #endif
 
-  static void Run(BatchedForwardParams& param, hipStream_t stream) {
-    BATCHED_INFER_HEADDIM_SWITCH(
-        param.K, param.Kv, [&] { RunWithKernel<FmhaKernel>(param, stream); });
-  };
+    static void Run(BatchedForwardParams& param, hipStream_t stream)
+    {
+        BATCHED_INFER_HEADDIM_SWITCH(
+            param.K, param.Kv, [&] { RunWithKernel<FmhaKernel>(param, stream); });
+    };
 
-  template <typename FmhaKernel>
-  static void RunWithKernel(BatchedForwardParams& param, hipStream_t stream) {
-    dim3 kGridSize = FmhaKernel::GridSize(param.B, param.Hq, param.M, param.Kv);
-    constexpr dim3 kBlockSize = FmhaKernel::BlockSize();
+    template <typename FmhaKernel>
+    static void RunWithKernel(BatchedForwardParams& param, hipStream_t stream)
+    {
+        dim3 kGridSize            = FmhaKernel::GridSize(param.B, param.Hq, param.M, param.Kv);
+        constexpr dim3 kBlockSize = FmhaKernel::BlockSize();
 
-    constexpr ck::index_t kWarpPerCu = 8; // 2 warps per SIMD
-    constexpr ck::index_t kWarpPerBlock = kBlockSize.x / warpSize;
-    constexpr ck::index_t kBlockPerCu = kWarpPerCu / kWarpPerBlock;
+        constexpr ck::index_t kWarpPerCu    = 8; // 2 warps per SIMD
+        constexpr ck::index_t kWarpPerBlock = kBlockSize.x / warpSize;
+        constexpr ck::index_t kBlockPerCu   = kWarpPerCu / kWarpPerBlock;
 
-    std::optional<
-        std::tuple<const void*, ck::index_t, ck::index_t, ck::index_t>>
-        bias;
+        std::optional<std::tuple<const void*, ck::index_t, ck::index_t, ck::index_t>> bias;
 
-    if (param.has_attn_bias)
-      bias = std::make_tuple(
-          param.attn_bias_ptr,
-          param.attn_bias_strides[2],
-          param.attn_bias_strides[1],
-          param.attn_bias_strides[0]);
+        if(param.has_attn_bias)
+            bias = std::make_tuple(param.attn_bias_ptr,
+                                   param.attn_bias_strides[2],
+                                   param.attn_bias_strides[1],
+                                   param.attn_bias_strides[0]);
 
-    auto kargs = FmhaKernel::MakeKargs(
-        param.q_ptr,
-        param.k_ptr,
-        param.v_ptr,
-        param.out_ptr,
-        param.M, // seqlen_q
-        param.N, // seqlen_k
-        param.K, // hdim_q
-        param.Kv, // hdim_v
-        param.scale,
-        param.q_strides[1], // q, k, v, out tensor seq-dim stride
-        param.k_strides[1],
-        param.v_strides[1],
-        param.out_strides[1],
-        param.q_strides[2], // q, k, v, out tensor head-dim stride
-        param.k_strides[2],
-        param.v_strides[2],
-        param.out_strides[2],
-        param.q_strides[0], // q, k, v, out tensor batch-dim stride
-        param.k_strides[0],
-        param.v_strides[0],
-        param.out_strides[0],
-        bias);
+        auto kargs =
+            FmhaKernel::MakeKargs(param.q_ptr,
+                                  param.k_ptr,
+                                  param.v_ptr,
+                                  param.out_ptr,
+                                  param.M,  // seqlen_q
+                                  param.N,  // seqlen_k
+                                  param.K,  // hdim_q
+                                  param.Kv, // hdim_v
+                                  param.scale,
+                                  param.q_strides[1], // q, k, v, out tensor seq-dim stride
+                                  param.k_strides[1],
+                                  param.v_strides[1],
+                                  param.out_strides[1],
+                                  param.q_strides[2], // q, k, v, out tensor head-dim stride
+                                  param.k_strides[2],
+                                  param.v_strides[2],
+                                  param.out_strides[2],
+                                  param.q_strides[0], // q, k, v, out tensor batch-dim stride
+                                  param.k_strides[0],
+                                  param.v_strides[0],
+                                  param.out_strides[0],
+                                  bias);
 
-    (void)launch_kernel<kBlockSize.x, kBlockPerCu>(
-        StreamConfig{stream, false},
-        FmhaKernel{},
-        kGridSize,
-        kBlockSize,
-        0,
-        kargs);
-  };
+        (void)launch_kernel<kBlockSize.x, kBlockPerCu>(
+            StreamConfig{stream, false}, FmhaKernel{}, kGridSize, kBlockSize, 0, kargs);
+    };
 };
 
 template <typename scalar_t, int32_t custom_mask_type>
-void run_batched_infer_masktype_attnbias_dispatched(
-    BatchedForwardParams& param,
-    hipStream_t stream) {
-  batched_infer_masktype_attnbias_dispatched<scalar_t, custom_mask_type>::Run(
-      param, stream);
+void run_batched_infer_masktype_attnbias_dispatched(BatchedForwardParams& param, hipStream_t stream)
+{
+    batched_infer_masktype_attnbias_dispatched<scalar_t, custom_mask_type>::Run(param, stream);
 };
--- xformers/csrc/attention/hip_fmha/ck_tiled_fmha_fwd_epilogue.h
+++ xformers/csrc/attention/hip_fmha/ck_tiled_fmha_fwd_epilogue.h
@@ -1,34 +1,32 @@
 #pragma once
 
+#include "ck/utility/common_header.hpp"
 #include "ck/tile_program/tile/store_tile.hpp"
 #include "ck/tile_program/tile/tile_elementwise.hpp"
-#include "ck/utility/common_header.hpp"
 
 template <typename OaccDataType_, typename ODataType_>
-struct FmhaFwdEpilogueProblem {
-  using OaccDataType = ck::remove_cvref_t<OaccDataType_>;
-  using ODataType = ck::remove_cvref_t<ODataType_>;
+struct FmhaFwdEpilogueProblem
+{
+    using OaccDataType = ck::remove_cvref_t<OaccDataType_>;
+    using ODataType    = ck::remove_cvref_t<ODataType_>;
 };
 
 template <typename Problem_, typename Policy_ = void>
-struct FmhaFwdEpilogue {
-  using Problem = ck::remove_cvref_t<Problem_>;
-  using OaccDataType = ck::remove_cvref_t<typename Problem::OaccDataType>;
-  using ODataType = ck::remove_cvref_t<typename Problem::ODataType>;
+struct FmhaFwdEpilogue
+{
+    using Problem      = ck::remove_cvref_t<Problem_>;
+    using OaccDataType = ck::remove_cvref_t<typename Problem::OaccDataType>;
+    using ODataType    = ck::remove_cvref_t<typename Problem::ODataType>;
 
-  __host__ __device__ static constexpr ck::index_t GetSmemSize() {
-    return 0;
-  }
+    __host__ __device__ static constexpr ck::index_t GetSmemSize() { return 0; }
 
-  template <typename ODramWindowTmp, typename OAccTile>
-  __device__ auto operator()(
-      ODramWindowTmp& o_dram_window_tmp,
-      const OAccTile& o_acc_tile) {
-    using namespace ck;
-    using namespace ck::tile_program;
+    template <typename ODramWindowTmp, typename OAccTile>
+    __device__ auto operator()(ODramWindowTmp& o_dram_window_tmp, const OAccTile& o_acc_tile)
+    {
+        using namespace ck;
+        using namespace ck::tile_program;
 
-    const auto o =
-        tile_elementwise_in(type_convert<ODataType, OaccDataType>, o_acc_tile);
-    store_tile(o_dram_window_tmp, o);
-  }
+        const auto o = tile_elementwise_in(type_convert<ODataType, OaccDataType>, o_acc_tile);
+        store_tile(o_dram_window_tmp, o);
+    }
 };
