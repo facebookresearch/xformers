d7132b9425b4c81b2cadbd7cafaa1d0cda6e2fa0
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-01-18 19:23:13 +0000

    address code review: remove unused _rand_seqlens

--- tests/test_mem_eff_attention.py
+++ tests/test_mem_eff_attention.py
@@ -379,45 +379,6 @@ def ref_attention_splitk(q, k, v, attn_bias, scale=None, split_k=2) -> torch.Ten
     return out
 
 
-def _rand_seqlens(
-    r: random.Random,
-    bs: int,
-    q_len: int,
-    kv_len: int,
-    more_keys_than_queries_per_block: bool,
-) -> Tuple[Sequence[int], Sequence[int]]:
-    """
-    Generates lists of lengths of query blocks and corresponding key blocks.
-    The total number of queries will be bs * q_len and the
-    total number of keys will be bs * kv_len.
-    """
-    if more_keys_than_queries_per_block:
-        assert kv_len >= q_len
-    q_len *= bs
-    kv_len *= bs
-    seqlens_q: List[int] = []
-    seqlens_k: List[int] = []
-
-    step_q = [max(1, q_len // 10), max(2, q_len // 2)]
-    step_k = [max(1, kv_len // 10), max(2, kv_len // 2)]
-    while sum(seqlens_q) < q_len and sum(seqlens_k) < kv_len:
-        num_queries = r.randrange(*step_q)
-        seqlens_q.append(num_queries)
-
-        if more_keys_than_queries_per_block:
-            # Must select at least `num_queries` keys
-            # But also leave enough keys for later
-            keys_left = kv_len - sum(seqlens_k, 0)
-            queries_left = q_len - sum(seqlens_q[:-1], 0)
-            assert keys_left >= queries_left
-            seqlens_k.append(num_queries + r.randrange(0, keys_left - queries_left))
-        else:
-            seqlens_k.append(r.randrange(*step_k))
-    seqlens_q[-1] = q_len - sum(seqlens_q[:-1])
-    seqlens_k[-1] = kv_len - sum(seqlens_k[:-1])
-    return seqlens_q, seqlens_k
-
-
 def _rand_partition(r: random.Random, total: int, n: int) -> List[int]:
     # returns list of n nonnegative integers summing to total
     idx = {0, total}
