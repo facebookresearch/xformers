243dc6a0ef3907ab1903ca84f91ce72b36c70e41
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2024-02-07 15:07:21 +0000

    Revert "Add conditional compiling for cuda-depending codes in ROCM"
    This reverts commit 12fb41c2460909285102426ca9ab52162725d64b.

--- xformers/csrc/attention/sparse_softmax.cpp
+++ xformers/csrc/attention/sparse_softmax.cpp
@@ -9,10 +9,8 @@
 #include <torch/types.h>
 
 TORCH_LIBRARY_FRAGMENT(xformers, m) {
-#if !defined(USE_ROCM)
   m.def(TORCH_SELECTIVE_SCHEMA(
       "xformers::sparse_softmax_sputnik(int m, int n, Tensor row_indices, Tensor values, Tensor row_offsets, Tensor column_indices) -> Tensor"));
   m.def(TORCH_SELECTIVE_SCHEMA(
       "xformers::sparse_softmax_backward_sputnik(int m, int n, Tensor row_indices, Tensor values, Tensor gradient, Tensor row_offsets, Tensor column_indices) -> Tensor"));
-#endif
 }
--- xformers/csrc/attention/spmm.cpp
+++ xformers/csrc/attention/spmm.cpp
@@ -9,8 +9,6 @@
 #include <torch/types.h>
 
 TORCH_LIBRARY_FRAGMENT(xformers, m) {
-#if !defined(USE_ROCM)
   m.def(TORCH_SELECTIVE_SCHEMA(
       "xformers::spmm_sputnik(Tensor b, Tensor row_indices, Tensor values, Tensor row_offsets, Tensor column_indices, int m) -> Tensor"));
-#endif
 }
