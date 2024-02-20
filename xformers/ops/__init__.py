99ad1723b0b80fb21c5e4dc45446e93752f41656
dan_the_3rd <43445237+danthe3rd@users.noreply.github.com>
2024-02-20 17:32:09 +0000

    Extract IPC component from fused seqpar (fairinternal/xformers#1032)
    __original_commit__ = fairinternal/xformers@6b7051524960da972796687f4b7206efd8c92dad

--- xformers/ops/__init__.py
+++ xformers/ops/__init__.py
@@ -23,6 +23,7 @@ from .fmha import (
     memory_efficient_attention_forward_requires_grad,
 )
 from .indexing import index_select_cat, scaled_index_add
+from .ipc import init_ipc
 from .modpar_layers import ColumnParallelLinear, RowParallelLinear
 from .rmsnorm import RMSNorm
 from .rope_padded import rope_padded
@@ -93,6 +94,8 @@ __all__ = [
     # indexing
     "index_select_cat",
     "scaled_index_add",
+    # ipc
+    "init_ipc",
     # modpar_layers
     "ColumnParallelLinear",
     "RowParallelLinear",
