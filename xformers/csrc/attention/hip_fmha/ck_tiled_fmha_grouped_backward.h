cf2b6224222528f5b0fc9c932ecd2e224260bee8
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2024-07-28 13:10:02 +0000

    Tiny fix in grouped_backward

--- xformers/csrc/attention/hip_fmha/ck_tiled_fmha_grouped_backward.h
+++ xformers/csrc/attention/hip_fmha/ck_tiled_fmha_grouped_backward.h
@@ -269,8 +269,7 @@ struct grouped_backward_causalmask_bias_dropout_dispatch {
           param.attn_bias_strides[1],
           0, // stride_randval
           param.grad_out_strides[0],
-          NeedConvertGradQ ? param.grad_q_f32_strides[0]
-                           : param.grad_q_f32_strides[0],
+          NeedConvertGradQ ? param.grad_q_f32_strides[0] : param.q_strides[0],
           param.grad_k_strides[0],
           param.grad_v_strides[0],
           param.attn_bias_strides[1], // assume grad_bias has same strides as
@@ -282,8 +281,7 @@ struct grouped_backward_causalmask_bias_dropout_dispatch {
           param.attn_bias_strides[0],
           0, // nhead_stride_randval
           param.grad_out_strides[1],
-          NeedConvertGradQ ? param.grad_q_f32_strides[1]
-                           : param.grad_q_f32_strides[1],
+          NeedConvertGradQ ? param.grad_q_f32_strides[1] : param.q_strides[1],
           param.lsed_strides[1], // assume lse/dot is in BHM contiguous layout
           param.attn_bias_strides[0], // assume grad_bias has same strides as
                                       // bias
