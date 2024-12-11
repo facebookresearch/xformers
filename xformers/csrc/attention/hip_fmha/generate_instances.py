da455ec71cf589eed235a9ac8848fe46fd860957
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-12-11 19:53:32 +0000

    fix merge conflict (1)

--- xformers/csrc/attention/hip_fmha/generate_instances.py
+++ xformers/csrc/attention/hip_fmha/generate_instances.py
@@ -137,7 +137,9 @@ def create_infer_instances(instance_dir: Path, headdims: List) -> None:
                             fname = FMHA_INFER_INSTANCE_FNAME.format(
                                 mode=mode,
                                 dtype_str=dtype,
-                                has_or_no_mask_str=BOOL_MAP_MASK[has_mask],
+                                has_or_no_mask_str=BOOL_MAP_MASK[
+                                    has_mask
+                                ],
                                 has_or_no_bias_str=BOOL_MAP_BIAS[has_bias],
                                 has_or_no_dropout_str=BOOL_MAP_DROPOUT[has_dropout],
                                 max_k_str=INT_MAP_MAX_K[max_k],
@@ -208,7 +210,9 @@ def create_forward_instances(instance_dir: Path, headdims: List) -> None:
                             fname = FMHA_FORWARD_INSTANCE_FNAME.format(
                                 mode=mode,
                                 dtype_str=dtype,
-                                has_or_no_mask_str=BOOL_MAP_MASK[has_mask],
+                                has_or_no_mask_str=BOOL_MAP_MASK[
+                                    has_mask
+                                ],
                                 has_or_no_bias_str=BOOL_MAP_BIAS[has_bias],
                                 has_or_no_dropout_str=BOOL_MAP_DROPOUT[has_dropout],
                                 max_k_str=INT_MAP_MAX_K[max_k],
@@ -285,7 +289,9 @@ def create_backward_instances(instance_dir: Path, headdims: List) -> None:
                             fname = FMHA_BACKWARD_INSTANCE_FNAME.format(
                                 mode=mode,
                                 dtype_str=dtype,
-                                has_or_no_mask_str=BOOL_MAP_MASK[has_mask],
+                                has_or_no_mask_str=BOOL_MAP_MASK[
+                                    has_mask
+                                ],
                                 has_or_no_bias_str=BOOL_MAP_BIAS[has_bias],
                                 has_or_no_biasgrad_str=BOOL_MAP_BIASGRAD[has_bias_grad],
                                 has_or_no_dropout_str=BOOL_MAP_DROPOUT[has_dropout],
