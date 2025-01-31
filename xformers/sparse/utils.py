9bb11fad224de0b4328df8a5ea0b3bab5cd49240
Francisco Massa <fvsmassa@gmail.com>
2025-01-31 16:05:08 +0000

    Fix _get_transpose_info following fix to _coo_to_csr (fairinternal/xformers#1287)
    After fixing the behavior of _coo_to_csr, that exposed an issue with _get_transpose_info that was not present because there was a double transposition
    
    __original_commit__ = fairinternal/xformers@e464301b2a20b8b16ea110d5cb379ffae196b511

--- xformers/sparse/utils.py
+++ xformers/sparse/utils.py
@@ -39,7 +39,7 @@ def _get_transpose_info(m, n, row_indices, row_offsets, column_indices):
     row_offsets_t, perm = column_indices.sort(dim=0, stable=True)
     column_indices_t = row_coo[perm]
 
-    row_offsets_t, _ = _coo_to_csr(m, n, row_offsets_t, column_indices)
+    row_offsets_t, _ = _coo_to_csr(n, m, row_offsets_t, column_indices)
     row_indices_t = _diffsort(row_offsets_t).int()
 
     return row_indices_t, row_offsets_t, column_indices_t, perm
