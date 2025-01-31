65b9f2a1ec035a406ed6cb134925158b10d238dd
Francisco Massa <fvsmassa@gmail.com>
2025-01-31 13:16:34 +0000

    Fix _coo_to_csr when n > m (fairinternal/xformers#1285)
    Fixes https://github.com/facebookresearch/xformers/issues/1125. Thanks @francois-rozet for the report and proposed fix!
    
    __original_commit__ = fairinternal/xformers@2d765618db79cc2316a59c85342a679ae7170a3e

--- xformers/sparse/utils.py
+++ xformers/sparse/utils.py
@@ -9,7 +9,7 @@ import torch
 
 def _coo_to_csr(m, n, row_indices, column_indices):
     # assumes coalesced coo
-    row_offsets = row_indices.bincount(minlength=n).cumsum(0, dtype=row_indices.dtype)
+    row_offsets = row_indices.bincount(minlength=m).cumsum(0, dtype=row_indices.dtype)
     row_offsets = torch.nn.functional.pad(row_offsets, (1, 0))
     return row_offsets, column_indices
 
