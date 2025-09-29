5146f2ab37b2163985c19fb4e8fbf6183e82f8ce
Jeremy Reizenstein <669761+bottler@users.noreply.github.com>
2025-09-29 17:02:27 +0000

    remove old flash support, enable 2.8.3 (fairinternal/xformers#1422)
    No need to support the old (pre pytorch2.7) flash in PyTorch, therefore flash always has varlen lse. Also allow FA 2.8.3 as requested in facebookresearch/xformers#1339
    
    __original_commit__ = fairinternal/xformers@35526eb1d9280b0f6be2cc039006a322f892b0e4

--- xformers/ops/fmha/torch_attention_compat.py
+++ xformers/ops/fmha/torch_attention_compat.py
@@ -3,8 +3,6 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional
-
 import torch
 from torch._C import parse_schema
 
@@ -61,41 +59,23 @@ def is_pt_cutlass_compatible(force: bool = False) -> bool:
     return compatible
 
 
-def is_pt_flash_old(force: bool) -> Optional[bool]:
+def ensure_pt_flash_ok() -> None:
     """
-    Returns True if the current PyTorch version has the old Flash-Attention
-    ops instead of the new ones.
-    If it has none at all, raises an ImportError or returns None.
+    Raises an ImportError if the current PyTorch version has an unexpected Flash-Attention.
     """
     if not torch.backends.cuda.is_flash_attention_available():
-        if force:
-            raise ImportError("Flash SDP backend is disabled")
-        return None
+        raise ImportError("Flash SDP backend is disabled")
 
     if not hasattr(torch.nn, "attention") or not hasattr(
         torch.nn.attention, "_get_flash_version"
     ):
-        if force:
-            raise ImportError(
-                f"Current Torch {torch.__version__} doesnt implement "
-                "torch.nn.attention._get_flash_version()"
-            )
-        return None
+        raise ImportError(
+            f"Current Torch {torch.__version__} doesnt implement "
+            "torch.nn.attention._get_flash_version()"
+        )
 
     FLASH_VERSION = torch.nn.attention._get_flash_version()
 
-    compatible = True
-
-    # old = before 25/2/2025
-    # https://github.com/pytorch/pytorch/commit/3ecfe6be256c585bcadf4c845d7119545444a222
-    old_fwd_schema_str = (
-        "aten::_flash_attention_forward(Tensor query, Tensor key, Tensor value, "
-        "Tensor? cum_seq_q, Tensor? cum_seq_k, SymInt max_q, SymInt max_k, float dropout_p, "
-        "bool is_causal, bool return_debug_mask, *, float? scale=None, "
-        "SymInt? window_size_left=None, SymInt? window_size_right=None, "
-        "Tensor? seqused_k=None, Tensor? alibi_slopes=None) -> (Tensor output, Tensor softmax_logsumexp, "
-        "Tensor philox_seed, Tensor philox_offset, Tensor debug_attn_mask)"
-    )
     fwd_schema_str = (
         "aten::_flash_attention_forward(Tensor query, Tensor key, Tensor value, "
         "Tensor? cum_seq_q, Tensor? cum_seq_k, SymInt max_q, SymInt max_k, float dropout_p, "
@@ -105,48 +85,29 @@ def is_pt_flash_old(force: bool) -> Optional[bool]:
         "Tensor rng_state, Tensor unused, Tensor debug_attn_mask)"
     )
     expected_fwd_schema = parse_schema(fwd_schema_str)
-    expected_old_fwd_schema = parse_schema(old_fwd_schema_str)
 
     current_schema = torch.ops.aten._flash_attention_forward.default._schema
-    old = current_schema.is_backward_compatible_with(expected_old_fwd_schema)  # type: ignore
-    if not old and not current_schema.is_backward_compatible_with(expected_fwd_schema):  # type: ignore
-        compatible = False
+    if not current_schema.is_backward_compatible_with(expected_fwd_schema):  # type: ignore
+        raise ImportError(
+            f"Current Torch with Flash-Attention {FLASH_VERSION} doesnt have "
+            "a compatible aten::_flash_attention_forward schema\n"
+            f"EXPECTED:\n{expected_fwd_schema}\n"
+            f"but GOT:\n{current_schema}"
+        )
 
-        if force:
-            raise ImportError(
-                f"Current Torch with Flash-Attention {FLASH_VERSION} doesnt have "
-                "a compatible aten::_flash_attention_forward schema\n"
-                f"EXPECTED:\n{expected_old_fwd_schema}\n"
-                f"or:\n{expected_fwd_schema}\n"
-                f"but GOT:\n{current_schema}"
-            )
-
-    bwd_schema_old_str = (
-        "aten::_flash_attention_backward(Tensor grad_out, Tensor query, Tensor key, Tensor value, "
-        "Tensor out, Tensor logsumexp, Tensor cum_seq_q, Tensor cum_seq_k, SymInt max_q, SymInt max_k, "
-        "float dropout_p, bool is_causal, Tensor philox_seed, Tensor philox_offset, *, float? scale=None, "
-        "SymInt? window_size_left=None, SymInt? window_size_right=None) -> (Tensor, Tensor, Tensor)"
-    )
     bwd_schema_str = (
         "aten::_flash_attention_backward(Tensor grad_out, Tensor query, Tensor key, Tensor value, "
         "Tensor out, Tensor logsumexp, Tensor cum_seq_q, Tensor cum_seq_k, SymInt max_q, SymInt max_k, "
         "float dropout_p, bool is_causal, Tensor rng_state, Tensor unused, *, float? scale=None, "
         "SymInt? window_size_left=None, SymInt? window_size_right=None) -> (Tensor, Tensor, Tensor)"
     )
-    expected_bwd_schema = parse_schema(bwd_schema_old_str if old else bwd_schema_str)
+    expected_bwd_schema = parse_schema(bwd_schema_str)
 
     current_schema = torch.ops.aten._flash_attention_backward.default._schema
     if not current_schema.is_backward_compatible_with(expected_bwd_schema):  # type: ignore
-        compatible = False
-
-        if force:
-            raise ImportError(
-                f"Current Torch with Flash-Attention {FLASH_VERSION} doesnt have "
-                "a compatible aten::_flash_attention_backward schema\n"
-                f"EXPECTED:\n{expected_bwd_schema}\n"
-                f"but GOT:\n{current_schema}"
-            )
-
-    if not compatible:
-        return None
-    return old
+        raise ImportError(
+            f"Current Torch with Flash-Attention {FLASH_VERSION} doesnt have "
+            "a compatible aten::_flash_attention_backward schema\n"
+            f"EXPECTED:\n{expected_bwd_schema}\n"
+            f"but GOT:\n{current_schema}"
+        )
